fb44723e5b9752a930f9b623368c81e7a3906052
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2015-09-23 13:39:02 +0200

    add some dummy code to the smbo class

--- smac/smbo/smbo.py
+++ smac/smbo/smbo.py
@@ -1,20 +1,35 @@
 import logging
 
-# I already implemented the local search and EI in RoBO maybe we can reuse it here
+# I already implemented the local search in ESMAC and EI in RoBO maybe we can reuse it here
 from robo.acquisition.ei import EI
 from robo.maximizers.local_search import LocalSearch
 from robo.solver.base_solver import BaseSolver
+
+import smac.smbo.intensification
+import smac.smbo.run_history
 
 
 class BayesianOptimization(BaseSolver):
     # Implements the Bayesian optimization loop. I derived this class from the RoBO interface in order to share the output and logging functionality between RoBO and SMAC.
     def __init__(self):
-        pass
+        self.model = RandomForest()
+        self.acquisition_func = EI(self.model)
+        self.local_search = LocalSearch(self.acquisition_func)
 
-    def run(self):
+    def run(self, max_iters):
         # Here will be the main Bayesian optimization loop by iteratively calling the choose_next method
-        pass
+        #TODO: Initialize X, Y
+        X = None
+        Y = None
+        for i in range(max_iters):
+            next_config = self.choose_next(X, Y)
+            incumbent = itensify()
+            #TODO: Evaluate nex_config and update X, Y
+        return incumbent
 
     def choose_next(self, X=None, Y=None):
         # Chooses the next configuration by training the model and optimizing the acquisition function. It performs basically one iteration of BO
-        pass
+        self.model.fit(X, Y)
+        self.acquisition_func.update(self.model)
+        configuration = self.local_search.maximize()
+        return configuration
