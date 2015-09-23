532448c77079ae01f97e9dfac057ae0a2cbee2df
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2015-09-23 14:56:01 +0200

    make it pep8 compatible

--- smac/smbo/smbo.py
+++ smac/smbo/smbo.py
@@ -1,27 +1,40 @@
 import logging
 import numpy as np
 
-# Aaron: I already implemented the local search in ESMAC and EI in RoBO maybe we can reuse it here
+# Aaron: I already implemented the local search in ESMAC and EI in RoBO maybe
+# we can reuse it here
+
 # from robo.acquisition.ei import EI
 # from robo.maximizers.local_search import LocalSearch
 # from robo.solver.base_solver import BaseSolver
 
 
 class BayesianOptimization(object):
-    # Aaron: I would like to derive this class from the RoBO interface in order to share the output and logging functionality between RoBO and SMAC.
-    # Aaron: I will change the default value to RandomForest, EI and LocalSearch as soon as we have configured the dependencies
-    def __init__(self, model=None, acquisition_func=None, maximizer=None, seed=42):
+    # Aaron: I would like to derive this class from the RoBO interface in order
+    # to share the output and logging functionality between RoBO and SMAC.
+    # Aaron: I will change the default value to RandomForest, EI and
+    # LocalSearch as soon as we have configured the dependencies
+
+    def __init__(self, model=None,
+                 acquisition_func=None,
+                 maximizer=None,
+                 seed=42):
         '''
             Implementation of the main Bayesian optimization loop
             Args:
-                model : A model that captures our believe of our objective function (robo model object)
-                acquisition_func: Surrogate function to pick a new configuration (robo acquisition object)
-                maximizer: Optimization strategy to maximize the acquisition function (robo maximizer object)
+                model : A model that captures our believe of our objective
+                    function (robo model object)
+                acquisition_func : Surrogate function to pick a new
+                    configuration (robo acquisition object)
+                maximizer: Optimization strategy to maximize the
+                    acquisition function (robo maximizer object)
                 seed : random seed (integer)
         '''
         self.model = model
-        # Aaron: Maybe we do not want to pass the acquisition function and the maximizer as argument but rather initialize them here. Passing them
-        # as an argument makes only sense if we want to use something different than EI and LocalSearch
+        # Aaron: Maybe we do not want to pass the acquisition function and the
+        # maximizer as argument but rather initialize them here. Passing them
+        # as an argument makes only sense if we want to use something different
+        # than EI and LocalSearch
         self.acquisition_func = acquisition_func
         self.maximizer = maximizer
         self.seed = seed
@@ -40,13 +53,15 @@ class BayesianOptimization(object):
             #incumbent = itensify()
             #Evaluate nex_config and update X, Y
 
-        # Aaron: I assume in RoBO always 2 dimensional numpy arrays (that is how vectors are handled in GPy as well)
+        # Aaron: I assume in RoBO always 2 dimensional numpy arrays (that is
+        # how vectors are handled in GPy as well)
         incumbent = np.random.randn(1, 1)
         return incumbent
 
     def choose_next(self, X=None, Y=None):
         '''
-            Chooses the next configuration by training the model and optimizing the acquisition function.
+            Chooses the next configuration by training the model and
+            optimizing the acquisition function.
             Args:
                 X : The configuration we have seen so far (2D numpy array)
                 Y : The function values of the configurations (2D numpy array)
