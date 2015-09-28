d360eb60b2a882f990a0315528cc0ecee6ad3d79
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2015-09-28 16:40:53 +0200

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+setuptools.setup(
+    name="smac",
+    version="0.0.1dev",
+    author="Marius Lindauer",
+    author_email="lindauer@cs.uni-freiburg.de",
+    description=("SMAC3, a python implementation of 'Sequential Model-based "
+                 "Algorithm Configuration'."),
+    license="BSD",
+    keywords="machine learning algorithm configuration hyperparameter "
+             "optimization tuning",
+    url="",
+    packages=setuptools.find_packages(exclude=['test']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    platforms=['Linux'],
+    install_requires=['setuptools',
+                      'numpy>=0.16.2',
+                      'scipy>=0.9.0',
+                      'pysmac'],
+    test_suite = 'nose.collector'
+)
