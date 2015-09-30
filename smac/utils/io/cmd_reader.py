43539c0d702d6fc6edef8afbee189c6168db104d
Marius Lindauer <marius.rks@googlemail.com>
2015-09-30 14:19:45 +0200

    adapt code to yesterdays meeting decisions

--- smac/utils/io/cmd_reader.py
+++ smac/utils/io/cmd_reader.py
@@ -1,8 +1,9 @@
-'''
-Created on Sep 23, 2015
-
-@author: lindauer
-'''
+__author__ = "Marius Lindauer"
+__copyright__ = "Copyright 2015, ML4AAD"
+__license__ = "BSD"
+__maintainer__ = "Marius Lindauer"
+__email__ = "lindauer@cs.uni-freiburg.de"
+__version__ = "0.0.1"
 
 import os
 import logging
@@ -10,23 +11,31 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
 class CMDReader(object):
-    '''
+
+    """
         use argparse to parse command line options
-    '''
+
+        Attributes
+        ----------
+        logger : Logger oject
+    """
 
     def __init__(self):
-        '''
+        """
         Constructor
-        '''
+        """
         self.logger = logging.getLogger("CMDReader")
         pass
 
     def read_cmd(self):
-        '''
+        """
             reads command line options
-            Returns:
-                parsed arguements
-        '''
+
+            Returns
+            -------
+                args_: parsed arguments; return of parse_args of ArgumentParser
+        """
+
         parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
         req_opts = parser.add_argument_group("Required Options")
         req_opts.add_argument("--scenario_file", required=True,
@@ -42,10 +51,20 @@ class CMDReader(object):
         return args_
 
     def _check_args(self, args_):
-        '''
+        """
             checks command line arguments
             (e.g., whether all given files exist)
-            Raises Errors in case of problems
-        '''
+
+            Parameters
+            ----------
+            args_: parsed arguments
+                parsed command line arguments
+
+            Raises
+            ------
+            ValueError
+                in case of missing files or wrong configurations
+        """
+
         if not os.path.isfile(args_.scenario_file):
             raise ValueError("Not found: %s" % (args_.scenario_file))
