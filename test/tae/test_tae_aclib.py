0e0dbe1579ce4b5b617c650b10ab280c384490e8
Marius Lindauer <marius.rks@googlemail.com>
2015-11-19 14:39:30 +0100

    running target algorithms in new Aclib 2.0 style

--- test/tae/test_tae_aclib.py
+++ test/tae/test_tae_aclib.py
@@ -7,7 +7,7 @@ import unittest
 import shlex
 import logging
 
-from smac.tae.execute_ta_run_old import ExecuteTARunOld
+from smac.tae.execute_ta_run_aclib import ExecuteTARunAClib
 from smac.tae.execute_ta_run import StatusType
 
 
@@ -15,29 +15,29 @@ class TaeOldTest(unittest.TestCase):
 
     def test_run(self):
         '''
-            running some simple algo in old style
+            running some simple algo in aclib 2.0 style
         '''
-        eta = ExecuteTARunOld(ta=shlex.split("python test/tae/dummy_ta_wrapper.py 1"))
+        eta = ExecuteTARunAClib(ta=shlex.split("python test/tae/dummy_ta_wrapper_aclib.py 1"))
         status, cost, runtime, ar_info = eta.run(config={})
-        assert status == StatusType.SUCCESS
-        assert cost == 1.0
-        assert runtime == 1.0
-        
-        print(status, cost, runtime)
-        
-        eta = ExecuteTARunOld(ta=shlex.split("python test/tae/dummy_ta_wrapper.py 2"))
-        status, cost, runtime, ar_info = eta.run(config={})
-        assert status == StatusType.SUCCESS
+        assert status == StatusType.TIMEOUT
         assert cost == 2.0
         assert runtime == 2.0
         
         print(status, cost, runtime)
         
-        eta = ExecuteTARunOld(ta=shlex.split("python test/tae/dummy_ta_wrapper.py 2"), run_obj="quality")
+        eta = ExecuteTARunAClib(ta=shlex.split("python test/tae/dummy_ta_wrapper_aclib.py 2"))
+        status, cost, runtime, ar_info = eta.run(config={})
+        assert status == StatusType.SUCCESS
+        assert cost == 3.0
+        assert runtime == 3.0
+        
+        print(status, cost, runtime)
+        
+        eta = ExecuteTARunAClib(ta=shlex.split("python test/tae/dummy_ta_wrapper_aclib.py 2"), run_obj="quality")
         status, cost, runtime, ar_info = eta.run(config={},)
         assert status == StatusType.SUCCESS
-        assert cost == 4.0
-        assert runtime == 2.0
+        assert cost == 2.0
+        assert runtime == 3.0
         
         print(status, cost, runtime, ar_info)
         
