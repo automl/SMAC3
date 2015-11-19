086c67058f3e070c930eef2aa97a21519368084b
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-11-19 14:53:00 +0100

    UPDATE docstrings

--- smac/epm/random_epm.py
+++ smac/epm/random_epm.py
@@ -1,47 +1,62 @@
-'''
-Created on Sep 25, 2015
-
-@author: eggenspk
-'''
 import logging
 import numpy
 
-import smac.smbo.run_history
-import smac.epm.base_Epm
+import smac.runhistory.runhistory
+import smac.epm.base_epm
 import smac.configspace
 
+__author__ = "Katharina Eggensperger"
+__copyright__ = "Copyright 2015, ML4AAD"
+__license__ = "BSD"
+__maintainer__ = "Katharina Eggensperger"
+__email__ = "eggenspk@cs.uni-freiburg.de"
+__version__ = "0.0.1"
 
-class RandomEpm(smac.epm.base_Epm.baseEPM):
-    """implement an epm, which returns only random values"""
+
+class RandomEpm(smac.epm.base_epm.baseEPM):
+    '''implement an epm, which returns only random values'''
 
     def __init__(self, rng):
-        """
-        initialize random number generator
-        :param rng: any integer
-        """
+        '''
+        initialize random number generator and logger
+
+        Parameters
+        ----------
+        rng : int
+        '''
         self.logger = logging.getLogger("random_epm")
         numpy.random.seed(rng)
 
     def fit(self, run_history):
-        """
+        '''
         fit model to run history
 
-        :param run_history: Object that keeps complete run_history
-        """
+        Parameters
+        ----------
+        run_history : dict
+            Object that keeps complete run_history
+        '''
+
         # (KE) I assume a model can either read run_history or run_history
         # provides a method to transform data to a matrix
-        if not isinstance(run_history, smac.smbo.run_history):
+        if not isinstance(run_history, smac.runhistory.runhistory.RunHistory):
             raise NotImplementedError("Can only fit on run_history")
         self.logger.debug("Fit model to data")
 
     def update(self, config, value, instance_feature=None):
-        """
+                '''
         Update model (if possible)
 
-        :param config: one configuration (configspace.configuration)
-        :param value: one return value (float)
-        :param instance_feature: list of features
-        """
+        Parameters
+        ----------
+        config : configSpace.config
+            configuration
+        value : float
+            costs for config
+        instance_features : list
+            list of instance features
+        '''
+
         if not isinstance(config, smac.configspace.configuration):
             raise NotImplementedError("Can only update with "
                                       "smac.configspace.configuration")
@@ -52,21 +67,30 @@ class RandomEpm(smac.epm.base_Epm.baseEPM):
             raise NotImplementedError("Can only update with instance_feature"
                                       " being a list")
 
-        dummy_run_history = smac.smbo.run_history()
+        dummy_run_history = smac.runhistory.runhistory.RunHistory()
         dummy_run_history.add(config, value, instance_feature)
         # (KE) If a model is updateable, it will read a run_history entry
         # or matrix
         self.logger.debug("Update model with one sample")
 
     def predict(self, configs, instance_features=None):
-        """Predict values for configs
+        '''
+        Predict values for configs
 
-        :param configs: list of configurations
-        :param instance_features: list of instance features
-        :return: predictions
-        """
+        Parameters
+        ----------
+        configs : list
+            list of configurations
+
+        instance_features : list
+            list of instance features
+
+        Returns
+        -------
+        predictions
+        '''
         if not isinstance(configs, list):
             raise NotImplementedError("Can only predict for lists of configs")
         # (KE): I assume a model can either read a list of config or configspace
         # provides a method to transform data to matrix
-        return numpy.random.rand(len(configs))
+        return numpy.random.rand(len(configs), 1)
