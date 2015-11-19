eac5ff646d0ebc007395b0c005f300a9e219e155
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-11-19 12:25:04 +0100

    ADD test for runhistory2epm

---
+++ test/runhistory/test_runhistory_to_epm.py
@@ -0,0 +1,33 @@
+__author__ = "Katharina Eggensperger"
+__copyright__ = "Copyright 2015, ML4AAD"
+__license__ = "BSD"
+__maintainer__ = "Katharina Eggensperger"
+__email__ = "eggenspk@cs.uni-freiburg.de"
+__version__ = "0.0.1"
+
+import unittest
+from smac.runhistory import runhistory, runhistory_to_epm
+
+
+class RunhistoryTest(unittest.TestCase):
+
+    def test_add(self):
+        '''
+            simply adding some rundata to runhistory
+        '''
+        rh = runhistory.RunHistory()
+        rh.add(config={'a': '1', 'b': '2'}, cost=10, time=20,
+               status="SUCCESS", instance_id=None,
+               seed=None,
+               additional_info=None)
+        rh.add(config={'a': '1', 'b': '2'}, cost=10, time=20,
+               status="SUCCESS", instance_id=1,
+               seed=12354,
+               additional_info={"start_time": 10})
+
+        rh2epm = runhistory_to_epm.RunHistoryToEPM()
+        print(rh2epm.fit(rh))
+
+
+if __name__ == "__main__":
+    unittest.main()
