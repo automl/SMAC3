4884e5a3c2bdb0a132c05399e54113789e1f53e3
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-11-19 12:09:30 +0100

    ADD skeleton for runhistory -> vector

---
+++ smac/runhistory/runhistory_to_epm.py
@@ -0,0 +1,56 @@
+__author__ = "Katharina Eggensperger"
+__copyright__ = "Copyright 2015, ML4AAD"
+__license__ = "BSD"
+__maintainer__ = "Katharina Eggensperger"
+__email__ = "eggenspk@cs.uni-freiburg.de"
+__version__ = "0.0.1"
+
+import logging
+
+
+class RunHistoryToEPM(object):
+    '''
+        takes a runhistory object and preprocess data in order to train EPM
+    '''
+
+    def __init__(self, config=None):
+        '''
+        Constructor
+        '''
+        self.config = dict()
+        self.logger = logging.getLogger("epm")
+        if config is not None:
+            self.config.update(config)
+
+    def fit(self, runhistory):
+        '''
+        sets all internal information to transform a runhistory to a vector
+
+        Attributes
+        ----------
+            runhistory : list of dicts
+                parameter configurations
+        '''
+        pass
+
+    def fit_transform(self, runhistory):
+        '''
+        calls fit and transform, returns vector representation of runhistory
+
+        Attributes
+        ----------
+        runhistory : list of dicts
+                parameter configurations
+        '''
+        pass
+
+    def tranform(self, runhistory):
+        '''
+        returns vector representation of runhistory
+
+        Attributes
+        ----------
+        runhistory : list of dicts
+                parameter configurations
+        '''
+        pass
