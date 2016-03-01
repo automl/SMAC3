7ab532b2907637b92b1d4e974e41251ceaec3a31
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-03-01 19:02:19 +0100

    ADD code to do imputation, not yet tested

--- smac/runhistory/runhistory2epm.py
+++ smac/runhistory/runhistory2epm.py
@@ -7,6 +7,7 @@ import numpy
 from smac.tae.execute_ta_run import StatusType
 from smac.runhistory.runhistory import RunHistory
 from smac.configspace import impute_inactive_values
+import smac.epm.base_imputor
 
 __author__ = "Katharina Eggensperger"
 __copyright__ = "Copyright 2015, ML4AAD"
@@ -16,13 +17,24 @@ __email__ = "eggenspk@cs.uni-freiburg.de"
 __version__ = "0.0.1"
 
 
+class RunType(object):
+
+    """
+        class to define numbers for status types
+    """
+    SUCCESS = 1
+    TIMEOUT = 2
+    CENSORED = 3
+
+
 class RunHistory2EPM(object):
     '''
         takes a runhistory object and preprocess data in order to train EPM
     '''
 
     def __init__(self, num_params, cutoff_time, success_states=None,
-                 impute_censored_data=False, impute_state=None):
+                 impute_censored_data=False, impute_state=None, imputor=None,
+                 rs=None):
         '''
         Constructor
         Parameters
@@ -38,12 +50,33 @@ class RunHistory2EPM(object):
         impute_state: list, optional
             list of states that mark censored data (such as StatusType.TIMEOUT)
             in combination with runtime < cutoff_time
+        rs : numpy.random.RandomState
+            only used for reshuffling data after imputation
         '''
+
+        if rs is None:
+            self.rs = numpy.random.RandomState()
+        else:
+            self.rs = rs
+
         if impute_state is None:
             impute_state = [StatusType.TIMEOUT, ]
 
         if success_states is None:
             success_states = [StatusType.SUCCESS, ]
+
+        self.imputor = imputor
+        if impute_censored_data and self.imputor is None:
+            self.logger.critical("You want me to impute cencored data, but "
+                                 "I don't know how. Imputor is None")
+            raise ValueError("impute_cencored data, but no imputor given")
+        elif impute_censored_data and not isinstance(self.imputor,
+                                                     smac.epm.base_imputor):
+            raise ValueError("Given imputor is not an instance of "
+                             "smac.epm.base_imputor")
+        else:
+            # Everything is fine
+            pass
 
         self.config = OrderedDict({
             'success_states': success_states,
@@ -54,33 +87,7 @@ class RunHistory2EPM(object):
         self.logger = logging.getLogger("runhistory2epm")
         self.num_params = num_params
 
-    def transform(self, runhistory):
-        '''
-        returns vector representation of runhistory
-
-        Parameters
-        ----------
-        runhistory : list of dicts
-                parameter configurations
-        '''
-        assert isinstance(runhistory, RunHistory)
-
-        # consider only successfully finished runs, put them in a list
-        run_list = self.__select_runs(rh_data=copy.deepcopy(runhistory.data),
-                                      select_censored=False)
-        # Store a list of instance IDs
-        instance_id_list = [k.instance_id for k in run_list.keys()]
-
-        if self.config['impute_censored_data']:
-            cens_list = self.__select_runs(rh_data=copy.deepcopy(runhistory.data),
-                                           select_censored=True)
-            # Store a list of instance IDs
-            cens_instance_id_list = [k.instance_id for k in cens_list.keys()]
-
-            cens_list = [c.config for c in cens_list.keys()]
-            raise NotImplementedError("Imputation of right censored data is not"
-                                      " yet possible")
-
+    def _build_matrix(self, run_list, runhistory, instances=None):
         # First build nan-matrix of size #configs x #params+1
         n_rows = len(run_list)
         n_cols = self.num_params
@@ -94,12 +101,80 @@ class RunHistory2EPM(object):
             conf = impute_inactive_values(conf)
             X[row, :] = conf.get_array()
             # TODO: replace with instance features if available
-            #run_array[row, -1] = instance_id_list[row]
+            #run_array[row, -1] = instances[row]
             Y[row, 0] = run.cost
+        return X, Y
+
+    def transform(self, runhistory, shuffle=True):
+        '''
+        returns vector representation of runhistory
+
+        Parameters
+        ----------
+        runhistory : list of dicts
+                parameter configurations
+        shuffle : bool, optional
+                shuffle array to mix successful, timeouts and imputed data,
+                probably makes everything nondeterministic
+        '''
+        assert isinstance(runhistory, RunHistory)
+
+        # consider only successfully finished runs
+        s_run_list = self.__select_runs(rh_data=copy.deepcopy(runhistory.data),
+                                        select=RunType.SUCCESS)
+        # Store a list of instance IDs
+        s_instance_id_list = [k.instance_id for k in s_run_list.keys()]
+        X, Y = self._build_matrix(run_list=s_run_list, runhistory=runhistory,
+                                  instances=s_instance_id_list)
+
+        # Also get TIMEOUT runs
+        t_run_list = self.__select_runs(rh_data=copy.deepcopy(runhistory.data),
+                                        select=RunType.TIMEOUT)
+        t_instance_id_list = [k.instance_id for k in s_run_list.keys()]
+
+        tX, tY = self._build_matrix(run_list=t_run_list, runhistory=runhistory,
+                                    instances=t_instance_id_list)
+
+        if self.config['impute_censored_data']:
+            # Get all censored runs
+            c_run_list = self.__select_runs(rh_data=copy.deepcopy(runhistory.data),
+                                            select=RunType.CENSORED)
+            # Store a list of instance IDs
+            c_instance_id_list = [k.instance_id for k in c_run_list.keys()]
+            c_run_list = [c.config for c in c_run_list.keys()]
+
+            cen_X, cen_Y = self._build_matrix(run_list=c_run_list,
+                                              runhistory=runhistory,
+                                              instances=c_instance_id_list)
+
+            # Also impute TIMEOUTS
+            cen_X = numpy.vstack((cen_X, tX))
+            cen_Y = numpy.vstack((cen_Y, tY))
+
+            if shuffle:
+                shuffle_idx = self.rs.permutation(X.shape[0])
+                cen_X = cen_X[shuffle_idx, :]
+                cen_Y = cen_Y[shuffle_idx, :]
+
+            imp_Y = self.imputor.impute(censored_X=cen_X, censored_y=cen_Y,
+                                        uncensored_X=X, uncensored_y=Y)
+
+            # Shuffle data to mix censored and imputed data
+            X = numpy.vstack((X, cen_X))
+            Y = numpy.concatenate(Y, imp_Y)
+        else:
+            # If we do not impute,w e also return TIMEOUT data
+            X = numpy.vstack((X, tX))
+            Y = numpy.vstack((Y, tY))
+
+        if shuffle:
+            shuffle_idx = self.rs.permutation(X.shape[0])
+            X = X[shuffle_idx, :]
+            Y = Y[shuffle_idx, :]
 
         return X, Y
 
-    def __select_runs(self, rh_data, select_censored=False):
+    def __select_runs(self, rh_data, select):
         '''
         select runs of a runhistory
 
@@ -108,27 +183,35 @@ class RunHistory2EPM(object):
         rh_data : runhistory
             dict of ConfigSpace.config
 
-        select_censored : bool, optional
-            return censored runs
-            if True return runs with status in self.config['impute_state'] and
-            runtime < self.config['cutoff_time']
-            if False return with with status in self.config['success_states']
-
+        select : RunType.SUCCESS
+            one of "success", "timeout", "censored"
+            return only runs for this type
         Returns
         -------
         list of ConfigSpace.config
         '''
         new_dict = dict()
-        if select_censored:
+
+        if select == RunType.SUCCESS:
             for run in rh_data.keys():
-                if rh_data[run].status in self.config['impute_state'] and \
-                        rh_data[run].time < self.config['cutoff_time']:
-                    # This run was censored
+                if rh_data[run].status in self.config['success_states']:
+                    new_dict[run] = rh_data[run]
+        elif select == RunType.TIMEOUT:
+            for run in rh_data.keys():
+                if (rh_data[run].status == StatusType.TIMEOUT and
+                             rh_data[run].time >= self.config['cutoff_time']):
+                    new_dict[run] = rh_data[run]
+        elif select == RunType.CENSORED:
+            for run in rh_data.keys():
+                if rh_data[run].status in self.config['impute_state'] \
+                        and rh_data[run].time < self.config['cutoff_time']:
                     new_dict[run] = rh_data[run]
         else:
-            for run in rh_data.keys():
-                if rh_data[run].status in self.config['success_states'] or \
-                    (rh_data[run].status == StatusType.TIMEOUT and rh_data[run].time >= self.config['cutoff_time']):
-                    # This run was successful or a not censored timeout
-                    new_dict[run] = rh_data[run]
+            err_msg = "select must be in (%s), but is %s" % \
+                      (",".join(["%d" % t for t in
+                                [RunType.SUCCESS, RunType.TIMEOUT,
+                                 RunType.CENSORED]]), select)
+            self.logger.critical(err_msg)
+            raise ValueError(err_msg)
+
         return new_dict
