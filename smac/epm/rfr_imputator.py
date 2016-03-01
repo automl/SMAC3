aca4cd25862c798157352feedb683ac7aa904afe
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-03-01 19:01:25 +0100

    MAINT remove cutoff and thresholf as prediction parameters

--- smac/epm/rfr_imputator.py
+++ smac/epm/rfr_imputator.py
@@ -16,7 +16,7 @@ __version__ = "0.0.1"
 class RFRImputator(smac.epm.base_imputor.BaseImputor):
     """Uses an rfr to do imputation"""
 
-    def __init__(self, max_iter, cs, rs, change_threshold=0.01):
+    def __init__(self, cs, rs, cutoff, threshold, change_threshold=0.01, max_iter=10):
         """
         initialize imputator module
 
@@ -25,6 +25,10 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
         max_iter : maximum number of iteration
         cs : config space object
         rs : random state generator
+        cutoff : int
+            cutoff value used for this scenario
+        threshold : float
+            highest possible values (e.g. cutoff * par)
         change_threshold : stop imputation if change is less than this
         -------
         """
@@ -33,6 +37,8 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
         self.logger = logging.getLogger("RFRImputor")
         self.max_iter = max_iter
         self.change_threshold = change_threshold
+        self.cutoff = cutoff
+        self.threshold = threshold
         self.seed = rs.random_integer(low=0, high=1000)
 
         # TODO find out correct syntax of this
@@ -94,8 +100,7 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
 
         return mean, std
 
-    def impute(self, censored_X, censored_y, uncensored_X, uncensored_y,
-               cutoff, threshold):
+    def impute(self, censored_X, censored_y, uncensored_X, uncensored_y):
         """
         impute runs and returns imputed y values
 
@@ -108,16 +113,15 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
         uncensored_y : list
         """
 
-        self.logger.debug("Start imputation with cutoff %f and threshold %f" %
-                          (cutoff, threshold))
-
         # first learn model without censored data
         m = self._get_model(uncensored_X, uncensored_y)
 
         self.logger.debug("Going to impute y-values with %s" % str(m))
 
         imputed_y = None  # define this, if imputation fails
-        self.used_it = -1
+
+        # Define variables
+        y = None
 
         it = 0
         change = 0
@@ -143,7 +147,7 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
             self.logger.critical("Going to replace %d nan-value(s) with "
                                  "threshold" %
                                  sum(numpy.isfinite(imputed_y) == False))
-            imputed_y[numpy.isfinite(imputed_y) == False] = threshold
+            imputed_y[numpy.isfinite(imputed_y) == False] = self.threshold
 
             if it > 0:
                 # Calc mean difference between imputed values this and last
@@ -153,12 +157,12 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
                                     y[uncensored_y.shape[0]:])
 
             # lower all values that are higher than threshold
-            imputed_y[imputed_y >= threshold] = threshold
+            imputed_y[imputed_y >= self.threshold] = self.threshold
 
             self.logger.debug("Change: %f" % change)
 
             X = numpy.concatenate((uncensored_X, censored_X))
-            y = numpy.concatenate((uncensored_y, imputed_y))
+            y = numpy.concatenate((uncensored_y, imputed_y)).flatten()
 
             if change > self.change_threshold or it == 0:
                 m = self._get_model(X, y)
@@ -170,10 +174,10 @@ class RFRImputator(smac.epm.base_imputor.BaseImputor):
                 break
 
         self.logger.info("Imputation used %d/%d iterations, last_change=%f)" %
-                     (it, self.max_iter, change))
+                         (it, self.max_iter, change))
 
-        new_ys = numpy.array(imputed_y, dtype=numpy.float32)
-        new_ys[new_ys >= threshold] = threshold
+        new_ys = numpy.array(imputed_y, dtype=numpy.float)
+        new_ys[new_ys >= self.threshold] = self.threshold
 
         if not numpy.isfinite(new_ys).all():
             self.logger.critical("Imputed values are not finite, %s" %
