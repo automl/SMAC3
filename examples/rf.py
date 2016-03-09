f80d2da40d8c6bf78a78764ca839a42bbb39f8b6
Marius Lindauer <lindauer@informatik.uni-freiburg.de>
2016-03-09 15:43:40 +0100

    adding a further example to tune the rf...

---
+++ examples/rf.py
@@ -0,0 +1,142 @@
+from __future__ import division, print_function
+
+
+# TODO: remove really ugly boilerplate
+import logging
+import sys
+import os
+import inspect
+cmd_folder = os.path.realpath(
+    os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+cmd_folder = os.path.realpath(os.path.join(cmd_folder, ".."))
+if cmd_folder not in sys.path:
+    sys.path.append(cmd_folder)
+
+import numpy as np
+from sklearn.cross_validation import KFold
+from pyrfr import regression32 as regression
+
+from smac.configspace import ConfigurationSpace
+from ConfigSpace.hyperparameters import CategoricalHyperparameter, \
+    UniformFloatHyperparameter, UniformIntegerHyperparameter
+
+from smac.tae.execute_func import ExecuteTAFunc
+from smac.scenario.scenario import Scenario
+from smac.smbo.smbo import SMBO
+from smac.stats.stats import Stats
+
+
+def rfr(cfg):
+    """
+    We optimize our own random forest with SMAC 
+    """
+
+    types = np.max(X, axis=0)
+    types[types<=1.1] = 0
+    types = np.array(types, dtype=np.uint)
+
+    rf = regression.binary_rss()
+    rf.num_trees = cfg["num_trees"]
+    rf.seed = 42
+    rf.do_bootstrapping = cfg["do_bootstrapping"] == "true"
+    rf.num_data_points_per_tree = int(X.shape[0] * cfg["frac_points_per_tree"])
+    rf.max_features = int(types.shape[0] * cfg["ratio_features"])
+    rf.min_samples_to_split = cfg["min_samples_to_split"]
+    rf.min_samples_in_leaf = cfg["min_samples_in_leaf"]
+    rf.max_depth = cfg["max_depth"]
+    #rf.epsilon_purity = cfg["eps_purity"]
+    rf.max_num_nodes = cfg["max_num_nodes"]
+    
+    rmses = []
+    for train, test in kf:
+        X_train = X[train,:]
+        y_train = y[train]
+        X_test = X[test,:]
+        y_test = y[test]
+        
+        print(X_train.shape)
+        print(y_train.shape)
+        print(types)
+        print(types.shape)
+        data = regression.numpy_data_container(X_train,
+                                                     y_train,
+                                                     types)
+        rf.fit(data)
+        
+        y_pred = rf.predict(X_test)
+        
+        rmse = np.sqrt(np.mean((y_pred - y_test[:,0])**2))
+        rmses.append(rmse)
+        
+    return np.mean(rmses) 
+    
+
+folder = os.path.realpath(
+    os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+
+# load data
+X = np.array(np.load(os.path.join(folder,"data/uncen_X.npy")))
+y = np.array(np.load(os.path.join(folder,"data/uncen_y.npy")))
+
+# cv folds
+kf = KFold(X.shape[0], n_folds=4)
+
+# register function to be optimize
+taf = ExecuteTAFunc(rfr)
+
+# build Configuration Space which defines all parameters and their ranges
+# to illustrate different parameter types,
+# we use continuous, integer and categorical parameters
+cs = ConfigurationSpace()
+do_bootstrapping = CategoricalHyperparameter(
+    "do_bootstrapping", ["true","false"], default="true")
+cs.add_hyperparameter(do_bootstrapping)
+
+num_trees = UniformIntegerHyperparameter("num_trees", 10, 50, default=10)
+cs.add_hyperparameter(num_trees)
+
+frac_points_per_tree = UniformFloatHyperparameter("frac_points_per_tree", 0.001, 1, default=1)
+cs.add_hyperparameter(frac_points_per_tree)
+
+ratio_features = UniformFloatHyperparameter("ratio_features", 0.001, 1, default=1)
+cs.add_hyperparameter(ratio_features)
+
+min_samples_to_split = UniformIntegerHyperparameter("min_samples_to_split", 2, 20, default=2)
+cs.add_hyperparameter(min_samples_to_split)
+
+min_samples_in_leaf = UniformIntegerHyperparameter("min_samples_in_leaf", 1, 20, default=1)
+cs.add_hyperparameter(min_samples_in_leaf)
+
+max_depth = UniformIntegerHyperparameter("max_depth", 20, 100, default=20)
+cs.add_hyperparameter(max_depth)
+
+max_num_nodes = UniformIntegerHyperparameter("max_num_nodes", 100, 100000, default=1000)
+cs.add_hyperparameter(max_num_nodes)
+
+# example call of the function
+# it returns: Status, Cost, Runtime, Additional Infos
+def_value = taf.run(cs.get_default_configuration())[1]
+print("Default Value: %.2f" % (def_value))
+
+scenario = Scenario({"run_obj": "quality",  # we optimize quality (alternative runtime)
+                     "runcount-limit": 200,  # at most 200 function evaluations
+                     "instances": [[1]],  # Dummy
+                     "cs": cs  # configuration space
+                     },
+                    tae_runner=taf  # above defined target algorithm function
+                    )
+
+# logger = logging.getLogger("Optimizer") # Enable to show Debug outputs
+
+# necessary to use stats options related to scenario information
+Stats.scenario = scenario
+
+# Optimize
+smbo = SMBO(scenario=scenario, rng=np.random.RandomState(42))
+smbo.run(max_iters=999)
+
+Stats.print_stats()
+print("Final Incumbent: %s" % (smbo.incumbent))
+
+inc_value = taf.run(smbo.incumbent)[1]
+print("Optimized Value: %.2f" % (inc_value))
