a9310c7fbadf362495f31d563fd5a9d8f2775e76
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-03-16 18:15:11 +0100

    fixed braning example

--- test_files/example_branin.py
+++ test_files/example_branin.py
@@ -11,17 +11,20 @@ from robo.task.branin import Branin
 from robo.initial_design.init_random_uniform import init_random_uniform
 
 from smac.smbo.smbo import SMBO
+from smac.scenario.scenario import Scenario
 
 
 logging.basicConfig(level=logging.DEBUG)
+
+scenario = Scenario()
 
 task = Branin()
 X = init_random_uniform(task.X_lower, task.X_upper, 20)
 Y = task.evaluate(X)
 
 instance_features = np.array([[1]])
-smac = SMBO("/home/kleinaa/devel/git/smac3/test_files/params_branin.pcs",
-            instance_features=instance_features)
+
+smac = SMBO(scenario)
 new_x = smac.choose_next(X, Y)
 
 print new_x
