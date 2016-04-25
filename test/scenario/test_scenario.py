dc2abce6fb1adc42ea166f1f7d9c16cb141e48b4
biedenka <biedenka@informatik.uni-freiburg.de>
2016-04-25 09:37:14 +0200

    Test senario as dict

--- test/scenario/test_scenario.py
+++ test/scenario/test_scenario.py
@@ -15,6 +15,18 @@ class ScenarioTest(unittest.TestCase):
         logging.basicConfig()
         self.logger = logging.getLogger('ScenarioTest')
         self.logger.setLevel(logging.DEBUG)
+        
+        self.test_scenario_dict = {'algo': 'echo Hello',
+                                   'paramfile': '../../test_files/scenario_test/param.pcs',
+                                   'execdir': '.',
+                                   'deterministic': 0,
+                                   'run_obj': 'runtime',
+                                   'overall_obj': 'mean10',
+                                   'cutoff_time': 5,
+                                   'wallclock-limit': 18000,
+                                   'instance_file': '../../test_files/scenario_test/training.txt',
+                                   'test_instance_file': '../../test_files/scenario_test/test.txt',
+                                   'feature_file': '../../test_files/scenario_test/features.txt'}
 
     def test_Exception(self):
         with self.assertRaises(TypeError):
@@ -40,7 +52,23 @@ class ScenarioTest(unittest.TestCase):
         self.assertFalse(scenario.tae_runner is None)
 
     def test_dict_scenario(self):
-        pass
+        scenario = Scenario(self.test_scenario_dict)
+        
+        self.assertEquals(scenario.ta, ['echo', 'Hello'])
+        self.assertEquals(scenario.execdir, '.')
+        self.assertFalse(scenario.deterministic)
+        self.assertEquals(scenario.pcs_fn, '../../test_files/scenario_test/param.pcs')
+        self.assertEquals(scenario.overall_obj, 'mean10')
+        self.assertEquals(scenario.cutoff, 5.)
+        self.assertEquals(scenario.algo_runs_timelimit, np.inf)
+        self.assertEquals(scenario.wallclock_limit, 18000)
+        self.assertEquals(scenario.par_factor, 10)
+        self.assertEquals(scenario.train_insts, ['d', 'e', 'f'])
+        self.assertEquals(scenario.test_insts, ['a', 'b', 'c'])
+        test_dict = {'d' : 1, 'e' : 2, 'f' : 3}
+        self.assertEquals(scenario.feature_dict, test_dict)
+        self.assertEquals(scenario.feature_array[0], 1)
+        self.assertFalse(scenario.tae_runner is None)
 
 if __name__ == "__main__":
     unittest.main()
