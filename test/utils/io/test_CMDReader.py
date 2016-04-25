1cdb0d66e659e84c9f0cda606a4e744999377192
biedenka <biedenka@informatik.uni-freiburg.de>
2016-04-25 11:55:19 +0200

    Newlines in traj_logging for json_file, cmdreader tests and traj_logging tests

---
+++ test/utils/io/test_CMDReader.py
@@ -0,0 +1,38 @@
+'''
+Created on Apr 12, 2015
+
+@author: Andre Biedenkapp
+'''
+import unittest
+import logging
+import numpy as np
+
+from smac.utils.io.cmd_reader import CMDReader
+
+class TestArgs():
+    
+    def __init__(self, sf, seed, mi, vl):
+        self.scenario_file = sf
+        self.seed = seed
+        self.max_iterations = mi
+        self.verbose_level = vl
+
+class CMDReaderTest(unittest.TestCase):
+
+    def setUp(self):
+        logging.basicConfig()
+        self.logger = logging.getLogger('CMDReader Test')
+        self.logger.setLevel(logging.DEBUG)
+        self.cr = CMDReader()
+
+    def test_check_args_exception(self):  # Tests if the Exception is correctly raised
+        targs = TestArgs('.', 1234, 2, 'DEBUG')
+        with self.assertRaises(ValueError):
+            self.cr._check_args(targs)
+    
+    def test_check_args(self):  # Tests if no Exception is raised
+        targs = TestArgs('../../../test_files/scenario_test/scenario.txt', 1234, 2, 'DEBUG')
+        self.cr._check_args(targs)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/utils/io/test_traj_logging.py
@@ -0,0 +1,135 @@
+'''
+Created on Apr 25, 2015
+
+@author: Andre Biedenkapp
+'''
+import unittest
+import logging
+import json
+import os
+
+from mock import patch
+from smac.utils.io.traj_logging import TrajLogger
+
+class TrajLoggerTest(unittest.TestCase):
+
+    def mocked_get_used_wallclock_time(self):
+        self.value += 1
+        return self.value
+
+    def setUp(self):
+        logging.basicConfig()
+        self.logger = logging.getLogger('TrajLogger Test')
+        self.logger.setLevel(logging.DEBUG)
+        self.value = 0
+
+    def test_init(self):
+        TrajLogger(output_dir='./tmp_test_folder')
+        self.assertFalse(os.path.exists('smac3-output'))
+        self.assertTrue(os.path.exists('tmp_test_folder'))
+
+    @patch('smac.utils.io.traj_logging.Stats')
+    def test_add_entry(self,mock_stats):
+
+        tl = TrajLogger(output_dir='./tmp_test_folder')
+        test_config = {'param_a': 0.5,
+                       'param_b': 1,
+                       'param_c': 'value'}
+
+        mock_stats.ta_time_used = .5
+        mock_stats.get_used_wallclock_time = self.mocked_get_used_wallclock_time
+        mock_stats.ta_runs = 1
+
+        tl.add_entry(0.9, 1, test_config)
+
+        self.assertTrue(os.path.exists('tmp_test_folder/traj_old.csv'))
+        self.assertTrue(os.path.exists('tmp_test_folder/traj_aclib2.json'))
+
+        with open('tmp_test_folder/traj_old.csv') as to:
+            data = to.read().split('\n')[:-1]
+
+        header = data[0].split(',')
+        self.assertEquals(header[0], '"CPU Time Used"')
+        self.assertEquals(header[-1], '"Configuration..."')
+
+        data = map(lambda x: x.split(', '), data[1:])
+        frmt_str = '%1.6f'
+        self.assertEquals(frmt_str % .5, data[0][0])
+        self.assertEquals(frmt_str % .9, data[0][1])
+        self.assertEquals(frmt_str % 0.5, data[0][-4])
+        
+        with open('tmp_test_folder/traj_aclib2.json') as js:
+            json_dict = json.load(js)
+
+        self.assertEquals(json_dict['cpu_time'], .5)
+        self.assertEquals(json_dict['cost'], 0.9)
+        self.assertEquals(len(json_dict['incumbent']), 3)
+        self.assertTrue("param_a='0.5'" in json_dict['incumbent'])
+        self.assertTrue(json_dict['total_cpu_time'] != None)  # This will currently fail. TODO in traj_logging.py
+
+    @patch('smac.utils.io.traj_logging.Stats')
+    def test_add_multiple_entries(self, mock_stats):
+        tl = TrajLogger(output_dir='./tmp_test_folder')
+        
+        test_config = {'param_a': 0.5,
+                       'param_b': 1,
+                       'param_c': 'value'}
+        mock_stats.ta_time_used = 0.5
+        mock_stats.get_used_wallclock_time = self.mocked_get_used_wallclock_time
+        mock_stats.ta_runs = 1
+
+        tl.add_entry(0.9, 1, test_config)
+        
+        mock_stats.ta_runs = 2
+        mock_stats.ta_time_used = 0
+        tl.add_entry(1.3, 1, test_config)
+        
+        test_config['param_a'] = 0.
+        mock_stats.ta_time_used = 0
+        tl.add_entry(0.7, 2, test_config)
+
+        self.assertTrue(os.path.exists('tmp_test_folder/traj_old.csv'))
+        self.assertTrue(os.path.exists('tmp_test_folder/traj_aclib2.json'))
+
+        with open('tmp_test_folder/traj_old.csv') as to:
+            data = to.read().split('\n')[:-1]
+
+        header = data[0].split(',')
+        self.assertEquals(header[0], '"CPU Time Used"')
+        self.assertEquals(header[-1], '"Configuration..."')
+
+        data = map(lambda x: x.split(', '), data[1:])
+        frmt_str = '%1.6f'
+        self.assertEquals(frmt_str % 0.5, data[0][0])
+        self.assertEquals(frmt_str % 0.9, data[0][1])
+        self.assertEquals(frmt_str % 0.5, data[0][-4])
+
+        self.assertEquals(frmt_str % 0, data[1][0])
+        self.assertEquals(frmt_str % 1.3, data[1][1])
+        self.assertEquals(frmt_str % 3, data[1][-4])
+
+        self.assertEquals(frmt_str % 0, data[2][0])
+        self.assertEquals(frmt_str % .7, data[2][1])
+        self.assertEquals(frmt_str % 5, data[2][-4])
+
+        json_dicts = []
+        with open('tmp_test_folder/traj_aclib2.json') as js:
+            data = js.read().split('\n')[:-1]
+        
+        for d in data:
+            json_dicts.append(json.loads(d))
+
+        self.assertEquals(json_dicts[0]['cpu_time'], .5)
+        self.assertEquals(json_dicts[0]['cost'], 0.9)
+        self.assertEquals(len(json_dicts[0]['incumbent']), 3)
+        self.assertTrue("param_a='0.5'" in json_dicts[0]['incumbent'])
+        self.assertTrue(json_dicts[0]['total_cpu_time'] != None)  # This will currently fail. TODO in traj_logging.py
+
+    def tearDown(self):
+        os.remove('tmp_test_folder/traj_old.csv')
+        if os.path.exists('tmp_test_folder/traj_aclib2.json'):
+            os.remove('tmp_test_folder/traj_aclib2.json')
+        os.rmdir('tmp_test_folder')
+
+if __name__ == "__main__":
+    unittest.main()
