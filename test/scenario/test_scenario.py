53d42fd460ba0395529605c3c1ecef5baab852db
biedenka <biedenka@informatik.uni-freiburg.de>
2016-05-24 14:44:28 +0200

    Added not commmited tests

--- test/scenario/test_scenario.py
+++ test/scenario/test_scenario.py
@@ -17,28 +17,28 @@ class ScenarioTest(unittest.TestCase):
         self.logger.setLevel(logging.DEBUG)
         
         self.test_scenario_dict = {'algo': 'echo Hello',
-                                   'paramfile': '../../test_files/scenario_test/param.pcs',
+                                   'paramfile': 'test/test_files/scenario_test/param.pcs',
                                    'execdir': '.',
                                    'deterministic': 0,
                                    'run_obj': 'runtime',
                                    'overall_obj': 'mean10',
                                    'cutoff_time': 5,
                                    'wallclock-limit': 18000,
-                                   'instance_file': '../../test_files/scenario_test/training.txt',
-                                   'test_instance_file': '../../test_files/scenario_test/test.txt',
-                                   'feature_file': '../../test_files/scenario_test/features.txt'}
+                                   'instance_file': 'test/test_files/scenario_test/training.txt',
+                                   'test_instance_file': 'test/test_files/scenario_test/test.txt',
+                                   'feature_file': 'test/test_files/scenario_test/features.txt'}
 
     def test_Exception(self):
         with self.assertRaises(TypeError):
             s = Scenario(['a', 'b'])
     
     def test_string_scenario(self):
-        scenario = Scenario('../../test_files/scenario_test/scenario.txt')
+        scenario = Scenario('test/test_files/scenario_test/scenario.txt')
         
         self.assertEquals(scenario.ta, ['echo', 'Hello'])
         self.assertEquals(scenario.execdir, '.')
         self.assertFalse(scenario.deterministic)
-        self.assertEquals(scenario.pcs_fn, '../../test_files/scenario_test/param.pcs')
+        self.assertEquals(scenario.pcs_fn, 'test/test_files/scenario_test/param.pcs')
         self.assertEquals(scenario.overall_obj, 'mean10')
         self.assertEquals(scenario.cutoff, 5.)
         self.assertEquals(scenario.algo_runs_timelimit, np.inf)
@@ -49,7 +49,6 @@ class ScenarioTest(unittest.TestCase):
         test_dict = {'d' : 1, 'e' : 2, 'f' : 3}
         self.assertEquals(scenario.feature_dict, test_dict)
         self.assertEquals(scenario.feature_array[0], 1)
-        self.assertFalse(scenario.tae_runner is None)
 
     def test_dict_scenario(self):
         scenario = Scenario(self.test_scenario_dict)
@@ -57,7 +56,7 @@ class ScenarioTest(unittest.TestCase):
         self.assertEquals(scenario.ta, ['echo', 'Hello'])
         self.assertEquals(scenario.execdir, '.')
         self.assertFalse(scenario.deterministic)
-        self.assertEquals(scenario.pcs_fn, '../../test_files/scenario_test/param.pcs')
+        self.assertEquals(scenario.pcs_fn, 'test/test_files/scenario_test/param.pcs')
         self.assertEquals(scenario.overall_obj, 'mean10')
         self.assertEquals(scenario.cutoff, 5.)
         self.assertEquals(scenario.algo_runs_timelimit, np.inf)
@@ -68,7 +67,6 @@ class ScenarioTest(unittest.TestCase):
         test_dict = {'d' : 1, 'e' : 2, 'f' : 3}
         self.assertEquals(scenario.feature_dict, test_dict)
         self.assertEquals(scenario.feature_array[0], 1)
-        self.assertFalse(scenario.tae_runner is None)
 
 if __name__ == "__main__":
     unittest.main()
