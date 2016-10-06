404557af69979aaaae72198ec3b12a687195ca11
biedenka <biedenka@informatik.uni-freiburg.de>
2016-10-06 14:27:30 +0200

    MAINT improved docs

--- doc/conf.py
+++ doc/conf.py
@@ -220,7 +220,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {'**': ['localtoc.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
