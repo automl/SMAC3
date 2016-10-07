9e2a2359acd3afe2f070105fe087be8a000de8d5
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-10-07 09:39:37 +0200

    MAINT add version and authors to __init__ file

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 import setuptools
 
+import smac
+
 
 requirements = ['setuptools',
                 'numpy>=1.6.1',
@@ -11,8 +13,8 @@ requirements = ['setuptools',
 
 setuptools.setup(
     name="smac",
-    version="0.1.3",
-    author="Marius Lindauer, Matthias Feurer, Katharina Eggensperger, Aaron Klein, Stefan Falkner and Frank Hutter",
+    version=smac.VERSION,
+    author=smac.AUTHORS,
     author_email="fh@cs.uni-freiburg.de",
     description=("SMAC3, a Python implementation of 'Sequential Model-based "
                  "Algorithm Configuration'."),
