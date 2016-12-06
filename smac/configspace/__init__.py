b9840e7ce21f0be43d2e58338a0495dcfcc2725b
Marius Lindauer <marius.rks@googlemail.com>
2016-12-06 14:27:33 +0100

    ADD reading of pcs file with new format

--- smac/configspace/__init__.py
+++ smac/configspace/__init__.py
@@ -1,3 +1,3 @@
 from ConfigSpace import ConfigurationSpace, Configuration
-from ConfigSpace.io import pcs
+from ConfigSpace.io import pcs, pcs_new
 from ConfigSpace.util import impute_inactive_values, get_random_neighbor, get_one_exchange_neighbourhood
