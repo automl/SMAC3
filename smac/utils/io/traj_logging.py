755224a25d2495188dc1f43820cdd6a7ff8630bb
Joshua Marben <joshua.marben@neptun.uni-freiburg.de>
2017-02-22 19:34:09 +0100

    ADD traj_logger writes stats.ta_runs to aclib/old_traj-files

--- smac/utils/io/traj_logging.py
+++ smac/utils/io/traj_logging.py
@@ -63,7 +63,7 @@ class TrajLogger(object):
             if not os.path.isfile(self.old_traj_fn):
                 with open(self.old_traj_fn, "w") as fp:
                     fp.write(
-                        '"CPU Time Used","Estimated Training Performance","Wallclock Time","Incumbent ID","Automatic Configurator (CPU) Time","Configuration..."\n')
+                        '"CPU Time Used","Estimated Training Performance","Wallclock Time","Incumbent ID","Automatic Configurator (CPU) Time","Algorithm Runs","Configuration..."\n')
 
             self.aclib_traj_fn = os.path.join(output_dir, "traj_aclib2.json")
 
@@ -90,12 +90,12 @@ class TrajLogger(object):
                                 ta_runs, ta_time_used, wallclock_time))
         if self.output_dir is not None:
             self._add_in_old_format(train_perf, incumbent_id, incumbent,
-                                    ta_time_used, wallclock_time)
+                                    ta_runs, ta_time_used, wallclock_time)
             self._add_in_aclib_format(train_perf, incumbent_id, incumbent,
-                                      ta_time_used, wallclock_time)
+                                      ta_runs, ta_time_used, wallclock_time)
 
     def _add_in_old_format(self, train_perf, incumbent_id, incumbent,
-                           ta_time_used, wallclock_time):
+                           ta_runs, ta_time_used, wallclock_time):
         """
             adds entries to old SMAC2-like trajectory file
 
@@ -107,6 +107,8 @@ class TrajLogger(object):
                 id of incumbent
             incumbent: Configuration()
                 current incumbent configuration
+            ta_runs: int
+                Runs performed by the targed algorithm
             ta_time_used: float
                 CPU time used by the target algorithm
             wallclock_time: float
@@ -119,17 +121,18 @@ class TrajLogger(object):
                 conf.append("%s='%s'" % (p, repr(incumbent[p])))
 
         with open(self.old_traj_fn, "a") as fp:
-            fp.write("%f, %f, %f, %d, %f, %s\n" % (
+            fp.write("%f, %f, %f, %d, %f, %d, %s\n" % (
                 ta_time_used,
                 train_perf,
                 wallclock_time,
                 incumbent_id,
                 wallclock_time - ta_time_used,
+                ta_runs,
                 ", ".join(conf)
             ))
 
     def _add_in_aclib_format(self, train_perf, incumbent_id, incumbent,
-                             ta_time_used, wallclock_time):
+                             ta_runs, ta_time_used, wallclock_time):
         """
             adds entries to AClib2-like trajectory file
 
@@ -141,6 +144,8 @@ class TrajLogger(object):
                 id of incumbent
             incumbent: Configuration()
                 current incumbent configuration
+            ta_runs: int
+                Runs performed by the targed algorithm
             ta_time_used: float
                 CPU time used by the target algorithm
             wallclock_time: float
@@ -152,7 +157,8 @@ class TrajLogger(object):
             if not incumbent[p] is None:
                 conf.append("%s='%s'" % (p, repr(incumbent[p])))
 
-        traj_entry = {"cpu_time": ta_time_used,
+        traj_entry = {"algo_runs" : ta_runs,
+                      "cpu_time": ta_time_used,
                       "total_cpu_time": None,  # TODO: fix this
                       "wallclock_time": wallclock_time,
                       "evaluations": self.stats.ta_runs,
@@ -180,7 +186,8 @@ class TrajLogger(object):
             -------
             trajectory: list
                 each entry in the list is a dictionary of the form
-                {"cpu_time": float,
+                {"algo_runs" : int,
+                 "cpu_time": float,
                  "total_cpu_time": None, # TODO
                  "wallclock_time": float,
                  "evaluations": int
