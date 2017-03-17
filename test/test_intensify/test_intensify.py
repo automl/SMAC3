4a6e193ecfafc16bc8038a6ac95a468c101e89ce
Marius Lindauer <marius.rks@googlemail.com>
2017-03-17 15:40:26 +0100

    FIX adaptive capping with re-occurring configurations
    * ADD new StatusType.CAPPED
    * CAPPED runs are added to runhistory in data but not in the remaining
    fast data structures as used in intensification
    * MAINT improve corresponding unit test

--- test/test_intensify/test_intensify.py
+++ test/test_intensify/test_intensify.py
@@ -234,15 +234,7 @@ class TestIntensify(unittest.TestCase):
 
         # self.assertTrue(False)
         self.assertEqual(inc, self.config1)
-        self.assertLess(
-            self.rh.get_cost(self.config2), 2, self.rh.get_cost(self.config2))
 
-        # get data for config2 to check that the correct run was performed
-        run = self.rh.get_runs_for_config(self.config2)[0]
-        config_id = self.rh.config_ids[self.config2]
-        self.assertEqual(run.instance, 1, run.instance)
-        self.assertEqual(run.seed, 12345, run.seed)
-        
     def test_race_challenger_3(self):
         '''
            test _race_challenger with adaptive capping on a previously capped configuration  
@@ -250,18 +242,18 @@ class TestIntensify(unittest.TestCase):
 
         def target(config: Configuration, seed: int, instance: str):
             if instance == 1:
-                time.sleep(2000)
+                time.sleep(2.1)
             else:
                 time.sleep(0.6)
             return (config['a'] + 1) / 1000.
-        taf = ExecuteTAFuncDict(ta=target, stats=self.stats, run_obj="runtime", par_factor=10)
+        taf = ExecuteTAFuncDict(ta=target, stats=self.stats, run_obj="runtime", par_factor=1)
         taf.runhistory = self.rh
 
         intensifier = Intensifier(
             tae_runner=taf, stats=self.stats,
             traj_logger=TrajLogger(output_dir=None, stats=self.stats),
             rng=np.random.RandomState(12345),
-            cutoff=200,
+            cutoff=2,
             instances=[1])
 
         self.rh.add(config=self.config1, cost=0.5, time=.5,
@@ -279,7 +271,7 @@ class TestIntensify(unittest.TestCase):
         self.assertEqual(inc, self.config1)
         
         # further run for incumbent
-        self.rh.add(config=self.config1, cost=200, time=200,
+        self.rh.add(config=self.config1, cost=2, time=2,
                     status=StatusType.TIMEOUT, instance_id=2,
                     seed=12345,
                     additional_info=None)
@@ -292,8 +284,10 @@ class TestIntensify(unittest.TestCase):
         
         # the incumbent should still be config1 because
         # config2 should get on inst 1 a full timeout
-        # such that c(config1) = 100.25 and c(config2) = 100.3
-        self.assertEqual(inc, self.config1, msg)
+        # such that c(config1) = 1.25 and c(config2) close to 1.3
+        self.assertEqual(inc, self.config1)
+        # the capped run should not be counted in runs_perf_config
+        self.assertAlmostEqual(self.rh.runs_per_config[2], 2)
 
     def test_race_challenger_large(self):
         '''
