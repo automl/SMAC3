7cb946dc6ff8f79febae5aef033bc213a7cd825b
mlindauer <marius.rks@googlemail.com>
2017-03-22 10:25:45 +0100

    RM unused import in configspace module

--- smac/configspace/__init__.py
+++ smac/configspace/__init__.py
@@ -1,5 +1,4 @@
 from ConfigSpace import ConfigurationSpace, Configuration
 from ConfigSpace.io import pcs, pcs_new
-from ConfigSpace.util import impute_inactive_values, get_random_neighbor, get_one_exchange_neighbourhood
 from ConfigSpace.util import get_random_neighbor, get_one_exchange_neighbourhood
 from smac.configspace.util import convert_configurations_to_array
