2f7c1ae812a707da2324c5678a803b4afc23a547
biedenka <biedenka@informatik.uni-freiburg.de>
2017-05-31 21:44:42 +0200

    MAINT correct handling of ordinals

--- smac/utils/util_funcs.py
+++ smac/utils/util_funcs.py
@@ -18,8 +18,8 @@ def get_types(config_space, instance_features=None):
 
         elif isinstance(param, (OrdinalHyperparameter)):
             n_cats = len(param.sequence)
-            types[i] = n_cats
-            bounds[i] = (int(n_cats), np.nan)
+            types[i] = 0
+            bounds[i] = (0, int(n_cats) - 1)
 
         elif isinstance(param, Constant):
             # for constants we simply set types to 0
