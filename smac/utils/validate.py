a5c533613a498c4fc73899a8614f5cca3a22ef27
shukon <jo.ma@posteo.de>
2018-01-19 15:08:28 +0100

    MAINT fix #377 validation epm without features

--- smac/utils/validate.py
+++ smac/utils/validate.py
@@ -308,13 +308,14 @@ class Validator(object):
 
         # Predict desired runs
         runs, rh_epm = self.get_runs(config_mode, instance_mode, repetitions, runhistory)
-        try:
+        if hasattr(self.scen, "feature_array") and self.scen.feature_array is not None:
             feature_array_size = len(self.scen.cs.get_hyperparameters()) + self.scen.feature_array.shape[1]
-        except AttributeError:
+        else:
             feature_array_size = len(self.scen.cs.get_hyperparameters())
         X_pred = np.empty((len(runs), feature_array_size))
         for idx, run in enumerate(runs):
-            if hasattr(self.scen, "feature_dict") and run.inst != None:
+            if (hasattr(self.scen, "feature_array") and
+                    self.scen.feature_array is not None and run.inst != None):
                 X_pred[idx] = np.hstack([convert_configurations_to_array([run.config])[0],
                                          self.scen.feature_dict[run.inst]])
             else:
