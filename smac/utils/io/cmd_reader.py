04e4d9f7e283318c02e1a575816fe9b902c37519
Aaron Kimmig <kimmiga@informatik.uni-freiburg.de>
2018-02-07 07:31:01 +0100

    issue #374: Don't print options prefixed with '--dev' in standard --help but only in --help-all

--- smac/utils/io/cmd_reader.py
+++ smac/utils/io/cmd_reader.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, SUPPRESS
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, SUPPRESS, Action
 
 __author__ = "Marius Lindauer"
 __copyright__ = "Copyright 2015, ML4AAD"
@@ -10,8 +10,68 @@ __email__ = "lindauer@cs.uni-freiburg.de"
 __version__ = "0.0.1"
 
 
-class CMDReader(object):
+help_type = "standard"
 
+
+class ConfigurableHelpFormatter(ArgumentDefaultsHelpFormatter):
+    """
+    Configurable Help Formatter. Can filter out developer options.
+    """
+
+    def __init(self, *args, **kwargs):
+        super(ConfigurableHelpFormatter, self).__init__(*args, **kwargs)
+
+    def _add_item(self, func, args):
+        if help_type == 'standard':
+            if func.__name__ == '_format_usage':
+                filtered_actions = []
+                for arg in args[1]:
+                    if isinstance(arg, Action):
+                        dev = False
+                        for s in arg.option_strings:
+                            if s.startswith('--dev'):
+                                dev = True
+                                break
+                        if not dev:
+                            filtered_actions.append(arg)
+                args = (args[0], filtered_actions, args[2], args[3])
+            elif isinstance(args, list):
+                for arg in args:
+                    if isinstance(arg, Action):
+                        for s in arg.option_strings:
+                            if s.startswith('--dev'):
+                                return
+        self._current_section.items.append((func, args))
+
+
+class StandardHelpAction(Action):
+    """Action to only show standard options in help message"""
+
+    def __init__(self, *args, **kwargs):
+        super(StandardHelpAction, self).__init__(*args, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        global help_type
+        help_type = 'standard'
+        parser.print_help()
+        parser.exit()
+
+
+class DevHelpAction(Action):
+    """Action to show standard and developer options in help message"""
+
+    def __init__(self, *args, **kwargs):
+        super(DevHelpAction, self).__init__(*args, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        global help_type
+        help_type = 'dev'
+        parser.print_help()
+        parser.exit()
+
+
+class CMDReader(object):
+    
     """Use argparse to parse command line options
 
     Attributes
@@ -31,12 +91,17 @@ class CMDReader(object):
             args_: parsed arguments; return of parse_args of ArgumentParser
         """
 
-        parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+        parser = ArgumentParser(formatter_class=ConfigurableHelpFormatter, add_help=False)
         req_opts = parser.add_argument_group("Required Options")
         req_opts.add_argument("--scenario_file", required=True,
                               help="scenario file in AClib format")
 
+        # let a help message begin with "[dev]" to add a developer option
         req_opts = parser.add_argument_group("Optional Options")
+        req_opts.add_argument("--help", action=StandardHelpAction, default=SUPPRESS,
+                              help="Show help messages for standard options.")
+        req_opts.add_argument("--help-all", action=DevHelpAction, default=SUPPRESS,
+                              help="Show help messages for both standard and developer options.")
         req_opts.add_argument("--seed", default=1, type=int,
                               help="random seed")
         req_opts.add_argument("--verbose_level", default=logging.INFO,
@@ -59,7 +124,8 @@ class CMDReader(object):
                               nargs="*",
                               help=SUPPRESS)# list of trajectory dump files, 
                                             # reads runhistory 
-                                            # and uses final incumbent as challenger 
+                                            # and uses final incumbent as challenger
+        req_opts.add_argument("--dev_test", action="store_true", help="[dev] Developer test option")
 
         args_, misc = parser.parse_known_args()
         self._check_args(args_)
