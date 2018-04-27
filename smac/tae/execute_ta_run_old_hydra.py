17891bf8a033fc6e50caf1f3d1177b7000cbc074
biedenka <biedenka@informatik.uni-freiburg.de>
2018-04-27 13:34:04 +0200

    MAINT hydra tae

--- smac/tae/execute_ta_run_old_hydra.py
+++ smac/tae/execute_ta_run_old_hydra.py
@@ -1,6 +1,7 @@
 import typing
 
 from smac.tae.execute_ta_run_old import ExecuteTARunOld
+from smac.tae.execute_ta_run_old import StatusType
 
 __copyright__ = "Copyright 2018, ML4AAD"
 __license__ = "3-clause BSD"
@@ -12,7 +13,9 @@ class ExecuteTARunOldHydra(ExecuteTARunOld):
     """Returns min(cost, cost_portfolio)
     """
     
-    def __init__(self, cost_oracle:typing.Mapping[str,float], **kwargs):
+    def __init__(self,
+                 cost_oracle: typing.Mapping[str, float],
+                 **kwargs):
         '''
             Constructor
             
@@ -28,19 +31,19 @@ class ExecuteTARunOldHydra(ExecuteTARunOld):
     def run(self, **kwargs):
         """ see ~smac.tae.execute_ta_run.ExecuteTARunOld for docstring
         """
-        status, cost, runtime, additional_info = super(ExecuteTARunOldHydra,self).run(**kwargs)
+        status, cost, runtime, additional_info = super(ExecuteTARunOldHydra, self).run(**kwargs)
         inst = kwargs["instance"]
         oracle_perf = self.cost_oracle.get(inst)
         if oracle_perf is not None:
-            # at this point, cost is not overwritten by runtime
-            #TODO: This could be problematic for PAR10?
             if self.run_obj == "runtime":
-                self.logger.debug("Portfolio perf: %f vs %f = %f" %(oracle_perf, runtime, min(oracle_perf,runtime)))
-                runtime = min(oracle_perf,runtime)
+                self.logger.debug("Portfolio perf: %f vs %f = %f", oracle_perf, runtime, min(oracle_perf, runtime))
+                runtime = min(oracle_perf, runtime)
+                cost = runtime
             else:
-                self.logger.debug("Portfolio perf: %f vs %f = %f" %(oracle_perf, cost, min(oracle_perf,cost)))
-                cost = min(oracle_perf,cost)
-            #TODO: update status?
+                self.logger.debug("Portfolio perf: %f vs %f = %f", oracle_perf, cost, min(oracle_perf, cost))
+                cost = min(oracle_perf, cost)
+            if oracle_perf < kwargs['cutoff'] and status is StatusType.TIMEOUT:
+                status = StatusType.SUCCESS
         else:
             self.logger.error("Oracle performance missing --- should not happen")
 
