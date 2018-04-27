b46584f063d65aa6be2bc8c59cc00d7057a86456
biedenka <biedenka@informatik.uni-freiburg.de>
2018-04-27 10:21:35 +0200

    FIX missing attributes

--- smac/facade/hydra_facade.py
+++ smac/facade/hydra_facade.py
@@ -30,7 +30,7 @@ class Hydra(object):
 
     def __init__(self,
                  scenario: Scenario,
-                 n_iterations:int,
+                 n_iterations: int,
                  **kwargs):
         """Constructor
 
@@ -41,18 +41,19 @@ class Hydra(object):
         n_iterations: int,
             number of Hydra iterations
         """
-        
+
         self.logger = logging.getLogger(
             self.__module__ + "." + self.__class__.__name__)
-        
+
         self.n_iterations = n_iterations
 
         self.logger = logging.getLogger(
             self.__module__ + "." + self.__class__.__name__)
-        
+
         self.scenario = scenario
         self.kwargs = kwargs
-        
+        self.output_dir = None
+        self.solver = None
 
     def optimize(self):
         """Optimizes the algorithm provided in scenario (given in constructor)
@@ -64,29 +65,29 @@ class Hydra(object):
         """
 
         portfolio = []
-        smac = SMAC(scenario=self.scenario, **self.kwargs)
+        self.solver = SMAC(scenario=self.scenario, **self.kwargs)
         portfolio_cost = np.inf
         for i in range(self.n_iterations):
-            self.logger.info("Iteration: %d" %(i+1))
-            
-            incumbent = smac.solver.run()
-            smac.stats.print_stats()
-            self.logger.info("Incumbent of %d-th Iteration" %(i+1))
+            self.logger.info("Iteration: %d" % (i + 1))
+
+            incumbent = self.solver.solver.run()
+            self.solver.stats.print_stats()
+            self.logger.info("Incumbent of %d-th Iteration" % (i + 1))
             self.logger.info(incumbent)
             portfolio.append(incumbent)
-            
+
             if self.output_dir is not None:
                 self.solver.runhistory.save_json(
                     fn=os.path.join(self.output_dir, "runhistory.json")
                 )
-            
+
             # validate incumbent on all trainings instances
-            new_rh = smac.validate(config_mode='inc', 
-                                  instance_mode='train', 
-                                  repetitions=1, 
-                                  use_epm=False, 
-                                  n_jobs=1)
-            self.logger.info("Number of validated runs: %d" %(len(new_rh.data)))
+            new_rh = self.solver.validate(config_mode='inc',
+                                          instance_mode='train',
+                                          repetitions=1,
+                                          use_epm=False,
+                                          n_jobs=1)
+            self.logger.info("Number of validated runs: %d" % (len(new_rh.data)))
             # since the TAE uses already the portfolio as an upper limit
             # the following dict already contains oracle performance
             self.logger.info("Start validation of current portfolio")
@@ -94,18 +95,17 @@ class Hydra(object):
 
             cur_portfolio_cost = np.mean(list(cost_per_inst.values()))
             if portfolio_cost <= cur_portfolio_cost:
-                self.logger.info("No further progress (%f) --- terminate hydra" %(portfolio_cost))
+                self.logger.info("No further progress (%f) --- terminate hydra" % (portfolio_cost))
                 break
             else:
                 portfolio_cost = cur_portfolio_cost
-                self.logger.info("Current pertfolio cost: %f" %(portfolio_cost))
-            
+                self.logger.info("Current pertfolio cost: %f" % (portfolio_cost))
+
             # modify TAE such that it return oracle performance
-            #TODO: This only works for the old command line interface
+            # TODO: This only works for the old command line interface
             tae = ExecuteTARunOldHydra(ta=self.scenario.ta, run_obj=self.scenario.run_obj,
                                        cost_oracle=cost_per_inst)
-            
-            smac = SMAC(scenario=self.scenario, tae_runner=tae, **self.kwargs)
-            
-        return portfolio
 
+            smac = SMAC(scenario=self.scenario, tae_runner=tae, **self.kwargs)
+
+        return portfolio
