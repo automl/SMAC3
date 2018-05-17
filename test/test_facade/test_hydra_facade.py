9f7b2c28111bec573ee2e272724ff0b5b4f8eaef
biedenka <biedenka@informatik.uni-freiburg.de>
2018-05-17 11:24:40 +0200

    FIX hydra-mip-psmac test

--- test/test_facade/test_hydra_facade.py
+++ test/test_facade/test_hydra_facade.py
@@ -7,57 +7,36 @@ from unittest.mock import patch
 
 import numpy as np
 
-from smac.facade.hydra_facade import Hydra, SMAC, PSMAC
-from smac.optimizer.smbo import SMBO
-from smac.runhistory.runhistory import RunHistory
-from smac.runhistory.runhistory import DataOrigin
-from smac.optimizer.objective import average_cost
+from smac.facade.hydra_facade import Hydra, PSMAC
+from smac.utils.io.output_writer import OutputWriter
 from smac.scenario.scenario import Scenario
 
 
-class MockSMBO(SMBO):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.stats.start_timing()
-        MockSMBO.calls = 10
-
-    def run(self):  # mock call such that we don't have to test with real algorithm calls
-        MockSMBO.calls -= 1
-        return MockSMBO.calls
-
-
-class MockSMAC(SMAC):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def validate(self, **kwargs):
-        # randomly populate the runhistory
-        rh = RunHistory(average_cost)
-        smbo_calls = self.solver.calls
-        config = self.scenario.cs.sample_configuration(1)
-        rh.add(config, smbo_calls, smbo_calls, 'SAT', instance_id=str(np.random.choice(['a', 'b', 'c'])),
-               seed=np.random.randint(999), origin=DataOrigin.INTERNAL)
-        rh.add(config, smbo_calls, smbo_calls - 1, 'SAT', instance_id=str(np.random.choice(['a', 'b', 'c'])),
-               seed=np.random.randint(999), origin=DataOrigin.INTERNAL)
-        rh.add(config, smbo_calls, smbo_calls - 2, 'SAT', instance_id=str(np.random.choice(['a', 'b', 'c'])),
-               seed=np.random.randint(999), origin=DataOrigin.INTERNAL)
-        rh.add(config, smbo_calls, smbo_calls - 1, 'SAT', instance_id=str(np.random.choice(['a', 'b', 'c'])),
-               seed=np.random.randint(999), origin=DataOrigin.INTERNAL)
-        rh.add(config, smbo_calls, smbo_calls - 2, 'SAT', instance_id=str(np.random.choice(['a', 'b', 'c'])),
-               seed=np.random.randint(999), origin=DataOrigin.INTERNAL)
-        return rh
+MOCKCALLS = 0
 
 
 class MockPSMAC(PSMAC):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.ow = OutputWriter()
+        self.ow.write_scenario_file(self.scenario)
+        global MOCKCALLS
+        MOCKCALLS += 1
 
     def optimize(self):
         return np.array(self.scenario.cs.sample_configuration(self.n_optimizers))
 
     def get_best_incumbents_ids(self, incs):
-        # cost_per_conf_v, val_ids, cost_per_conf_e, est_ids = self.optimizer.get_best_incumbents_ids(incs)
-        pass
+        cost_per_conf_v = cost_per_conf_e = {}
+        val_ids = est_ids = list(range(len(incs)))
+        global MOCKCALLS
+        for inc in incs:
+            # in successive runs will always be smaller -> hydra doesn't terminate early
+            cost_per_conf_v[inc] = cost_per_conf_e[inc] = {inst: max(100 - MOCKCALLS,
+                                                                     0) for inst in self.scenario.train_insts}
+        if not self.validate:
+            cost_per_conf_v = val_ids = None
+        return cost_per_conf_v, val_ids, cost_per_conf_e, est_ids
 
 
 class TestHydraFacade(unittest.TestCase):
@@ -68,12 +47,17 @@ class TestHydraFacade(unittest.TestCase):
         fn = os.path.join(os.path.dirname(__file__), '../test_files/spear_hydra_test_scenario.txt')
         self.scenario = Scenario(fn)
 
-    @patch('smac.facade.smac_facade.SMBO', new=MockSMBO)
-    @patch('smac.facade.hydra_facade.SMAC', new=MockSMAC)
+    @patch('smac.facade.hydra_facade.PSMAC', new=MockPSMAC)
     def test_hydra(self):
         optimizer = Hydra(self.scenario, n_iterations=3)
         portfolio = optimizer.optimize()
         self.assertEqual(len(portfolio), 3)
+
+    @patch('smac.facade.hydra_facade.PSMAC', new=MockPSMAC)
+    def test_hydra_mip(self):
+        optimizer = Hydra(self.scenario, n_iterations=3, incs_per_round=2)
+        portfolio = optimizer.optimize()
+        self.assertEqual(len(portfolio), 6)
         
     def tearDown(self):
         hydras = glob.glob1('.', 'hydra*')
