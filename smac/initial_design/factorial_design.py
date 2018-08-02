e009c41ed59a9855bc42158cdeaa29f6d0face38
Marius Lindauer <marius.rks@googlemail.com>
2018-08-02 17:16:42 +0200

    ADD default and midpoint in factorial design

--- smac/initial_design/factorial_design.py
+++ smac/initial_design/factorial_design.py
@@ -51,20 +51,33 @@ class FactorialInitialDesign(MultiConfigInitialDesign):
         params = cs.get_hyperparameters()
         
         values = [] 
+        mid = []
         for param in params:
             if isinstance(param, Constant):
                 v = [param.value]
+                mid.append(param.value)
             elif isinstance(param, NumericalHyperparameter):
                 v = [param.lower, param.upper]
+                mid.append(np.average([param.lower, param.upper]))
             elif isinstance(param, CategoricalHyperparameter):
                 v = list(param.choices)
+                mid.append(param.choices[0])
             elif isinstance(param, OrdinalHyperparameter):
                 v = [param.sequence[0], param.sequence[-1]]
+                l = len(param.sequence)
+                mid.append(param.sequence[int(l/2)])
             values.append(v)
+            
         factorial_design = itertools.product(*values)
         
         self.logger.debug("Initial Design")
-        configs = []
+        configs = [cs.get_default_configuration()]
+        # add middle point in space
+        conf_dict = dict([(p.name,v) for p,v in zip(params,mid)])
+        middle_conf = deactivate_inactive_hyperparameters(conf_dict, cs)
+        configs.append(middle_conf)
+        
+        # add corner points
         for design in factorial_design:
             conf_dict = dict([(p.name,v) for p,v in zip(params,design)])
             conf = deactivate_inactive_hyperparameters(conf_dict, cs)
