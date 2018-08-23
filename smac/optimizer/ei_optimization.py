9d7b3e1e97fc39506cd21288e5f1c1f7383d7b77
Marius Lindauer <marius.rks@googlemail.com>
2018-08-23 10:02:36 +0200

    ADD DifferentialEvolutionSolver for EI optimization

--- smac/optimizer/ei_optimization.py
+++ smac/optimizer/ei_optimization.py
@@ -317,6 +317,73 @@ class LocalSearch(AcquisitionFunctionMaximizer):
 
         return acq_val_incumbent, incumbent
 
+class DiffOpt(AcquisitionFunctionMaximizer):
+    """Get candidate solutions via DifferentialEvolutionSolvers.
+
+    Parameters
+    ----------
+    acquisition_function : ~smac.optimizer.acquisition.AbstractAcquisitionFunction
+
+    config_space : ~smac.configspace.ConfigurationSpace
+
+    rng : np.random.RandomState or int, optional
+    """
+
+    def _maximize(
+            self,
+            runhistory: RunHistory,
+            stats: Stats,
+            num_points: int,
+            _sorted: bool=False,
+            **kwargs
+    ) -> List[Tuple[float, Configuration]]:
+        """DifferentialEvolutionSolver
+
+        Parameters
+        ----------
+        runhistory: ~smac.runhistory.runhistory.RunHistory
+            runhistory object
+        stats: ~smac.stats.stats.Stats
+            current stats object
+        num_points: int
+            number of points to be sampled
+        _sorted: bool
+            whether random configurations are sorted according to acquisition function
+        **kwargs
+            not used
+
+        Returns
+        -------
+        iterable
+            An iterable consistng of
+            tuple(acqusition_value, :class:`smac.configspace.Configuration`).
+        """
+
+
+        from scipy.optimize._differentialevolution import DifferentialEvolutionSolver
+        configs = []
+
+        def func(x):
+            return -self.acquisition_function([Configuration(self.config_space, vector=x)])
+
+        ds = DifferentialEvolutionSolver(func, bounds=[[0, 1], [0, 1]], args=(),
+                                    strategy='best1bin', maxiter=1000,
+                                    popsize=50, tol=0.01,
+                                    mutation=(0.5, 1),
+                                    recombination=0.7,
+                                    seed=self.rng.randint(1000), polish=True,
+                                    callback=None,
+                                    disp=False, init='latinhypercube', atol=0)
+
+        rval = ds.solve()
+        for pop, val in zip(ds.population, ds.population_energies):
+            rc = Configuration(self.config_space, vector=pop)
+            rc.origin = 'DifferentialEvolution'
+            configs.append((-val, rc))
+
+        configs.sort(key=lambda t: t[0])
+        configs.reverse()
+        return configs
 
 class RandomSearch(AcquisitionFunctionMaximizer):
     """Get candidate solutions via random sampling of configurations.
@@ -425,6 +492,14 @@ class InterleavedLocalAndRandomSearch(AcquisitionFunctionMaximizer):
             n_steps_plateau_walk=n_steps_plateau_walk
         )
         self.n_sls_iterations = n_sls_iterations
+
+        #=======================================================================
+        # self.local_search = DiffOpt(
+        #     acquisition_function=acquisition_function,
+        #     config_space=config_space,
+        #     rng=rng
+        # )
+        #=======================================================================
 
     def maximize(
             self,
