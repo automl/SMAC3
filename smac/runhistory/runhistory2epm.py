FIX perc scaling

smac/runhistory/runhistory2epm.py
@@ -396,7 +396,7 @@ class RunHistory2EPM4ScaledCost(RunHistory2EPM4Cost):
 
         if y.size > 0:
             perc = np.percentile(y, 5)
-            min_y = np.min(y) - perc # ensure that scaled y cannot be 0
+            min_y = 2 * np.min(y) - perc # ensure that scaled y cannot be 0
             max_y = np.max(y)
             # linear scaling
             y = (y - min_y) / (max_y - min_y)
@@ -433,7 +433,7 @@ class RunHistory2EPM4InvScaledCost(RunHistory2EPM4Cost):
 
         if y.size > 0:
             perc = np.percentile(y, 5)
-            min_y = np.min(y) - perc # ensure that scaled y cannot be 0
+            min_y = 2 * np.min(y) - perc # ensure that scaled y cannot be 0
             max_y = np.max(y)
             # linear scaling
             y = (y - min_y) / (max_y - min_y)
@@ -471,7 +471,7 @@ class RunHistory2EPM4SqrtScaledCost(RunHistory2EPM4Cost):
 
         if y.size > 0:
             perc = np.percentile(y, 5)
-            min_y = np.min(y) - perc # ensure that scaled y cannot be 0
+            min_y = 2 * np.min(y) - perc # ensure that scaled y cannot be 0
             max_y = np.max(y)
             # linear scaling
             y = (y - min_y) / (max_y - min_y)
@@ -509,7 +509,7 @@ class RunHistory2EPM4LogScaledCost(RunHistory2EPM4Cost):
 
         if y.size > 0:
             perc = np.percentile(y, 5)
-            min_y = np.min(y) - perc # ensure that scaled y cannot be 0
+            min_y = 2 * np.min(y) - perc # ensure that scaled y cannot be 0
             max_y = np.max(y)
             # linear scaling
             y = (y - min_y) / (max_y - min_y)