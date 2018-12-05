5065ff050ce4dc3f922b45fc6d9456de4b3b72dd
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2018-12-05 14:13:54 +0100

    ADD new scoring function for model and acquisition function selection

---
+++ smac/optimizer/score.py
@@ -0,0 +1,112 @@
+from typing import Tuple, Callable
+
+from ConfigSpace import Configuration
+import numpy as np
+from scipy.stats import spearmanr
+# TODO replace by group shuffle split to take instances into account?
+from sklearn.model_selection import ShuffleSplit
+
+from smac.epm.base_epm import AbstractEPM
+from smac.epm.random_epm import RandomEPM
+from smac.optimizer.acquisition import AbstractAcquisitionFunction, EI, LogEI
+from smac.runhistory.runhistory import RunHistory
+from smac.runhistory.runhistory2epm import AbstractRunHistory2EPM
+
+
+def spearman(X, acq, model, runhistory2EPM, test_indices, train_indices, y):
+    X_train = X[train_indices]
+    y_train = y[train_indices]
+    X_test = X[test_indices]
+    y_test = y[test_indices]
+
+    model.train(X_train, y_train)
+    acq.update(model=model, eta=np.min(y_train))
+    configs = [Configuration(runhistory2EPM.scenario.cs, vector=x) for x in X_test]
+    acquivals = acq(configurations=configs).flatten()
+    return 1 - spearmanr(y_test, -acquivals)[0]
+
+
+def two_step_lookback_bo(X, acq, model, runhistory2EPM, test_indices, train_indices, y):
+    X_train = X[train_indices]
+    y_train = y[train_indices]
+    X_test = X[test_indices]
+    y_test = y[test_indices]
+
+    losses_for_split = []
+    model.train(X_train, y_train)
+    acq.update(model=model, eta=np.min(y_train))
+    configs = [Configuration(runhistory2EPM.scenario.cs, vector=x) for x in X_test]
+    acquivals = acq(configurations=configs).flatten()
+    argmax = np.nanargmax(acquivals)
+    x = X_test[argmax]
+    y_ = y_test[argmax]
+    losses_for_split.append(y_)
+    X_test = np.delete(X_test, argmax, axis=0)
+    y_test = np.delete(y_test, argmax, axis=0)
+    X_train = np.concatenate((X_train, x.reshape((1, -1))), axis=0)
+    y_train = np.concatenate((y_train, y_.reshape((1, 1))), axis=0)
+    model.train(X_train, y_train)
+    acq.update(model=model, eta=np.min(y_train))
+    configs = [Configuration(runhistory2EPM.scenario.cs, vector=x) for x in X_test]
+    acquivals = acq(configurations=configs).flatten()
+    argmax = np.nanargmax(acquivals)
+    y_ = y_test[argmax]
+    losses_for_split.append(y_)
+    l = np.min(losses_for_split)
+    return l
+
+
+def great_new_selection_function(
+    runhistory: RunHistory,
+    runhistory2EPM: AbstractRunHistory2EPM,
+    default_model: AbstractEPM,
+    default_acquisition_function: AbstractAcquisitionFunction,
+    loss_function: Callable = spearman,
+    sampling_based: bool = True,
+) -> Tuple[AbstractEPM, AbstractAcquisitionFunction]:
+
+    if len(runhistory.data) < 10:
+        return default_model, default_acquisition_function
+
+    X, y = runhistory2EPM.transform(runhistory)
+    test_size = max(5, int(len(X) * 0.2))
+
+    ei = LogEI(par=0.0, model=default_model)
+    ei2 = LogEI(par=0.1, model=default_model)
+    ei3 = LogEI(par=1.0, model=default_model)
+    ei4 = LogEI(par=-0.1, model=default_model)
+    ei5 = LogEI(par=-1, model=default_model)
+
+    combinations = [
+        (default_model, ei),
+        (default_model, ei2),
+        (default_model, ei3),
+        (default_model, ei4),
+        (default_model, ei5),
+        (RandomEPM(np.random.RandomState(1), types=default_model.types.copy(), bounds=default_model.bounds), ei)
+    ]
+
+    combination_losses = []
+    for model, acq in combinations:
+        losses = []
+        splitter = ShuffleSplit(n_splits=200, test_size=test_size, random_state=1)
+        for train_indices, test_indices in splitter.split(X, y):
+            loss = loss_function(X, acq, model, runhistory2EPM, test_indices, train_indices, y)
+            losses.append(loss)
+
+        if sampling_based:
+            combination_losses.append(losses)
+        else:
+            combination_losses.append(np.mean(losses))
+
+    if sampling_based:
+        wins = np.zeros((len(combinations), ))
+        for cl in np.array(combination_losses).transpose():
+            wins[np.argmin(cl)] += 1
+        wins = wins / np.sum(wins)
+        choice = np.random.choice(len(combinations), p=wins)
+        print(wins, X.shape, test_size, choice, len(combinations))
+    else:
+        choice = np.nanargmin(combination_losses)
+        print(combination_losses, X.shape, test_size, choice)
+    return combinations[choice][0], combinations[choice][1]
--- smac/optimizer/smbo.py
+++ smac/optimizer/smbo.py
@@ -32,6 +32,9 @@ from smac.utils.io.traj_logging import TrajLogger
 from smac.utils.validate import Validator
 from smac.utils.util_funcs import get_types
 from smac.utils.constants import MAXINT
+from smac.configspace.util import convert_configurations_to_array
+from smac.optimizer.score import great_new_selection_function
+
 
 
 __author__ = "Aaron Klein, Marius Lindauer, Matthias Feurer"
@@ -146,7 +149,7 @@ class SMBO(object):
         self._random_search = RandomSearch(
             acquisition_func, self.config_space, rng
         )
-        
+
         self.predict_incumbent = predict_incumbent
 
     def start(self):
@@ -196,7 +199,15 @@ class SMBO(object):
                            logger=self.logger)
 
             start_time = time.time()
-            X, Y = self.rh2EPM.transform(self.runhistory)
+
+            _, self.acquisition_func = great_new_selection_function(
+                runhistory=self.runhistory,
+                runhistory2EPM=self.rh2EPM,
+                default_model=self.model,
+                default_acquisition_function=self.acquisition_func,
+            )
+
+            X, Y = self.rh2EPM.transform(runhistory=self.runhistory)
 
             self.logger.debug("Search for next configuration")
             # get all found configurations sorted according to acq
@@ -233,7 +244,7 @@ class SMBO(object):
 
     def choose_next(self, X: np.ndarray, Y: np.ndarray,
                     incumbent_value: float=None):
-        """Choose next candidate solution with Bayesian optimization. The 
+        """Choose next candidate solution with Bayesian optimization. The
         suggested configurations depend on the argument ``acq_optimizer`` to
         the ``SMBO`` class.
 
@@ -271,20 +282,23 @@ class SMBO(object):
             incumbent_value = self._get_incumbent_value()
 
         self.acquisition_func.update(model=self.model, eta=incumbent_value, num_data=len(self.runhistory.data))
+        self.acq_optimizer.acquisition_function = self.acquisition_func
+        self.acq_optimizer.random_search.acquisition_function = self.acquisition_func
+        self.acq_optimizer.local_search.acquisition_function = self.acquisition_func
 
         challengers = self.acq_optimizer.maximize(
-            runhistory=self.runhistory, 
-            stats=self.stats, 
-            num_points=self.scenario.acq_opt_challengers, 
+            runhistory=self.runhistory,
+            stats=self.stats,
+            num_points=self.scenario.acq_opt_challengers,
             random_configuration_chooser=self.random_configuration_chooser
         )
         return challengers
-    
+
     def _get_incumbent_value(self):
         ''' get incumbent value either from runhistory
             or from best predicted performance on configs in runhistory
             (depends on self.predict_incumbent)"
-            
+
             Return
             ------
             float
@@ -298,7 +312,7 @@ class SMBO(object):
             ))
             incumbent_value = np.min(costs)
             # won't need log(y) if EPM was already trained on log(y)
-            
+
         else:
             if self.runhistory.empty():
                 raise ValueError("Runhistory is empty and the cost value of "
@@ -306,7 +320,7 @@ class SMBO(object):
             incumbent_value = self.runhistory.get_cost(self.incumbent)
             if isinstance(self.rh2EPM,RunHistory2EPM4LogCost):
                 incumbent_value = np.log(incumbent_value)
-        
+
         return incumbent_value
 
     def validate(self, config_mode='inc', instance_mode='train+test',
@@ -361,7 +375,7 @@ class SMBO(object):
                                         output_fn=new_rh_path)
         return new_rh
 
-    def _get_timebound_for_intensification(self, time_spent:float):
+    def _get_timebound_for_intensification(self, time_spent: float):
         """Calculate time left for intensify from the time spent on
         choosing challengers using the fraction of time intended for
         intensification (which is specified in
@@ -387,127 +401,3 @@ class SMBO(object):
                           "intensification: %.4f (%.2f)" %
                           (total_time, time_spent, (1 - frac_intensify), time_left, frac_intensify))
         return time_left
-    
-    def _component_builder(self, conf:typing.Union[Configuration, dict]) \
-        -> typing.Tuple[AbstractAcquisitionFunction, AbstractEPM]:
-        """
-            builds new Acquisition function object
-            and EPM object and returns these
-            
-            Parameters
-            ----------
-            conf: typing.Union[Configuration, dict]
-                configuration specificing "model" and "acq_func"
-                
-            Returns
-            -------
-            typing.Tuple[AbstractAcquisitionFunction, AbstractEPM]
-            
-        """
-        types, bounds = get_types(self.config_space, instance_features=self.scenario.feature_array)
-        
-        if conf["model"] == "RF":
-            model = RandomForestWithInstances(
-                  types=types,
-                  bounds=bounds,
-                  instance_features=self.scenario.feature_array,
-                  seed=self.rng.randint(MAXINT),
-                  pca_components=conf.get("pca_dim", self.scenario.PCA_DIM),
-                  log_y=conf.get("log_y", self.scenario.transform_y in ["LOG", "LOGS"]),
-                  num_trees=conf.get("num_trees", self.scenario.rf_num_trees), 
-                  do_bootstrapping=conf.get("do_bootstrapping", self.scenario.rf_do_bootstrapping),  
-                  ratio_features=conf.get("ratio_features", self.scenario.rf_ratio_features),
-                  min_samples_split=conf.get("min_samples_split", self.scenario.rf_min_samples_split),
-                  min_samples_leaf=conf.get("min_samples_leaf", self.scenario.rf_min_samples_leaf),
-                  max_depth=conf.get("max_depth", self.scenario.rf_max_depth))
-            
-        elif conf["model"] == "GP":
-            cov_amp = 2
-            n_dims = len(types)
-
-            initial_ls = np.ones([n_dims])
-            exp_kernel = george.kernels.Matern52Kernel(initial_ls, ndim=n_dims)
-            kernel = cov_amp * exp_kernel
-
-            prior = DefaultPrior(len(kernel) + 1, rng=self.rng)
-
-            n_hypers = 3 * len(kernel)
-            if n_hypers % 2 == 1:
-                n_hypers += 1
-
-            model = GaussianProcessMCMC(
-                types=types,
-                bounds=bounds,
-                kernel=kernel,
-                prior=prior,
-                n_hypers=n_hypers,
-                chain_length=200,
-                burnin_steps=100,
-                normalize_input=True,
-                normalize_output=True,
-                rng=self.rng,
-            )
-            
-        if conf["acq_func"] == "EI":
-            acq = EI(model=model,
-                     par=conf.get("par_ei", 0))
-        elif conf["acq_func"] == "LCB":
-            acq = LCB(model=model,
-                par=conf.get("par_lcb", 0))
-        elif conf["acq_func"] == "PI":
-            acq = PI(model=model,
-                     par=conf.get("par_pi", 0))
-        elif conf["acq_func"] == "LogEI":
-            # par value should be in log-space
-            acq = LogEI(model=model,
-                        par=conf.get("par_logei", 0))
-        
-        return acq, model
-    
-    def _get_acm_cs(self):
-        """
-            returns a configuration space 
-            designed for querying ~smac.optimizer.smbo._component_builder
-            
-            Returns
-            ------- 
-                ConfigurationSpace
-        """
-        
-        cs = ConfigurationSpace()
-        cs.seed(self.rng.randint(0,2**20))
-        
-        model = CategoricalHyperparameter("model", choices=("RF", "GP"))
-        
-        num_trees = Constant("num_trees", value=10)
-        bootstrap = CategoricalHyperparameter("do_bootstrapping", choices=(True, False), default_value=True)
-        ratio_features = CategoricalHyperparameter("ratio_features", choices=(3 / 6, 4 / 6, 5 / 6, 1), default_value=1)
-        min_split = UniformIntegerHyperparameter("min_samples_to_split", lower=1, upper=10, default_value=2)
-        min_leaves = UniformIntegerHyperparameter("min_samples_in_leaf", lower=1, upper=10, default_value=1)
-        
-        cs.add_hyperparameters([model, num_trees, bootstrap, ratio_features, min_split, min_leaves])
-        
-        inc_num_trees = InCondition(num_trees, model, ["RF"])
-        inc_bootstrap = InCondition(bootstrap, model, ["RF"])
-        inc_ratio_features = InCondition(ratio_features, model, ["RF"])
-        inc_min_split = InCondition(min_split, model, ["RF"])
-        inc_min_leavs = InCondition(min_leaves, model, ["RF"])
-        
-        cs.add_conditions([inc_num_trees, inc_bootstrap, inc_ratio_features, inc_min_split, inc_min_leavs])
-        
-        acq  = CategoricalHyperparameter("acq_func", choices=("EI", "LCB", "PI", "LogEI"))
-        par_ei = UniformFloatHyperparameter("par_ei", lower=-10, upper=10)
-        par_pi = UniformFloatHyperparameter("par_pi", lower=-10, upper=10)
-        par_logei = UniformFloatHyperparameter("par_logei", lower=0.001, upper=100, log=True)
-        par_lcb = UniformFloatHyperparameter("par_lcb", lower=0.0001, upper=0.9999)
-        
-        cs.add_hyperparameters([acq, par_ei, par_pi, par_logei, par_lcb])
-        
-        inc_par_ei = InCondition(par_ei, acq, ["EI"])
-        inc_par_pi = InCondition(par_pi, acq, ["PI"])
-        inc_par_logei = InCondition(par_logei, acq, ["LogEI"])
-        inc_par_lcb = InCondition(par_lcb, acq, ["LCB"])
-        
-        cs.add_conditions([inc_par_ei, inc_par_pi, inc_par_logei, inc_par_lcb])
-        
-        return cs
