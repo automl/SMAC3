b240e67cb6aeabce4697149adc0a26c6b23e3c43
Marius Lindauer <marius.rks@googlemail.com>
2018-12-14 15:45:56 +0100

    FIX adaptive unit tests

--- test/test_smbo/test_adaptive_component_selection.py
+++ test/test_smbo/test_adaptive_component_selection.py
@@ -7,6 +7,7 @@ from smac.epm.rf_with_instances import RandomForestWithInstances
 from smac.epm.gaussian_process_mcmc import GaussianProcessMCMC
 from smac.optimizer.adaptive_component_selection import AdaptiveComponentSelection
 from smac.optimizer.acquisition import AbstractAcquisitionFunction, EI
+from smac.runhistory.runhistory2epm import AbstractRunHistory2EPM
 from smac.scenario.scenario import Scenario
 from smac.utils import test_helpers
 
@@ -27,17 +28,19 @@ class TestAdaptiveComponentSelection(unittest.TestCase):
         )
 
     def test_comp_builder(self):
-        conf = {"model": "RF", "acq_func": "EI"}
-        model, acqf = self.acs._component_builder(conf)
+        conf = {"model": "RF", "acq_func": "EI", "y_transform": "y"}
+        model, acqf, rh2epm = self.acs._component_builder(conf)
 
         self.assertIsInstance(acqf, EI)
         self.assertIsInstance(model, RandomForestWithInstances)
+        self.assertIsInstance(rh2epm, AbstractRunHistory2EPM)
 
-        conf = {"model": "GP", "acq_func": "EI"}
-        model, acqf = self.acs._component_builder(conf)
+        conf = {"model": "GP", "acq_func": "EI", "y_transform": "y"}
+        model, acqf, rh2epm = self.acs._component_builder(conf)
 
         self.assertIsInstance(acqf, EI)
         self.assertIsInstance(model, GaussianProcessMCMC)
+        self.assertIsInstance(rh2epm, AbstractRunHistory2EPM)
 
     def test_smbo_cs(self):
         seed = 42
@@ -47,6 +50,7 @@ class TestAdaptiveComponentSelection(unittest.TestCase):
         cs = self.acs._get_acm_cs()
         conf = cs.sample_configuration()
 
-        model, acqf = self.acs._component_builder(conf)
+        model, acqf, rh2epm = self.acs._component_builder(conf)
         self.assertIsInstance(acqf, AbstractAcquisitionFunction)
         self.assertIsInstance(model, AbstractEPM)
+        self.assertIsInstance(rh2epm, AbstractRunHistory2EPM)
