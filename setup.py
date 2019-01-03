f49ed6710d38c99be0a734727ae7519ac7125844
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2019-01-03 17:50:21 +0100

    simplify setup.py by moving most thing to setup.cfg

--- setup.py
+++ setup.py
@@ -1,46 +1,14 @@
-import setuptools
-import sys
-
-import smac
-
+#!/usr/bin/env python3
+from setuptools import setup
 
 with open('requirements.txt') as fh:
     requirements = fh.read()
 requirements = requirements.split('\n')
 requirements = [requirement.strip() for requirement in requirements]
 
-with open("smac/__version__.py") as fh:
-    version = fh.readlines()[-1].split()[-1].strip("\"'")
-
-if sys.version_info < (3, 5, 2):
-    raise ValueError('Unsupported Python version %s found. SMAC3 requires Python 3.5.2 or higher.' % sys.version_info)
-
-setuptools.setup(
-    name="smac",
-    version=version,
-    author=smac.AUTHORS,
-    author_email="fh@cs.uni-freiburg.de",
-    description=("SMAC3, a Python implementation of 'Sequential Model-based "
-                 "Algorithm Configuration'."),
-    license="3-clause BSD",
-    keywords="machine learning algorithm configuration hyperparameter "
-             "optimization tuning",
-    url="",
-    entry_points={
-        'console_scripts': ['smac=smac.smac_cli:cmd_line_call'],
-    },
-    packages=setuptools.find_packages(exclude=['test', 'source']),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "License :: OSI Approved :: BSD License",
-    ],
-    platforms=['Linux'],
+setup(
+    python_requires=">=3.5.2",
     install_requires=requirements,
-    python_requires='>=3.5.2',
-    tests_require=['mock',
-                   'nose'],
-    test_suite='nose.collector'
+    test_suite="nose.collector",
+    tests_require=["mock", "nose"]
 )
--- smac/__init__.py
+++ smac/__init__.py
@@ -1,32 +1,16 @@
 import os
 import sys
 
+import pkg_resources
+from smac.utils import dependencies
+
+
 if sys.version_info < (3, 5, 2):
     raise ValueError("SMAC requires Python 3.5.2 or newer.")
 
-from smac.__version__ import __version__
-from smac.utils import dependencies
 
-AUTHORS = "Marius Lindauer, Matthias Feurer, Katharina Eggensperger, " \
-          "Aaron Klein, Stefan Falkner and Frank Hutter"
-
-__MANDATORY_PACKAGES__ = '''
-numpy>=1.7.1
-scipy>=0.18.1
-scikit-learn>=0.18.0
-pyrfr>=0.5.0
-ConfigSpace>=0.4.6,<0.5
-emcee>=2.1.0
-george
-cython
-psutil
-pyDOE
-statsmodels
-joblib
-sobol_seq
-'''
-
-
+__version__ = pkg_resources.get_distribution("smac").version
+__MANDATORY_PACKAGES__ = "\n".join(["%s" % r for r in pkg_resources.get_distribution("smac").requires()])
 dependencies.verify_packages(__MANDATORY_PACKAGES__)
 
 if os.name != 'posix':
