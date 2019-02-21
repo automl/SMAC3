fb089605946bfc9a2212a68e2fbc816f6a152e1a
Marius Lindauer <marius.rks@gmail.com>
2019-02-21 14:48:52 +0100

    MAINT extend lhd to non-continuous params

--- smac/initial_design/latin_hypercube_design.py
+++ smac/initial_design/latin_hypercube_design.py
@@ -5,7 +5,8 @@ import numpy as np
 from pyDOE import lhs
 
 from ConfigSpace.configuration_space import Configuration
-from ConfigSpace.hyperparameters import FloatHyperparameter
+from ConfigSpace.hyperparameters import NumericalHyperparameter, \
+    Constant, CategoricalHyperparameter, OrdinalHyperparameter
 from ConfigSpace.util import deactivate_inactive_hyperparameters
 
 from smac.initial_design.initial_design import InitialDesign
@@ -44,21 +45,37 @@ class LHDesign(InitialDesign):
         # manual seeding of lhd design
         np.random.seed(self.rng.randint(1,2*20))
 
-        lhd = lhs(n=len(params), samples=self.init_budget)
+        constants = 0
+        for p in params:
+            if isinstance(p, Constant):
+                constants += 1
+
+        lhd = lhs(n=len(params)-constants, samples=self.init_budget)
 
         for idx, param in enumerate(params):
-            if isinstance(param, FloatHyperparameter):
-                lhd[:,idx] = lhd[:,idx] * (param.upper - param.lower) + param.lower
+            if isinstance(param, NumericalHyperparameter):
+                continue
+                #lhd[:, idx] = lhd[:, idx] * (param.upper - param.lower) + param.lower
+            elif isinstance(param, Constant):
+                # add a vector with zeros
+                lhd_ = np.zeros(np.array(lhd.shape) + np.array((0, 1)))
+                lhd_[:, :idx] = lhd[:, :idx]
+                lhd_[:, idx+1:] = lhd[:, idx:]
+                lhd = lhd_
+            elif isinstance(param, CategoricalHyperparameter):
+                lhd[:, idx] = np.array(lhd[:, idx] * len(param.choices), dtype=np.int)
+            elif isinstance(param, OrdinalHyperparameter):
+                lhd[:, idx] = np.array(lhd[:, idx] * len(param.sequence), dtype=np.int)
             else:
-                raise ValueError("only FloatHyperparameters supported in LHD")
+                raise ValueError("Hyperparamet not supported in LHD")
 
         self.logger.debug("Initial Design")
         configs = []
         # add middle point in space
-
         for design in lhd:
-            conf_dict = dict([(p.name,v) for p,v in zip(params,design)])
-            conf = deactivate_inactive_hyperparameters(conf_dict, cs)
+            conf = deactivate_inactive_hyperparameters(configuration=None,
+                                                       configuration_space=cs,
+                                                       vector=design)
             conf.origin = "LHD"
             configs.append(conf)
             self.logger.debug(conf)
