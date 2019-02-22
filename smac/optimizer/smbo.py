FIX perform scaling for each runhistory2epm (#493)
* FIX perform scaling for each runhistory2epm

* Include work on Marius comments

* minor readability and typos

smac/optimizer/smbo.py
@@ -304,8 +304,11 @@ class SMBO(object):
                 raise ValueError("Runhistory is empty and the cost value of "
                                  "the incumbent is unknown.")
             incumbent_value = self.runhistory.get_cost(self.incumbent)
-            if isinstance(self.rh2EPM,RunHistory2EPM4LogCost):
-                incumbent_value = np.log(incumbent_value)
+            # It's unclear how to do this for inv scaling and potential future scaling. This line should be changed if
+            # necessary
+            incumbent_value_as_array = np.array(incumbent_value).reshape((1, 1))
+            incumbent_value = self.rh2EPM.transform_response_values(incumbent_value_as_array)
+            incumbent_value = incumbent_value[0][0]
 
         return incumbent_value
 

test/test_runhistory/test_runhistory2epm.py
@@ -105,7 +105,7 @@ class RunhistoryTest(unittest.TestCase):
                                                           [0.995, 0.005],
                                                           [0.995, 0.995]]),
                                              decimal=3)
-        
+
         np.testing.assert_array_almost_equal(y, np.array([[0.], [2.727], [5.2983]]),
                                              decimal=3)
 
@@ -210,8 +210,7 @@ class RunhistoryTest(unittest.TestCase):
             adding some rundata to RunHistory2EPM4Cost without imputation
         '''
 
-        rh2epm = runhistory2epm.RunHistory2EPM4Cost(num_params=2,
-                                                       scenario=self.scen)
+        rh2epm = runhistory2epm.RunHistory2EPM4Cost(num_params=2, scenario=self.scen)
 
         self.rh.add(config=self.config1, cost=1, time=1,
                     status=StatusType.SUCCESS, instance_id=23,
@@ -222,17 +221,16 @@ class RunhistoryTest(unittest.TestCase):
         self.assertTrue(np.allclose(X, np.array([[0.005, 0.995]]), atol=0.001))
         self.assertTrue(np.allclose(y, np.array([[1.]])))
 
-        # rh2epm should use time and not cost field later
-        self.rh.add(config=self.config3, cost=2, time=20,
+        # rh2epm should use cost and not time
+        self.rh.add(config=self.config3, cost=200, time=20,
                     status=StatusType.TIMEOUT, instance_id=1,
                     seed=45,
                     additional_info={"start_time": 20})
 
         X, y = rh2epm.transform(self.rh)
-        self.assertTrue(
-            np.allclose(X, np.array([[0.005, 0.995], [0.995, 0.995]]), atol=0.001))
+        np.testing.assert_allclose(X, np.array([[0.005, 0.995], [0.995, 0.995]]), atol=0.001)
         # log_10(20 * 10)
-        self.assertTrue(np.allclose(y, np.array([[1.], [200.]]), atol=0.001))
+        np.testing.assert_allclose(y, np.array([[1.], [200.]]), atol=0.001)
 
         self.rh.add(config=self.config2, cost=100, time=10,
                     status=StatusType.TIMEOUT, instance_id=1,
@@ -279,14 +277,14 @@ class RunhistoryTest(unittest.TestCase):
         self.assertTrue(np.allclose(y, np.array([[1.], [200.]]), atol=0.001))
 
         #TODO: unit test for censored data in quality scenario
-        
+
     def test_get_X_y(self):
         '''
             add some data to RH and check returned values in X,y format
         '''
 
         self.scen = Scenario({'cutoff_time': 20, 'cs': self.cs,
-                              'run_obj': 'runtime', 
+                              'run_obj': 'runtime',
                               'instances': [['1'],['2']],
                               'features': {
                                   '1': [1,1],
@@ -301,38 +299,38 @@ class RunhistoryTest(unittest.TestCase):
                     status=StatusType.SUCCESS, instance_id='1',
                     seed=None,
                     additional_info=None)
-        
+
         self.rh.add(config=self.config1, cost=2, time=10,
                     status=StatusType.SUCCESS, instance_id='2',
                     seed=None,
                     additional_info=None)
-        
+
         self.rh.add(config=self.config2, cost=1, time=10,
                     status=StatusType.TIMEOUT, instance_id='1',
                     seed=None,
                     additional_info=None)
-        
+
         self.rh.add(config=self.config2, cost=0.1, time=10,
                     status=StatusType.CAPPED, instance_id='2',
                     seed=None,
                     additional_info=None)
-        
+
         X,y,c = rh2epm.get_X_y(self.rh)
-        
+
         print(X,y,c)
-        
+
         X_sol = np.array([[0,100,1,1],
                           [0,100,2,2],
                           [100,0,1,1],
                           [100,0,2,2]])
         self.assertTrue(np.all(X==X_sol))
-        
+
         y_sol = np.array([1,2,1,0.1])
         self.assertTrue(np.all(y==y_sol))
-        
+
         c_sol = np.array([False, False, True, True])
         self.assertTrue(np.all(c==c_sol))
-        
+
 
 if __name__ == "__main__":
     unittest.main()