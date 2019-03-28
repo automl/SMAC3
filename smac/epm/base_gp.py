replace george by skopt, fix gradient of priors

smac/epm/base_gp.py
@@ -9,8 +9,7 @@ class BaseModel(AbstractEPM):
         """
         Abstract base class for all Gaussian process models.
         """
-        super().__init__(types=types, bounds=bounds, seed=seed,
-                         **kwargs)
+        super().__init__(types=types, bounds=bounds, seed=seed, **kwargs)
 
         self.rng = np.random.RandomState(seed)
 

smac/epm/gaussian_process.py
@@ -1,13 +1,14 @@
 import logging
 import typing
 
-import george
 import numpy as np
+import skopt.learning.gaussian_process
+import skopt.learning.gaussian_process.kernels
 from scipy import optimize
 
-from smac.epm import normalization
 from smac.epm.base_gp import BaseModel
 from smac.epm.gp_base_prior import BasePrior
+from smac.utils.constants import VERY_SMALL_NUMBER
 
 logger = logging.getLogger(__name__)
 
@@ -41,11 +42,6 @@ class GaussianProcess(BaseModel):
     prior : prior object
         Defines a prior for the hyperparameters of the GP. Make sure that
         it implements the Prior interface.
-    noise : float
-        Noise term that is added to the diagonal of the covariance matrix
-        for the Cholesky decomposition.
-    use_gradients : bool
-        Use gradient information to optimize the negative log likelihood
     normalize_output : bool
         Zero mean unit variance normalization of the output values
     normalize_input : bool
@@ -60,23 +56,18 @@ class GaussianProcess(BaseModel):
         types: np.ndarray,
         bounds: typing.List[typing.Tuple[float, float]],
         seed: int,
-        kernel: george.kernels.Kernel,
+        kernel: skopt.learning.gaussian_process.kernels.Kernel,
         prior: BasePrior=None,
-        noise: float=1e-3,
-        use_gradients: bool=False,
-        normalize_output: bool=True,
-        normalize_input: bool=True,
+        normalize_y: bool=True,
+        **kwargs
     ):
 
-        super().__init__(types=types, bounds=bounds, seed=seed)
+        super().__init__(types=types, bounds=bounds, seed=seed, **kwargs)
 
         self.kernel = kernel
         self.gp = None
         self.prior = prior
-        self.noise = noise
-        self.use_gradients = use_gradients
-        self.normalize_output = normalize_output
-        self.normalize_input = normalize_input
+        self.normalize_y = normalize_y
         self.X = None
         self.y = None
         self.hypers = []
@@ -101,49 +92,28 @@ class GaussianProcess(BaseModel):
             the default hyperparameters of the kernel are used.
         """
 
-        if self.normalize_input:
-            # Normalize input to be in [0, 1]
-            self.X, self.lower, self.upper = normalization.zero_one_normalization(X, self.lower, self.upper)
-        else:
-            self.X = X
-
-        if len(y.shape) > 1:
-            y = y.flatten()
-            if len(y) != len(X):
-                raise ValueError('Shape mismatch: %s vs %s' % (y.shape, X.shape))
-
-        if self.normalize_output:
-            # Normalize output to have zero mean and unit standard deviation
-            self.y, self.y_mean, self.y_std = normalization.zero_mean_unit_var_normalization(y)
-            if self.y_std == 0:
-                raise ValueError("Cannot normalize output. All targets have the same value")
-        else:
-            self.y = y
-
-        # Use the empirical mean of the data as mean for the GP
-        self.mean = np.mean(self.y, axis=0)
-        self.gp = george.GP(self.kernel, mean=self.mean)
+        self.gp = skopt.learning.gaussian_process.GaussianProcessRegressor(
+            kernel=self.kernel,
+            normalize_y=self.normalize_y,
+            optimizer=None,
+            n_restarts_optimizer=-1,  # Do not use scikit-learn's optimization routine
+            alpha=0,  # Governed by the kernel
+            noise=None,
+        )
+        # Initialize some variables
+        self.gp.fit(X, y)
 
         if do_optimize:
             self.hypers = self._optimize()
-            self.gp.kernel.set_parameter_vector(self.hypers[:-1])
-            self.noise = np.exp(self.hypers[-1])  # sigma^2
         else:
-            self.hypers = self.gp.kernel.get_parameter_vector()
-            self.hypers = np.append(self.hypers, np.log(self.noise))
+            self.hypers = self.gp.kernel.theta
 
-        try:
-            self.gp.compute(self.X, yerr=np.sqrt(self.noise))
-        except np.linalg.LinAlgError:
-            self.noise *= 10
-            self.gp.compute(self.X, yerr=np.sqrt(self.noise))
+        self.gp.kernel.theta = self.hypers
+        self.gp.fit(X, y)
 
         self.is_trained = True
 
-    def _get_noise(self):
-        return self.noise
-
-    def _nll(self, theta: np.ndarray) -> float:
+    def _nll(self, theta: np.ndarray) -> typing.Tuple[float, np.ndarray]:
         """
         Returns the negative marginal log likelihood (+ the prior) for
         a hyperparameter configuration theta.
@@ -160,52 +130,19 @@ class GaussianProcess(BaseModel):
         float
             lnlikelihood + prior
         """
-        # Specify bounds to keep things sane
-        if np.any((-20 > theta) + (theta > 20)):
-            return 1e25
 
-        # The last entry of theta is always the noise
-        self.gp.kernel.set_parameter_vector(theta[:-1])
-        noise = np.exp(theta[-1])  # sigma^2
-
-        try:
-            self.gp.compute(self.X, yerr=np.sqrt(noise))
-        except np.linalg.LinAlgError:
-            return 1e25
-
-        ll = self.gp.lnlikelihood(self.y, quiet=True)
+        lml, grad = self.gp.log_marginal_likelihood(theta, eval_gradient=True)
 
         # Add prior
         if self.prior is not None:
-            ll += self.prior.lnprob(theta)
+            lml += self.prior.lnprob(theta)
+            grad += self.prior.gradient(theta)
 
         # We add a minus here because scipy is minimizing
-        return -ll if np.isfinite(ll) else 1e25
-
-    def _grad_nll(self, theta: np.ndarray):
-
-        self.gp.kernel.set_parameter_vector(theta[:-1])
-        noise = np.exp(theta[-1])
-
-        self.gp.compute(self.X, yerr=np.sqrt(noise))
-
-        self.gp._compute_alpha(self.y)
-        K_inv = self.gp.solver.apply_inverse(np.eye(self.gp._alpha.size),
-                                             in_place=True)
-
-        # The gradients of the Gram matrix, for the noise this is just
-        # the identity matrix
-        Kg = self.gp.kernel.gradient(self.gp._x)
-        Kg = np.concatenate((Kg, np.eye(Kg.shape[0])[:, :, None]), axis=2)
-
-        # Calculate the gradient.
-        A = np.outer(self.gp._alpha, self.gp._alpha) - K_inv
-        g = 0.5 * np.einsum('ijk,ij', Kg, A)
-
-        if self.prior is not None:
-            g += self.prior.gradient(theta)
-
-        return -g
+        if not np.isfinite(lml).all() or not np.all(np.isfinite(grad)):
+            return 1e25, np.array([1e25] * theta.shape[0])
+        else:
+            return -lml, -grad
 
     def _optimize(self) -> np.ndarray:
         """
@@ -218,21 +155,9 @@ class GaussianProcess(BaseModel):
             Hyperparameter vector that maximizes the marginal log likelihood
         """
         # Start optimization from the previous hyperparameter configuration
-        p0 = self.gp.kernel.get_parameter_vector()
-        p0 = np.append(p0, np.log(self.noise))
-
-        if self.use_gradients:
-            theta, _, _ = optimize.minimize(self._nll, p0,
-                                            method="BFGS",
-                                            jac=self._grad_nll)
-        else:
-            try:
-                results = optimize.minimize(self._nll, p0, method='L-BFGS-B')
-                theta = results.x
-            except ValueError:
-                logging.error("Could not find a valid hyperparameter configuration! Use initial configuration")
-                theta = p0
-
+        p0 = self.gp.kernel.theta
+        bounds = [(np.exp(b[0]), np.exp(b[1])) for b in self.gp.kernel.bounds]
+        theta, f_opt, _ = optimize.fmin_l_bfgs_b(self._nll, p0, bounds=bounds)
         return theta
 
     def _predict(self, X_test: np.ndarray, full_cov: bool=False):
@@ -259,26 +184,12 @@ class GaussianProcess(BaseModel):
         if not self.is_trained:
             raise Exception('Model has to be trained first!')
 
-        if self.normalize_input:
-            X_test_norm, _, _ = normalization.zero_one_normalization(X_test, self.lower, self.upper)
-        else:
-            X_test_norm = X_test
-
-        mu, var = self.gp.predict(self.y, X_test_norm)
-
-        if self.normalize_output:
-            mu = normalization.zero_mean_unit_var_unnormalization(mu, self.y_mean, self.y_std)
-            var *= self.y_std ** 2
-        if not full_cov:
-            var = np.diag(var)
+        mu, var = self.gp.predict(X_test, return_cov=True)
+        var = np.diag(var)
 
         # Clip negative variances and set them to the smallest
         # positive float value
-        if var.shape[0] == 1:
-            var = np.clip(var, np.finfo(var.dtype).eps, np.inf)
-        else:
-            var = np.clip(var, np.finfo(var.dtype).eps, np.inf)
-            var[np.where((var < np.finfo(var.dtype).eps) & (var > -np.finfo(var.dtype).eps))] = 0
+        np.clip(var, VERY_SMALL_NUMBER, np.inf)
 
         return mu, var
 
@@ -300,18 +211,10 @@ class GaussianProcess(BaseModel):
             The F function values drawn at the N test points.
         """
 
-        if self.normalize_input:
-            X_test_norm, _, _ = normalization.zero_one_normalization(X_test, self.lower, self.upper)
-        else:
-            X_test_norm = X_test
-
         if not self.is_trained:
             raise Exception('Model has to be trained first!')
 
-        funcs = self.gp.sample_conditional(self.y, X_test_norm, n_funcs)
-
-        if self.normalize_output:
-            funcs = normalization.zero_mean_unit_var_unnormalization(funcs, self.y_mean, self.y_std)
+        funcs = self.gp.sample_y(X_test, n_samples=n_funcs, random_state=self.rng)
 
         if len(funcs.shape) == 1:
             return funcs[None, :]

smac/epm/gaussian_process_mcmc.py
@@ -2,13 +2,13 @@ from copy import deepcopy
 import logging
 import typing
 
-import george
 import emcee
 import numpy as np
+import skopt.learning.gaussian_process
+import skopt.learning.gaussian_process.kernels
 
 from smac.epm.base_gp import BaseModel
 from smac.epm.gaussian_process import GaussianProcess
-from smac.epm import normalization
 from smac.epm.gp_base_prior import BasePrior
 
 logger = logging.getLogger(__name__)
@@ -21,14 +21,12 @@ class GaussianProcessMCMC(BaseModel):
         types: np.ndarray,
         bounds: typing.List[typing.Tuple[float, float]],
         seed: int,
-        kernel: george.kernels.Kernel,
+        kernel: skopt.learning.gaussian_process.kernels.Kernel,
         prior: BasePrior=None,
-        n_hypers: int=20,
+        n_mcmc_walkers: int=20,
         chain_length: int=2000,
         burnin_steps: int=2000,
-        normalize_output: bool=True,
-        normalize_input: bool=True,
-        noise: int=-8,
+        normalize_y: bool=True,
         **kwargs
     ):
         """
@@ -62,39 +60,31 @@ class GaussianProcessMCMC(BaseModel):
             Defines a prior for the hyperparameters of the GP. Make sure that
             it implements the Prior interface. During MCMC sampling the
             lnlikelihood is multiplied with the prior.
-        n_hypers : int
+        n_mcmc_walkers : int
             The number of hyperparameter samples. This also determines the
             number of walker for MCMC sampling as each walker will
             return one hyperparameter sample.
         chain_length : int
-            The length of the MCMC chain. We start n_hypers walker for
+            The length of the MCMC chain. We start n_mcmc_walkers walker for
             chain_length steps and we use the last sample
             in the chain as a hyperparameter sample.
         burnin_steps : int
             The number of burnin steps before the actual MCMC sampling starts.
-        normalize_output : bool
+        normalize_y : bool
             Zero mean unit variance normalization of the output values
-        normalize_input : bool
-            Normalize all inputs to be in [0, 1]. This is important to define good priors for the
-            length scales.
         rng: np.random.RandomState
             Random number generator
-        noise : float
-            Noise term that is added to the diagonal of the covariance matrix
-            for the Cholesky decomposition.
         """
         super().__init__(types=types, bounds=bounds, seed=seed, **kwargs)
 
         self.kernel = kernel
         self.prior = prior
-        self.noise = noise
-        self.n_hypers = n_hypers
+        self.n_mcmc_walkers = n_mcmc_walkers
         self.chain_length = chain_length
         self.burned = False
         self.burnin_steps = burnin_steps
         self.models = []
-        self.normalize_output = normalize_output
-        self.normalize_input = normalize_input
+        self.normalize_y = normalize_y
         self.X = None
         self.y = None
         self.is_trained = False
@@ -116,42 +106,35 @@ class GaussianProcessMCMC(BaseModel):
             hyperparameter specified in the kernel.
         """
 
-        if self.normalize_input:
-            # Normalize input to be in [0, 1]
-            self.X, self.lower, self.upper = normalization.zero_one_normalization(X, self.lower, self.upper)
-        else:
-            self.X = X
-
         if len(y.shape) > 1:
             y = y.flatten()
             if len(y) != len(X):
                 raise ValueError('Shape mismatch: %s vs %s' % (y.shape, X.shape))
 
-        if self.normalize_output:
-            # Normalize output to have zero mean and unit standard deviation
-            self.y, self.y_mean, self.y_std = normalization.zero_mean_unit_var_normalization(y)
-            if self.y_std == 0:
-                raise ValueError("Cannot normalize output. All targets have the same value")
-        else:
-            self.y = y
-
-        # Use the mean of the data as mean for the GP
-        self.mean = np.mean(self.y, axis=0)
-        self.gp = george.GP(self.kernel, mean=self.mean)
+        self.gp = skopt.learning.gaussian_process.GaussianProcessRegressor(
+            kernel=self.kernel,
+            normalize_y=self.normalize_y,
+            optimizer=None,
+            n_restarts_optimizer=-1,  # Do not use scikit-learn's optimization routine
+            alpha=0,  # Governed by the kernel
+            noise=None,
+        )
+        # Initialize some variables
+        self.gp.fit(X, y)
 
         if do_optimize:
             # We have one walker for each hyperparameter configuration
-            sampler = emcee.EnsembleSampler(self.n_hypers,
-                                            len(self.kernel) + 1,
-                                            self._loglikelihood)
+            sampler = emcee.EnsembleSampler(self.n_mcmc_walkers,
+                                            len(self.kernel.theta),
+                                            self._ll)
             sampler.random_state = self.rng.get_state()
             # Do a burn-in in the first iteration
             if not self.burned:
                 # Initialize the walkers by sampling from the prior
                 if self.prior is None:
-                    self.p0 = self.rng.rand(self.n_hypers, len(self.kernel) + 1)
+                    self.p0 = self.rng.rand(self.n_mcmc_walkers, len(self.kernel.theta))
                 else:
-                    self.p0 = self.prior.sample_from_prior(self.n_hypers)
+                    self.p0 = self.prior.sample_from_prior(self.n_mcmc_walkers)
                 # Run MCMC sampling
                 self.p0, _, _ = sampler.run_mcmc(self.p0,
                                                  self.burnin_steps,
@@ -159,14 +142,8 @@ class GaussianProcessMCMC(BaseModel):
 
                 self.burned = True
 
-            # Start sampling
-            pos, _, _ = sampler.run_mcmc(self.p0,
-                                         self.chain_length,
-                                         rstate0=self.rng)
-
-            # Save the current position, it will be the start point in
-            # the next iteration
-            self.p0 = pos
+            # Start sampling & save the current position, it will be the start point in the next iteration
+            self.p0, _, _ = sampler.run_mcmc(self.p0, self.chain_length, rstate0=self.rng)
 
             # Take the last samples from each walker
             self.hypers = sampler.chain[:, -1]
@@ -181,15 +158,12 @@ class GaussianProcessMCMC(BaseModel):
 
             # Instantiate a GP for each hyperparameter configuration
             kernel = deepcopy(self.kernel)
-            kernel.set_parameter_vector(sample[:-1])
-            noise = np.exp(sample[-1])
+            kernel.theta = sample
             model = GaussianProcess(
                 types=self.types,
                 bounds=self.bounds,
                 kernel=kernel,
-                normalize_output=self.normalize_output,
-                normalize_input=self.normalize_input,
-                noise=noise,
+                normalize_y=self.normalize_y,
                 seed=self.rng.randint(low=0, high=10000),
             )
             model._train(X, y, do_optimize=False)
@@ -197,10 +171,10 @@ class GaussianProcessMCMC(BaseModel):
 
         self.is_trained = True
 
-    def _loglikelihood(self, theta: np.ndarray):
+    def _ll(self, theta: np.ndarray) -> float:
         """
-        Return the loglikelihood (+ the prior) for a hyperparameter
-        configuration theta.
+        Returns the marginal log likelihood (+ the prior) for
+        a hyperparameter configuration theta.
 
         Parameters
         ----------
@@ -217,22 +191,22 @@ class GaussianProcessMCMC(BaseModel):
         # Bound the hyperparameter space to keep things sane. Note all
         # hyperparameters live on a log scale
         if np.any((-20 > theta) + (theta > 20)):
-            return -np.inf
-
-        # The last entry is always the noise
-        sigma_2 = np.exp(theta[-1])
-        # Update the kernel and compute the lnlikelihood.
-        self.gp.kernel.set_parameter_vector(theta[:-1])
+            return -1e25
 
         try:
-            self.gp.compute(self.X, yerr=np.sqrt(sigma_2))
-        except:
-            return -np.inf
+            lml = self.gp.log_marginal_likelihood(theta)
+        except ValueError as e:
+            return -1e25
 
+        # Add prior
         if self.prior is not None:
-            return self.prior.lnprob(theta) + self.gp.lnlikelihood(self.y, quiet=True)
+            lml += self.prior.lnprob(theta)
+
+        # We add a minus here because scipy is minimizing
+        if not np.isfinite(lml):
+            return -1e25
         else:
-            return self.gp.lnlikelihood(self.y, quiet=True)
+            return lml
 
     def _predict(self, X_test: np.ndarray):
         r"""

smac/epm/gp_base_prior.py
@@ -1,6 +1,7 @@
-
 import numpy as np
 import scipy.stats as sps
+
+from smac.utils.constants import VERY_SMALL_NUMBER
 
 
 class BasePrior(object):
@@ -211,7 +212,10 @@ class HorseshoePrior(BasePrior):
         # We computed it exactly as in the original spearmint code
         if np.any(theta == 0.0):
             return np.inf
-        return np.log(np.log(1 + 3.0 * (self.scale / np.exp(theta)) ** 2))
+        a = np.maximum(np.exp(theta), VERY_SMALL_NUMBER)
+        b = np.log(1 + 3.0 * (self.scale / a) ** 2)
+        b = np.maximum(b, VERY_SMALL_NUMBER)
+        return np.log(b)
 
     def sample_from_prior(self, n_samples: int):
         """
@@ -248,10 +252,12 @@ class HorseshoePrior(BasePrior):
         (D) np.array
             The gradient of the prior at theta.
         """
-        a = -(6 * self.scale ** 2)
-        b = (3 * self.scale ** 2 + np.exp(2 * theta))
-        b *= np.log(3 * self.scale ** 2 * np.exp(- 2 * theta) + 1)
-        return a / b
+        a = -(6 * np.power(self.scale, 2))
+        b = (3 * np.power(self.scale, 2) + np.exp(2 * theta))
+        b *= np.log(3 * np.power(self.scale, 2) * np.exp(- 2 * theta) + 1)
+        b = np.maximum(b, 1e-14)
+        rval = a / b
+        return rval
 
 
 class LognormalPrior(BasePrior):
@@ -280,6 +286,9 @@ class LognormalPrior(BasePrior):
         else:
             self.rng = rng
 
+        if mean != 0:
+            raise NotImplementedError(mean)
+
         self.sigma = sigma
         self.mean = mean
 
@@ -298,7 +307,6 @@ class LognormalPrior(BasePrior):
         float
             The log probability of theta
         """
-
         return sps.lognorm.logpdf(theta, self.sigma, loc=self.mean)
 
     def sample_from_prior(self, n_samples: int):
@@ -336,7 +344,8 @@ class LognormalPrior(BasePrior):
         (D) np.array
             The gradient of the prior at theta.
         """
-        pass
+        # derivative 1 / (x * s^2 * sqrt(2 pi)) * exp( - 0.5 * (log(x ) / s^2))^2)
+        return - (self.sigma **2 + np.log(theta)) / (self.sigma ** 2 * theta)
 
 
 class NormalPrior(BasePrior):

smac/epm/gp_default_priors.py
@@ -56,5 +56,11 @@ class DefaultPrior(BasePrior):
         return p0
 
     def gradient(self, theta: np.ndarray):
-        # TODO: Implement real gradient here
-        return np.zeros([theta.shape[0]])
+        grad = np.zeros([theta.shape[0]])
+        # Covariance amplitude
+        grad[0] += self.ln_prior.gradient(theta[0])
+        # Lengthscales
+        grad[1: -1] += self.tophat.gradient(theta[1: -1])
+        # Noise
+        grad[-1] += self.horseshoe.gradient(theta[-1])
+        return grad

smac/facade/smac_bo_facade.py
@@ -1,5 +1,5 @@
 import numpy as np
-import george
+import skopt
 
 from smac.facade.smac_ac_facade import SMAC4AC
 from smac.epm.gp_default_priors import DefaultPrior
@@ -51,19 +51,22 @@ class SMAC4BO(SMAC4AC):
         if kwargs.get('model') is None:
             _, rng = get_rng(rng=kwargs.get("rng", None), run_id=kwargs.get("run_id", None), logger=None)
 
-            cov_amp = 2
             types, bounds = get_types(kwargs['scenario'].cs, instance_features=None)
             n_dims = len(types)
 
-            initial_ls = np.ones([n_dims])
-            exp_kernel = george.kernels.Matern52Kernel(initial_ls, ndim=n_dims)
-            kernel = cov_amp * exp_kernel
+            cov_amp = skopt.learning.gaussian_process.kernels.ConstantKernel(2.0, constant_value_bounds=(1e-10, 2))
+            exp_kernel = skopt.learning.gaussian_process.kernels.Matern(
+                np.ones([n_dims]),
+                [(np.exp(-10), np.exp(2)) for _ in range(n_dims)],
+                nu=2.5,
+            )
+            noise_kernel = skopt.learning.gaussian_process.kernels.WhiteKernel(
+                noise_level=1e-3,
+                noise_level_bounds=(1e-10, 2),
+            )
+            kernel = cov_amp * exp_kernel + noise_kernel
 
-            prior = DefaultPrior(len(kernel) + 1, rng=rng)
-
-            n_hypers = 3 * len(kernel)
-            if n_hypers % 2 == 1:
-                n_hypers += 1
+            prior = DefaultPrior(len(kernel.theta), rng=rng)
 
             if model_type == "gp":
                 model_class = GaussianProcess
@@ -71,9 +74,7 @@ class SMAC4BO(SMAC4AC):
                 model_kwargs = kwargs.get('model_kwargs', dict())
                 model_kwargs['kernel'] = kernel
                 model_kwargs['prior'] = prior
-                model_kwargs['normalize_input'] = True
-                model_kwargs['normalize_output'] = True
-                model_kwargs['normalize_input'] = True
+                model_kwargs['normalize_y'] = True
                 model_kwargs['seed'] = rng.randint(0, 2 ** 20)
             elif model_type == "gp_mcmc":
                 model_class = GaussianProcessMCMC
@@ -81,11 +82,15 @@ class SMAC4BO(SMAC4AC):
                 model_kwargs = kwargs.get('model_kwargs', dict())
                 model_kwargs['kernel'] = kernel
                 model_kwargs['prior'] = prior
-                model_kwargs['n_hypers'] = n_hypers
+
+                n_mcmc_walkers = 3 * len(kernel.theta)
+                if n_mcmc_walkers % 2 == 1:
+                    n_mcmc_walkers += 1
+
+                model_kwargs['n_mcmc_walkers'] = n_mcmc_walkers
                 model_kwargs['chain_length'] = 200
                 model_kwargs['burnin_steps'] = 100
-                model_kwargs['normalize_input'] = True
-                model_kwargs['normalize_output'] = True
+                model_kwargs['normalize_y'] = True
                 model_kwargs['seed'] = rng.randint(0, 2**20)
             else:
                 raise ValueError('Unknown model type %s' % model_type)

smac/optimizer/smbo.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 import typing
 
-import george
+import skopt.learning.gaussian_process
 
 from smac.configspace import ConfigurationSpace, Configuration, Constant,\
      CategoricalHyperparameter, UniformFloatHyperparameter, \
@@ -425,29 +425,33 @@ class SMBO(object):
                   max_depth=conf.get("max_depth", self.scenario.rf_max_depth))
 
         elif conf["model"] == "GP":
-            cov_amp = 2
-            n_dims = len(types)
+            cov_amp = skopt.learning.gaussian_process.kernels.ConstantKernel(2.0, constant_value_bounds=(1e-10, 2))
+            exp_kernel = skopt.learning.gaussian_process.kernels.Matern(
+                np.ones([len(types)]),
+                [(np.exp(-10), np.exp(2)) for _ in range(len(types))],
+                nu=2.5,
+            )
+            noise_kernel = skopt.learning.gaussian_process.kernels.WhiteKernel(
+                noise_level=1e-3,
+                noise_level_bounds=(1e-10, 2),
+            )
+            kernel = cov_amp * exp_kernel + noise_kernel
 
-            initial_ls = np.ones([n_dims])
-            exp_kernel = george.kernels.Matern52Kernel(initial_ls, ndim=n_dims)
-            kernel = cov_amp * exp_kernel
+            prior = DefaultPrior(len(kernel.theta) + 1, rng=self.rng)
 
-            prior = DefaultPrior(len(kernel) + 1, rng=self.rng)
-
-            n_hypers = 3 * len(kernel)
-            if n_hypers % 2 == 1:
-                n_hypers += 1
+            n_mcmc_walkers = 3 * len(kernel)
+            if n_mcmc_walkers % 2 == 1:
+                n_mcmc_walkers += 1
 
             model = GaussianProcessMCMC(
                 types=types,
                 bounds=bounds,
                 kernel=kernel,
                 prior=prior,
-                n_hypers=n_hypers,
+                n_mcmc_walkers=n_mcmc_walkers,
                 chain_length=200,
                 burnin_steps=100,
-                normalize_input=True,
-                normalize_output=True,
+                normalize_y=True,
                 seed=self.rng.randint(low=0, high=10000),
             )
 

test/test_epm/test_gp.py
@@ -1,7 +1,8 @@
 import unittest
 import unittest.mock
 
-import george
+import scipy.optimize
+import skopt.learning.gaussian_process.kernels
 import numpy as np
 import sklearn.datasets
 import sklearn.model_selection
@@ -11,25 +12,30 @@ from smac.epm.gp_default_priors import DefaultPrior
 
 
 def get_gp(n_dimensions, rs, noise=1e-3):
-    cov_amp = 2
+    cov_amp = skopt.learning.gaussian_process.kernels.ConstantKernel(2.0, constant_value_bounds=(1e-10, 2))
+    exp_kernel = skopt.learning.gaussian_process.kernels.Matern(
+        np.ones([n_dimensions]),
+        [(np.exp(-10), np.exp(2)) for _ in range(n_dimensions)],
+        nu=2.5,
+    )
+    noise_kernel = skopt.learning.gaussian_process.kernels.WhiteKernel(
+        noise_level=noise,
+        noise_level_bounds=(1e-10, 2),
+    )
+    kernel = cov_amp * exp_kernel + noise_kernel
 
-    initial_ls = np.ones([n_dimensions])
-    exp_kernel = george.kernels.Matern52Kernel(initial_ls, ndim=n_dimensions)
-    kernel = cov_amp * exp_kernel
-
-    prior = DefaultPrior(len(kernel) + 1, rng=rs)
-
-    n_hypers = 3 * len(kernel)
-    if n_hypers % 2 == 1:
-        n_hypers += 1
+    prior = DefaultPrior(len(kernel.theta) + 1, rng=rs)
 
     bounds = [(0., 1.) for _ in range(n_dimensions)]
     types = np.zeros(n_dimensions)
 
     model = GaussianProcess(
-        bounds=bounds, types=types, kernel=kernel,
-        prior=prior, seed=rs.randint(low=1, high=10000), noise=noise,
-        normalize_output=False, normalize_input=True,
+        bounds=bounds,
+        types=types,
+        kernel=kernel,
+        prior=prior,
+        seed=rs.randint(low=1, high=10000),
+        normalize_y=False,
     )
     return model
 
@@ -93,20 +99,20 @@ class TestGP(unittest.TestCase):
             [109.],
             [109.2]], dtype=np.float64)
         rs = np.random.RandomState(1)
-        model = get_gp(3, rs, noise=1e-9)
+        model = get_gp(3, rs)
         model.train(np.vstack((X, X, X, X, X, X, X, X)), np.vstack((y, y, y, y, y, y, y, y)))
 
         y_hat, mu_hat = model.predict(X)
         for y_i, y_hat_i, mu_hat_i in zip(
             y.reshape((1, -1)).flatten(), y_hat.reshape((1, -1)).flatten(), mu_hat.reshape((1, -1)).flatten(),
         ):
-            self.assertAlmostEqual(y_i, y_hat_i, delta=0.001)
-            self.assertAlmostEqual(mu_hat_i, 0)
+            self.assertAlmostEqual(y_i, y_hat_i, delta=2)
+            self.assertAlmostEqual(mu_hat_i, 0, delta=2)
 
         # Regression test that performance does not drastically decrease in the near future
         y_hat, mu_hat = model.predict(np.array([[10, 10, 10]]))
-        self.assertAlmostEqual(y_hat[0][0], 54.61250000002053)
-        self.assertAlmostEqual(mu_hat[0][0], 2.0)
+        self.assertAlmostEqual(y_hat[0][0], 1.2579769765743784e-05)
+        self.assertAlmostEqual(mu_hat[0][0], 8.701826756269622)
 
     def test_gp_on_sklearn_data(self):
         X, y = sklearn.datasets.load_boston(return_X_y=True)
@@ -116,7 +122,7 @@ class TestGP(unittest.TestCase):
         model = get_gp(X.shape[1], rs)
         cv = sklearn.model_selection.KFold(shuffle=True, random_state=rs, n_splits=2)
 
-        maes = [9.810206561442628563, 9.259995916707702291]
+        maes = [8.6835211971259218715, 9.087941533717404484]
 
         for i, (train_split, test_split) in enumerate(cv.split(X, y)):
             X_train = X[train_split]
@@ -127,3 +133,16 @@ class TestGP(unittest.TestCase):
             y_hat, mu_hat = model.predict(X_test)
             mae = np.mean(np.abs(y_hat - y_test), dtype=np.float128)
             self.assertAlmostEqual(mae, maes[i])
+
+    def test_nll(self):
+        rs = np.random.RandomState(1)
+        gp = get_gp(1, rs)
+        gp.train(np.array([[0], [1]]), np.array([0, 1]))
+        n_above_1 = 0
+        for i in range(1000):
+            theta = np.array([rs.uniform(1e-10, 10), rs.uniform(-10, 2), rs.uniform(-10, 1)])  # Values from the default prior
+            error = scipy.optimize.check_grad(lambda x: gp._nll(x)[0], lambda x: gp._nll(x)[1], theta, epsilon=1e-5)
+            # print(error, theta, gp._nll(theta)[1], scipy.optimize.approx_fprime(theta, lambda x: gp._nll(x)[0], 1e-5))
+            if error > 1:
+                n_above_1 += 1
+        self.assertLessEqual(n_above_1, 10)

test/test_epm/test_gp_mcmc.py
@@ -1,27 +1,33 @@
 import unittest
 import unittest.mock
 
-import george
 import numpy as np
 import sklearn.datasets
 import sklearn.model_selection
+import skopt.learning.gaussian_process
 
 from smac.epm.gaussian_process_mcmc import GaussianProcessMCMC
 from smac.epm.gp_default_priors import DefaultPrior
 
 
-def get_gp(n_dimensions, rs, noise=-8):
-    cov_amp = 2
+def get_gp(n_dimensions, rs, noise=1e-3):
+    cov_amp = skopt.learning.gaussian_process.kernels.ConstantKernel(2.0, constant_value_bounds=(1e-10, 2))
+    exp_kernel = skopt.learning.gaussian_process.kernels.Matern(
+        np.ones([n_dimensions]),
+        [(np.exp(-10), np.exp(2)) for _ in range(n_dimensions)],
+        nu=2.5,
+    )
+    noise_kernel = skopt.learning.gaussian_process.kernels.WhiteKernel(
+        noise_level=noise,
+        noise_level_bounds=(1e-10, 2),
+    )
+    kernel = cov_amp * exp_kernel + noise_kernel
 
-    initial_ls = np.ones([n_dimensions])
-    exp_kernel = george.kernels.Matern52Kernel(initial_ls, ndim=n_dimensions)
-    kernel = cov_amp * exp_kernel
+    prior = DefaultPrior(len(kernel.theta), rng=rs)
 
-    prior = DefaultPrior(len(kernel) + 1, rng=rs)
-
-    n_hypers = 3 * len(kernel)
-    if n_hypers % 2 == 1:
-        n_hypers += 1
+    n_mcmc_walkers = 3 * len(kernel.theta)
+    if n_mcmc_walkers % 2 == 1:
+        n_mcmc_walkers += 1
 
     bounds = [(0., 1.) for _ in range(n_dimensions)]
     types = np.zeros(n_dimensions)
@@ -31,13 +37,11 @@ def get_gp(n_dimensions, rs, noise=-8):
         bounds=bounds,
         kernel=kernel,
         prior=prior,
-        n_hypers=n_hypers,
+        n_mcmc_walkers=n_mcmc_walkers,
         chain_length=20,
         burnin_steps=100,
-        normalize_input=True,
-        normalize_output=True,
+        normalize_y=True,
         seed=rs.randint(low=1, high=10000),
-        noise=noise,
     )
     return model
 
@@ -101,7 +105,7 @@ class TestGPMCMC(unittest.TestCase):
             [109.],
             [109.2]], dtype=np.float64)
         rs = np.random.RandomState(1)
-        model = get_gp(3, rs, noise=-8)
+        model = get_gp(3, rs)
         model.train(np.vstack((X, X, X, X, X, X, X, X)), np.vstack((y, y, y, y, y, y, y, y)))
 
         y_hat, var_hat = model.predict(X)
@@ -109,14 +113,14 @@ class TestGPMCMC(unittest.TestCase):
             y.reshape((1, -1)).flatten(), y_hat.reshape((1, -1)).flatten(), var_hat.reshape((1, -1)).flatten(),
         ):
             # Chain length too short to get excellent predictions
-            self.assertAlmostEqual(y_i, y_hat_i, delta=0.1)
-            self.assertAlmostEqual(var_hat_i, 0, delta=0.2)
+            self.assertAlmostEqual(y_i, y_hat_i)
+            self.assertAlmostEqual(var_hat_i, 0)
 
         # Regression test that performance does not drastically decrease in the near future
         y_hat, var_hat = model.predict(np.array([[10, 10, 10]]))
         self.assertAlmostEqual(y_hat[0][0], 54.61249999999999)
         # Massive variance due to internally used law of total variances
-        self.assertAlmostEqual(var_hat[0][0], 8710.329996484437)
+        self.assertAlmostEqual(var_hat[0][0], 5726.732275219232)
 
     def test_gp_on_sklearn_data(self):
         X, y = sklearn.datasets.load_boston(return_X_y=True)
@@ -126,7 +130,7 @@ class TestGPMCMC(unittest.TestCase):
         model = get_gp(X.shape[1], rs)
         cv = sklearn.model_selection.KFold(shuffle=True, random_state=rs, n_splits=2)
 
-        maes = [9.370525409655151321, 9.056537966356946708]
+        maes = [7.430556296025081061, 7.723028018833807827]
 
         for i, (train_split, test_split) in enumerate(cv.split(X, y)):
             X_train = X[train_split]