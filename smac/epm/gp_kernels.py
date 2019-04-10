1d7ecbc1241c1ad2cf112b9c649adea1ca1eff12
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2019-04-10 09:33:40 +0200

    add Note

--- smac/epm/gp_kernels.py
+++ smac/epm/gp_kernels.py
@@ -227,6 +227,11 @@ class HammingKernel(MagicMixin, skopt.learning.gaussian_process.kernels.HammingK
             The gradient of the kernel k(X, X) with respect to the
             hyperparameter of the kernel. Only returned when eval_gradient
             is True.
+
+        Note
+        ----
+        Code partially copied from skopt (https://github.com/scikit-optimize).
+        Made small changes to only compute necessary values and use scikit-learn helper functions.
         """
         if self.operate_on is None:
             pass
@@ -258,11 +263,9 @@ class HammingKernel(MagicMixin, skopt.learning.gaussian_process.kernels.HammingK
 
             # dK / dL computation
             if np.iterable(length_scale) and length_scale.shape[0] > 1:
-                grad = (-np.expand_dims(kernel_prod, axis=-1) *
-                        np.array(indicator, dtype=np.float32))
+                grad = (-np.expand_dims(kernel_prod, axis=-1) * np.array(indicator, dtype=np.float32))
             else:
-                grad = -np.expand_dims(kernel_prod * np.sum(indicator, axis=2),
-                                       axis=-1)
+                grad = -np.expand_dims(kernel_prod * np.sum(indicator, axis=2), axis=-1)
 
             grad *= length_scale
 
