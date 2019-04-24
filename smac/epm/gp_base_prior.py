correct gradients and ln_prob

smac/epm/gp_base_prior.py
@@ -408,7 +408,7 @@ class LognormalPrior(Prior):
             else:
                 # derivative of log(1 / (x * s^2 * sqrt(2 pi)) * exp( - 0.5 * (log(x ) / s^2))^2))
                 # This is without the mean!!!
-                return -(self.sigma_square + math.log(theta)) / (self.sigma_square * (theta))
+                return -(self.sigma_square + math.log(theta)) / (self.sigma_square * (theta)) * theta
 
         else:
             raise NotImplementedError()
@@ -542,12 +542,7 @@ class GammaPrior(Prior):
             The gradient wrt to theta
         """
         if np.ndim(theta) == 0:
-            b = 1/self.scale
-            grad = b ** (-self.a -1)
-            grad *= theta ** (self.a - 2)
-            grad *= math.exp(-theta / b)
-            grad *= (-self.a * b + b + theta)
-            grad /= scsp.gamma(theta)
-            return grad
+            # Multiply by theta because of the chain rule...
+            return ((self.a - 1) / theta - (1 / self.scale)) * theta
         else:
             raise NotImplementedError()

test/test_epm/test_gp_priors.py
@@ -9,18 +9,6 @@ from smac.epm.gp_base_prior import TophatPrior, HorseshoePrior, LognormalPrior, 
 from smac.utils.constants import VERY_SMALL_NUMBER
 
 
-def compare_to_scalar(self, prior, bounds):
-    """
-    1) Samples 1000 configs within bounds
-    2) Computes lnprob using prior
-    3) ASSERTS lnprog for array is the same as lnprob for scalars
-    """
-    samples = np.linspace(bounds[0], bounds[1], 5)
-    lns = np.array([prior.lnprob(np.log(s)) for s in samples])
-    self.assertEqual(np.nansum(lns), prior.lnprob(np.log(samples)))
-    return lns
-
-
 def wrap_ln(theta, prior):
     return np.exp(prior.lnprob(np.log(theta)))
 
@@ -28,12 +16,7 @@ def wrap_ln(theta, prior):
 class TestTophatPrior(unittest.TestCase):
 
     def test_lnprob_sums_to_1(self):
-        rng = np.random.RandomState(1)
-        for i in range(100):
-            lower_bound = 2+rng.random_sample()*50
-            upper_bound = lower_bound + rng.random_sample()*50
-            prior = TophatPrior(lower_bound=lower_bound, upper_bound=upper_bound)
-            compare_to_scalar(self, prior, [lower_bound, upper_bound])
+        pass
 
         # Doesn't work yet
         #integral = scin.quad(wrap_ln, args=(TophatPrior(lower_bound=-1, upper_bound=1), ), a=-5, b=5)
@@ -61,21 +44,11 @@ class TestTophatPrior(unittest.TestCase):
     def test_lnprob_and_grad_array(self):
         prior = TophatPrior(lower_bound=-10, upper_bound=2)
 
-        # Legal arrays
-        for val in ([-1, -1], [0, 0], [1, 1]):
-            val = np.array(val)
-            self.assertEqual(prior.lnprob(val), 0)
-            np.testing.assert_array_almost_equal(prior.gradient(val), [0, 0])
-
-        # Boundary
-        self.assertEqual(prior.lnprob(np.array([-10, 2])), 0)
-        np.testing.assert_array_almost_equal(prior.gradient(np.array([-10, 2])), [0, 0])
-
-        # Values outside the boundary
-        for val in ([-20, 1], [-5, 20], [-11, -11], [3, 3]):
-            val = np.array(val)
-            self.assertTrue(np.isinf(prior.lnprob(val)))
-            np.testing.assert_array_almost_equal(prior.gradient(val), [0, 0])
+        val = np.array([-1, -1])
+        with self.assertRaises(NotImplementedError):
+            prior.lnprob(val)
+        with self.assertRaises(NotImplementedError):
+            prior.gradient(val)
 
     def test_sample_from_prior(self):
         prior = TophatPrior(lower_bound=-10, upper_bound=2, rng=np.random.RandomState(1))
@@ -83,19 +56,29 @@ class TestTophatPrior(unittest.TestCase):
         np.testing.assert_array_equal(samples >= -10, True)
         np.testing.assert_array_equal(samples <= 2, True)
         # Test that the rng is set
-        self.assertAlmostEqual(samples[0][0], -4.995735943569112)
+        self.assertAlmostEqual(samples[0], -4.995735943569112)
+
+    def test_sample_from_prior_shapes(self):
+        rng = np.random.RandomState(1)
+        lower_bound = 2 + rng.random_sample() * 50
+        upper_bound = lower_bound + rng.random_sample() * 50
+        prior = TophatPrior(lower_bound=lower_bound, upper_bound=upper_bound)
+        sample = prior.sample_from_prior(1)
+        self.assertEqual(sample.shape, (1,))
+        sample = prior.sample_from_prior(2)
+        self.assertEqual(sample.shape, (2,))
+        sample = prior.sample_from_prior(10)
+        self.assertEqual(sample.shape, (10,))
+        with self.assertRaises(ValueError):
+            prior.sample_from_prior(0)
+        with self.assertRaises(ValueError):
+            prior.sample_from_prior((2,))
 
 
 class TestHorseshoePrior(unittest.TestCase):
 
     def test_lnprob_sums_to_1(self):
-        rng = np.random.RandomState(1)
-        for i in range(100):
-            lower_bound = 1+rng.random_sample()
-            upper_bound = lower_bound + rng.random_sample()*5
-            scale = 0.5+rng.random_sample()
-            prior = HorseshoePrior(scale=scale)
-            compare_to_scalar(self, prior, [lower_bound, upper_bound])
+        pass
 
         # Does not work
         #integral = scin.quad(wrap_ln, args=(HorseshoePrior(scale=1), ), a=1e-10, b=5)#, b=100)
@@ -110,53 +93,60 @@ class TestHorseshoePrior(unittest.TestCase):
         self.assertEqual(prior.gradient(-1), -0.6089187456211098)
 
         # Boundary
-        self.assertTrue(np.isinf(prior.lnprob(0)))
-        self.assertTrue(np.isinf(prior.gradient(0)))
+        self.assertTrue(np.isinf(prior._lnprob(0)))
+        self.assertTrue(np.isinf(prior._gradient(0)))
 
     def test_lnprob_and_grad_array(self):
         prior = HorseshoePrior(scale=1)
 
-        # Legal array
         val = np.array([-1, -1])
-        self.assertEqual(prior.lnprob(val), 1.838240975836031)
-        np.testing.assert_array_almost_equal(prior.gradient(val), [-0.608919, -0.608919])
-
-        # Boundary
-        self.assertTrue(np.isinf(prior.lnprob(np.array([0, 2]))))
-        self.assertTrue(np.isinf(prior.gradient(np.array([0, 2]))).all())
+        with self.assertRaises(NotImplementedError):
+            prior.lnprob(val)
+        with self.assertRaises(NotImplementedError):
+            prior.gradient(val)
 
     def test_sample_from_prior(self):
         prior = HorseshoePrior(scale=1, rng=np.random.RandomState(1))
         samples = prior.sample_from_prior(10)
         # Test that the rng is set
-        self.assertAlmostEqual(samples[0][0], 1.0723988839129437)
+        self.assertAlmostEqual(samples[0], 1.0723988839129437)
+
+    def test_sample_from_prior_shapes(self):
+        rng = np.random.RandomState(1)
+        lower_bound = 2 + rng.random_sample() * 50
+        upper_bound = lower_bound + rng.random_sample() * 50
+        prior = TophatPrior(lower_bound=lower_bound, upper_bound=upper_bound)
+        sample = prior.sample_from_prior(1)
+        self.assertEqual(sample.shape, (1,))
+        sample = prior.sample_from_prior(2)
+        self.assertEqual(sample.shape, (2,))
+        sample = prior.sample_from_prior(10)
+        self.assertEqual(sample.shape, (10,))
+        with self.assertRaises(ValueError):
+            prior.sample_from_prior(0)
+        with self.assertRaises(ValueError):
+            prior.sample_from_prior((2,))
 
     def test_gradient(self):
-        for scale in (0.5, 1., 2.):
+        for scale in (0.1, 0.5, 1., 2.):
             prior = HorseshoePrior(scale=scale)
             # The function appears to be unstable above 15
             for theta in range(-20, 15):
                 if theta == 0:
                     continue
-                grad = prior.gradient(theta)
-                grad_vector = prior.gradient(np.array([theta]))
-                self.assertEqual(grad, grad_vector)
-                error = scipy.optimize.check_grad(prior.lnprob, prior.gradient, np.array([theta]), epsilon=1e-5)
+                error = scipy.optimize.check_grad(
+                    lambda x: prior.lnprob(x[0]),
+                    lambda x: prior.gradient(x[0]),
+                    np.array([theta]),
+                    epsilon=1e-5,
+                )
                 self.assertAlmostEqual(error, 0, delta=5)
 
 
 class TestGammaPrior(unittest.TestCase):
 
     def test_lnprob_sums_to_1(self):
-        rng = np.random.RandomState(1)
-        for i in range(100):
-            lower_bound = rng.randint(low=1, high=5)
-            upper_bound = lower_bound + 5
-            loc = lower_bound-0.5
-            a = rng.random_sample()
-            scale = 0.1 + rng.random_sample()
-            prior = GammaPrior(a=a, loc=loc, scale=scale)
-            compare_to_scalar(self, prior, [lower_bound, upper_bound])
+        pass
 
         integral = scin.quad(wrap_ln, args=(GammaPrior(a=1, loc=0, scale=1),), a=0, b=100)
         self.assertLess(integral[1], 0.1)
@@ -168,41 +158,36 @@ class TestGammaPrior(unittest.TestCase):
         # Legal scalar
         x = -1
         self.assertEqual(prior.lnprob(x), -0.46155023498761205)
-        self.assertEqual(prior.gradient(x), 0.7458042693520156)
+        self.assertEqual(prior.gradient(x), -1.2357588823428847)
 
     def test_lnprob_and_grad_array(self):
         prior = GammaPrior(a=0.5, scale=1/2, loc=0)
-
-        # Legal array
         val = np.array([-1, -1])
-        print(prior.lnprob(val))
-        self.assertEqual(prior.lnprob(val), -0.9231004699752241)
-        np.testing.assert_array_almost_equal(prior.gradient(val), [0.7458042693520156, 0.7458042693520156])
+        with self.assertRaises(NotImplementedError):
+            prior.lnprob(val)
+        with self.assertRaises(NotImplementedError):
+            prior.gradient(val)
 
     def test_gradient(self):
         for scale in (0.5, 1., 2.):
-            prior = GammaPrior(a=0.5, scale=scale, loc=0)
-            # The function appears to be unstable above 15
-            for theta in range(-20, 15):
+            prior = GammaPrior(a=2, scale=scale, loc=0)
+            # The function appears to be unstable above 10
+            for theta in np.arange(1e-15, 10, 0.01):
                 if theta == 0:
                     continue
-                grad = prior.gradient(theta)
-                grad_vector = prior.gradient(np.array([theta]))
-                self.assertEqual(grad, grad_vector)
-                error = scipy.optimize.check_grad(prior.lnprob, prior.gradient, np.array([theta]), epsilon=1e-5)
-                self.assertAlmostEqual(error, 0, delta=5)
+                error = scipy.optimize.check_grad(
+                    lambda x: prior.lnprob(x[0]),
+                    lambda x: prior.gradient(x[0]),
+                    np.array([theta]),
+                    epsilon=1e-5,
+                )
+                self.assertAlmostEqual(error, 0, delta=5, msg=str(theta))
 
 
 class TestLogNormalPrior(unittest.TestCase):
 
     def test_lnprob_sums_to_1(self):
-        rng = np.random.RandomState(1)
-        for i in range(100):
-            lower_bound = 0.001
-            upper_bound = 5
-            scale = 0.1+rng.random_sample()
-            prior = LognormalPrior(mean=0, sigma=scale)
-            compare_to_scalar(self, prior, [lower_bound, upper_bound])
+        pass
 
         integral = scin.quad(wrap_ln, args=(LognormalPrior(mean=0, sigma=1), ), a=1e-10, b=100)
         self.assertLess(integral[1], 0.1)
@@ -215,23 +200,19 @@ class TestLogNormalPrior(unittest.TestCase):
             for theta in range(0, 15):
                 # Gradient approximation becomes unstable when going closer to zero
                 theta += 1e-2
-                grad = prior.gradient(theta)
-                grad_vector = prior.gradient(np.array([theta]))
-                self.assertEqual(grad, grad_vector)
-                error = scipy.optimize.check_grad(prior.lnprob, prior.gradient, np.array([theta]), epsilon=1e-5)
+                error = scipy.optimize.check_grad(
+                    lambda x: prior.lnprob(x[0]),
+                    lambda x: prior.gradient(x[0]),
+                    np.array([theta]),
+                    epsilon=1e-5,
+                )
                 self.assertAlmostEqual(error, 0, delta=5, msg=theta)
 
 
 class TestSoftTopHatPrior(unittest.TestCase):
 
     def test_lnprob_sums_to_1(self):
-        rng = np.random.RandomState(1)
-        for i in range(100):
-            lower_bound = -rng.randint(low=-50, high=-6)
-            upper_bound = -lower_bound
-            exponent = 2
-            prior = SoftTopHatPrior(lower_bound=lower_bound+5, upper_bound=upper_bound-5, exponent=exponent)
-            compare_to_scalar(self, prior, [lower_bound, upper_bound])
+        pass
 
         # Does not work
         #integral = scin.quad(wrap_ln, args=(SoftTopHatPrior(lower_bound=1, upper_bound=5, exponent=2),), a=-10, b=100)