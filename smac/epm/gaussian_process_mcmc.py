fix GPMCMC normalization of y values

smac/epm/gaussian_process_mcmc.py
@@ -117,6 +117,9 @@ class GaussianProcessMCMC(BaseModel):
             hyperparameter specified in the kernel.
         """
         X = self._impute_inactive(X)
+        if self.normalize_y:
+            y = self._normalize_y(y)
+
         self.gp = GaussianProcessRegressor(
             kernel=self.kernel,
             normalize_y=self.normalize_y,
@@ -198,10 +201,10 @@ class GaussianProcessMCMC(BaseModel):
                     max_depth=10,
                     rng=self.rng,
                 )
-                print('hypers', 'log space', self.p0, 'regular space', np.exp(self.p0))
                 indices = [int(np.rint(ind)) for ind in np.linspace(start=0, stop=len(samples) - 1, num=10)]
                 self.hypers = samples[indices]
                 self.p0 = self.hypers.mean(axis=0)
+                print('hypers', 'log space', self.p0, 'regular space', np.exp(self.p0))
             else:
                 raise ValueError(self.mcmc_sampler)
 
@@ -215,6 +218,11 @@ class GaussianProcessMCMC(BaseModel):
         self.models = []
         for sample in self.hypers:
 
+            if (sample < -50).any():
+                sample[sample < -50] = -50
+            if (sample > 50).any():
+                sample[sample > 50] = 50
+
             # Instantiate a GP for each hyperparameter configuration
             kernel = deepcopy(self.kernel)
             kernel.theta = sample
@@ -223,7 +231,7 @@ class GaussianProcessMCMC(BaseModel):
                 types=self.types,
                 bounds=self.bounds,
                 kernel=kernel,
-                normalize_y=self.normalize_y,
+                normalize_y=False,
                 seed=self.rng.randint(low=0, high=10000),
             )
             try:
@@ -236,14 +244,21 @@ class GaussianProcessMCMC(BaseModel):
             kernel = deepcopy(self.kernel)
             kernel.theta = self.p0
             model = GaussianProcess(
+                configspace=self.configspace,
                 types=self.types,
                 bounds=self.bounds,
                 kernel=kernel,
-                normalize_y=self.normalize_y,
+                normalize_y=False,
                 seed=self.rng.randint(low=0, high=10000),
             )
             model._train(X, y, do_optimize=False)
             self.models.append(model)
+
+        if self.normalize_y:
+            for model in self.models:
+                model.normalize_y = True
+                model.mean_y_ = self.mean_y_
+                model.std_y_ = self.std_y_
 
         self.is_trained = True
         print('#LL evaluations', self._n_ll_evals)
@@ -268,12 +283,10 @@ class GaussianProcessMCMC(BaseModel):
 
         # Bound the hyperparameter space to keep things sane. Note all
         # hyperparameters live on a log scale
-        if np.ndim(theta) == 0:
-            if (theta < -20) or (theta > 20):
-                return -np.inf
-        else:
-            if ((theta < -20) | (theta > 20)).any():
-                return -np.inf
+        if (theta < -50).any():
+            theta[theta < -50] = -50
+        if (theta > 50).any():
+            theta[theta > 50] = 50
 
         try:
             lml = self.gp.log_marginal_likelihood(theta)

test/test_epm/test_gp_mcmc.py
@@ -175,7 +175,7 @@ class TestGPMCMC(unittest.TestCase):
         self.assertAlmostEqual(y_hat[0][0], 54.613410745846785, delta=0.1)
         # Massive variance due to internally used law of total variances, also a massive difference locally and on
         # travis-ci
-        self.assertLessEqual(abs(var_hat[0][0] - 5555), 100, msg=str(var_hat))
+        self.assertLessEqual(abs(var_hat[0][0]) - 860, 100, msg=str(var_hat))
 
     def test_gp_on_sklearn_data(self):
         X, y = sklearn.datasets.load_boston(return_X_y=True)
@@ -185,7 +185,7 @@ class TestGPMCMC(unittest.TestCase):
         model = get_gp(X.shape[1], rs, noise=1e-10, normalize_y=True)
         cv = sklearn.model_selection.KFold(shuffle=True, random_state=rs, n_splits=2)
 
-        maes = [6.529696318466649324, 7.3709784671630151423]
+        maes = [6.7831378207124642665, 7.6098846993685922186]
 
         for i, (train_split, test_split) in enumerate(cv.split(X, y)):
             X_train = X[train_split]
@@ -204,9 +204,13 @@ class TestGPMCMC(unittest.TestCase):
         rng = np.random.RandomState(1)
         gp = get_gp(n_dimensions=1, rs=rng, noise=1e-10, normalize_y=False)
         gp._train(X, y, do_optimize=False)
+        self.assertFalse(gp.models[0].normalize_y)
+        self.assertFalse(hasattr(gp.models[0], 'mean_y_'))
         mu_hat, var_hat = gp.predict(X_test)
         gp_norm = get_gp(n_dimensions=1, rs=rng, noise=1e-10, normalize_y=True)
         gp_norm._train(X, y, do_optimize=False)
+        self.assertTrue(gp_norm.models[0].normalize_y)
+        self.assertTrue(hasattr(gp_norm.models[0], 'mean_y_'))
         mu_hat_prime, var_hat_prime = gp_norm.predict(X_test)
         np.testing.assert_array_almost_equal(mu_hat, mu_hat_prime, decimal=4)
         np.testing.assert_array_almost_equal(var_hat, var_hat_prime, decimal=4)