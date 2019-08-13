Fix GPMCMC normalization of y values (#521)
* fix GPMCMC normalization of y values

* replace variable by constant

* loosen test requirements

* update fixtures

* add comments

smac/epm/gaussian_process_mcmc.py
@@ -117,9 +117,18 @@ class GaussianProcessMCMC(BaseModel):
             hyperparameter specified in the kernel.
         """
         X = self._impute_inactive(X)
+        if self.normalize_y:
+            # A note on normalization for the Gaussian process with MCMC:
+            # Scikit-learn uses a different "normalization" than we use in SMAC3. Scikit-learn normalizes the data to
+            # have zero mean, while we normalize it to have zero mean unit variance. To make sure the scikit-learn GP
+            # behaves the same when we use it directly or indirectly (through the gaussian_process.py file), we
+            # normalize the data here. Then, after the individual GPs are fit, we inject the statistics into them so
+            # they unnormalize the data at prediction time.
+            y = self._normalize_y(y)
+
         self.gp = GaussianProcessRegressor(
             kernel=self.kernel,
-            normalize_y=self.normalize_y,
+            normalize_y=False,
             optimizer=None,
             n_restarts_optimizer=-1,  # Do not use scikit-learn's optimization routine
             alpha=0,  # Governed by the kernel
@@ -198,10 +207,10 @@ class GaussianProcessMCMC(BaseModel):
                     max_depth=10,
                     rng=self.rng,
                 )
-                print('hypers', 'log space', self.p0, 'regular space', np.exp(self.p0))
                 indices = [int(np.rint(ind)) for ind in np.linspace(start=0, stop=len(samples) - 1, num=10)]
                 self.hypers = samples[indices]
                 self.p0 = self.hypers.mean(axis=0)
+                print('hypers', 'log space', self.p0, 'regular space', np.exp(self.p0))
             else:
                 raise ValueError(self.mcmc_sampler)
 
@@ -215,6 +224,11 @@ class GaussianProcessMCMC(BaseModel):
         self.models = []
         for sample in self.hypers:
 
+            if (sample < -50).any():
+                sample[sample < -50] = -50
+            if (sample > 50).any():
+                sample[sample > 50] = 50
+
             # Instantiate a GP for each hyperparameter configuration
             kernel = deepcopy(self.kernel)
             kernel.theta = sample
@@ -223,7 +237,7 @@ class GaussianProcessMCMC(BaseModel):
                 types=self.types,
                 bounds=self.bounds,
                 kernel=kernel,
-                normalize_y=self.normalize_y,
+                normalize_y=False,
                 seed=self.rng.randint(low=0, high=10000),
             )
             try:
@@ -236,14 +250,23 @@ class GaussianProcessMCMC(BaseModel):
             kernel = deepcopy(self.kernel)
             kernel.theta = self.p0
             model = GaussianProcess(
+                configspace=self.configspace,
                 types=self.types,
                 bounds=self.bounds,
                 kernel=kernel,
-                normalize_y=self.normalize_y,
+                normalize_y=False,
                 seed=self.rng.randint(low=0, high=10000),
             )
             model._train(X, y, do_optimize=False)
             self.models.append(model)
+
+        if self.normalize_y:
+            # Inject the normalization statistics into the individual models. Setting normalize_y to True makes the
+            # individual GPs unnormalize the data at predict time.
+            for model in self.models:
+                model.normalize_y = True
+                model.mean_y_ = self.mean_y_
+                model.std_y_ = self.std_y_
 
         self.is_trained = True
         print('#LL evaluations', self._n_ll_evals)
@@ -268,12 +291,10 @@ class GaussianProcessMCMC(BaseModel):
 
         # Bound the hyperparameter space to keep things sane. Note all
         # hyperparameters live on a log scale
-        if np.ndim(theta) == 0:
-            if (theta < -20) or (theta > 20):
-                return -np.inf
-        else:
-            if ((theta < -20) | (theta > 20)).any():
-                return -np.inf
+        if (theta < -50).any():
+            theta[theta < -50] = -50
+        if (theta > 50).any():
+            theta[theta > 50] = 50
 
         try:
             lml = self.gp.log_marginal_likelihood(theta)