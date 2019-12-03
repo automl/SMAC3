FIX incumbent computation

smac/optimizer/epm_configuration_chooser.py
@@ -81,6 +81,13 @@ class EPMChooser(object):
 
         self.predict_incumbent = predict_incumbent
 
+    def _collect_data_to_train_model(self):
+        X, Y = self.rh2EPM.transform(self.runhistory)
+        return X, Y
+
+    def _get_evaluated_configs(self):
+        return len(self.runhistory.data)
+
     def choose_next(self, incumbent_value: float = None):
         """Choose next candidate solution with Bayesian optimization. The
         suggested configurations depend on the argument ``acq_optimizer`` to
@@ -101,8 +108,7 @@ class EPMChooser(object):
         """
 
         self.logger.debug("Search for next configuration")
-
-        X, Y = self.rh2EPM.transform(self.runhistory)
+        X, Y = self._collect_data_to_train_model()
 
         if X.shape[0] == 0:
             # Only return a single point to avoid an overly high number of
@@ -110,7 +116,6 @@ class EPMChooser(object):
             return self._random_search.maximize(
                 runhistory=self.runhistory, stats=self.stats, num_points=1
             )
-
         self.model.train(X, Y)
 
         if incumbent_value is None:
@@ -127,7 +132,7 @@ class EPMChooser(object):
             eta=incumbent_value,
             incumbent=incumbent,
             incumbent_array=incumbent_array,
-            num_data=len(self.runhistory.data),
+            num_data=len(self._get_evaluated_configs()),
             X=X,
         )
 
@@ -151,7 +156,7 @@ class EPMChooser(object):
         np.ndarry
         Configuration
         """
-        all_configs = self.runhistory.get_all_configs()
+        all_configs = self._get_evaluated_configs()
         if self.predict_incumbent:
             configs_array = convert_configurations_to_array(all_configs)
             costs = list(map(
@@ -180,3 +185,65 @@ class EPMChooser(object):
             incumbent_value = incumbent_value[0][0]
 
         return incumbent, incumbent_array, incumbent_value
+
+
+class EPMChooserBudgets(EPMChooser):
+
+    def __init__(self,
+                 scenario: Scenario,
+                 stats: Stats,
+                 runhistory: RunHistory,
+                 runhistory2epm: AbstractRunHistory2EPM,
+                 model: RandomForestWithInstances,
+                 acq_optimizer: AcquisitionFunctionMaximizer,
+                 acquisition_func: AbstractAcquisitionFunction,
+                 rng: np.random.RandomState,
+                 restore_incumbent: Configuration = None,
+                 random_configuration_chooser: typing.Union[
+                     ChooserNoCoolDown, ChooserLinearCoolDown] = ChooserNoCoolDown(2.0),
+                 predict_incumbent: bool = True,
+                 min_samples_model: int = 10):
+        """
+        Works with budgets. To choose the next configuration to evaluate it only considers samples from the highest
+        budget with more than min_samples_model points.
+
+        Parameters
+        ----------
+        min_samples_model: int
+            Minimum number of samples to build a model
+        """
+        super().__init__(scenario=scenario,
+                         stats=stats,
+                         runhistory=runhistory,
+                         runhistory2epm=runhistory2epm,
+                         model=model,
+                         acquisition_func=acquisition_func,
+                         acq_optimizer=acq_optimizer,
+                         rng=rng,
+                         restore_incumbent=restore_incumbent,
+                         random_configuration_chooser=random_configuration_chooser,
+                         predict_incumbent=predict_incumbent,
+                         )
+        self.min_samples_model = min_samples_model
+        self.currently_considered_budgets = None
+
+    def _collect_data_to_train_model(self):
+        # if we use a float value as a budget, we want to train the model only on the highest budget
+        available_budgets = []
+        for i in self.runhistory.data.keys():
+            available_budgets.append(i.budget)
+
+        # Sort available budgets from highest to lowest budget
+        available_budgets = sorted(list(set(available_budgets)), reverse=True)
+
+        # Get #points per budget and if there are enough samples, then build a model
+        for b in available_budgets:
+            X, Y = self.rh2EPM.transform(self.runhistory, budget_subset=[b, ])
+            if X.shape[0] >= self.min_samples_model:
+                self.currently_considered_budgets = [b, ]
+                return X, Y
+
+        return np.empty(shape=[0, 0]), np.empty(shape=[0, ])
+
+    def _get_evaluated_configs(self):
+        return self.runhistory.get_all_configs_per_budget(budget_subset=self.currently_considered_budgets)