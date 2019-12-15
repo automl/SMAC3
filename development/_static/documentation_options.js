8efefd192d03cb4f3f76540a794a987ab4dfc9a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-15 18:56:22 +0000

    Deploy automl/SMAC3 to github.com/automl/SMAC3.git:gh-pages

--- development/_static/documentation_options.js
+++ development/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '0.11.2dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
