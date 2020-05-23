dbf41d98456e37eb71ebf06458f89afe46a198ba
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2020-05-23 12:34:20 +0200

    prepare new release (#656)

--- smac/__init__.py
+++ smac/__init__.py
@@ -5,7 +5,7 @@ import sys
 import lazy_import
 from smac.utils import dependencies
 
-__version__ = '0.12.2dev'
+__version__ = '0.12.2'
 __author__ = 'Marius Lindauer, Matthias Feurer, Katharina Eggensperger, Joshua Marben, André Biedenkapp, Aaron Klein,'\
     'Stefan Falkner and Frank Hutter'
 
