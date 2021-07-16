ef3a3f58973c15d4eebbbb0b515a9c6a7e8ff68a
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2021-07-16 13:51:04 +0200

    drop unit test

--- test/test_facade/test_smac4bo_facade.py
+++ test/test_facade/test_smac4bo_facade.py
@@ -4,6 +4,7 @@ import unittest.mock
 
 from ConfigSpace.hyperparameters import UniformFloatHyperparameter
 
+import numpy as np
 from smac.configspace import ConfigurationSpace
 
 from smac.facade.smac_bo_facade import SMAC4BO
@@ -27,20 +28,3 @@ class TestSMACFacade(unittest.TestCase):
         for output_dir in self.output_dirs:
             if output_dir:
                 shutil.rmtree(output_dir, ignore_errors=True)
-
-    def test_exchange_sobol_for_lhd(self):
-        cs = ConfigurationSpace()
-        for i in range(40):
-            cs.add_hyperparameter(UniformFloatHyperparameter('x%d' % (i + 1), 0, 1))
-        scenario = Scenario({'cs': cs, 'run_obj': 'quality'})
-        facade = SMAC4BO(scenario=scenario)
-        self.assertIsInstance(facade.solver.initial_design, SobolDesign)
-        cs.add_hyperparameter(UniformFloatHyperparameter('x41', 0, 1))
-
-        with self.assertRaisesRegex(
-            ValueError,
-            'Sobol sequence" can only handle up to 40 dimensions. Please use a different initial design, such as '
-            '"the Latin Hypercube design"',
-        ):
-            SMAC4BO(scenario=scenario)
-        self.output_dirs.append(scenario.output_dir)
--- test/test_facade/test_smac4hpo_facade.py
+++ test/test_facade/test_smac4hpo_facade.py
@@ -28,19 +28,3 @@ class TestSMACFacade(unittest.TestCase):
         for output_dir in self.output_dirs:
             if output_dir:
                 shutil.rmtree(output_dir, ignore_errors=True)
-
-    def test_exchange_sobol_for_lhd(self):
-        cs = ConfigurationSpace()
-        for i in range(40):
-            cs.add_hyperparameter(UniformFloatHyperparameter('x%d' % (i + 1), 0, 1))
-        scenario = Scenario({'cs': cs, 'run_obj': 'quality'})
-        facade = SMAC4HPO(scenario=scenario)
-        self.assertIsInstance(facade.solver.initial_design, SobolDesign)
-        cs.add_hyperparameter(UniformFloatHyperparameter('x41', 0, 1))
-        with self.assertRaisesRegex(
-                ValueError,
-                'Sobol sequence" can only handle up to 40 dimensions. Please use a different initial design, such as '
-                '"the Latin Hypercube design"',
-        ):
-            SMAC4HPO(scenario=scenario)
-        self.output_dirs.append(scenario.output_dir)
