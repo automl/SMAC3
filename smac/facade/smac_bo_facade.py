8f56af3b61310c9a543ea217957685c0d1e72027
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2021-07-16 12:07:13 +0200

    fix tests and docstrings

--- smac/facade/smac_bo_facade.py
+++ smac/facade/smac_bo_facade.py
@@ -59,11 +59,11 @@ class SMAC4BO(SMAC4AC):
 
         scenario = kwargs['scenario']
 
-        if len(scenario.cs.get_hyperparameters()) <= 40:
+        if len(scenario.cs.get_hyperparameters()) <= 21201:
             kwargs['initial_design'] = kwargs.get('initial_design', SobolDesign)
         else:
             raise ValueError(
-                'The default initial design "Sobol sequence" can only handle up to 40 dimensions. '
+                'The default initial design "Sobol sequence" can only handle up to 21201 dimensions. '
                 'Please use a different initial design, such as "the Latin Hypercube design".',
             )
         kwargs['runhistory2epm'] = kwargs.get('runhistory2epm', RunHistory2EPM4Cost)
