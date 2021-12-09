a898f6e574a9f8eee92fe3e7bc8d0b88bd259179
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-12-09 14:36:25 +0100

    added schaffer solution

--- test/test_runhistory/test_multiobjective.py
+++ test/test_runhistory/test_multiobjective.py
@@ -2,17 +2,43 @@ __copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
 __license__ = "3-clause BSD"
 
 import unittest
+import numpy as np
 
 from smac.configspace import ConfigurationSpace
 from ConfigSpace.hyperparameters import \
     CategoricalHyperparameter, UniformFloatHyperparameter, UniformIntegerHyperparameter
 
 
+def schaffer_func1(x):
+    return np.square(x)
+
+
+def schaffer_func2(x):
+    return np.square(x - 2)
+
+
+def schaffer_n1(x):
+    return schaffer_func1(x), schaffer_func2(x)
+
+
+def schaffer_pareto(x):
+    f1 = schaffer_func1(x)
+    pareto = lambda x: np.square(np.sqrt(x) - 2)  # substituted x in f2 with x=np.sqrt(f1)
+    return f1, pareto(f1)
+
+
 class MultiObjectiveTest(unittest.TestCase):
     def test_Schaffer_no1(self):
         def tae(x):
             """x is a single continous hyperparameter."""
-            return {'metric1': x ** 2, 'metric2': (x - 2) ** 2}
+            f1, f2 = schaffer_n1(x)
+            return {'metric1': f1, 'metric2': f2}
+
+        # x should be evaluated in the inteval [-2, 2]
+        A = 2
+        n = 100
+        X = np.linspace(-A, A, n)
 
         cs = ConfigurationSpace()
-        UniformFloatHyperparameter('x', lower=0., upper=1.)
+        UniformFloatHyperparameter('x', lower=-A, upper=A)
+
