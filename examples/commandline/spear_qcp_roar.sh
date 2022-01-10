b163ed013a814cbc1c86bc7ff30800f4fc6f864f
renesass <mail@renesass.de>
2022-01-10 14:50:10 +0100

    Try to make it run

--- examples/commandline/spear_qcp_roar.sh
+++ examples/commandline/spear_qcp_roar.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
 # An example showing how to use commandline to optimization with ROAR facade
+echo $PATH
+which python
 python ../../scripts/smac.py --scenario spear_qcp/scenario.txt --verbose DEBUG --mode ROAR
