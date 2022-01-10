230be779ceed2022733f08a6677a36212bf65a8b
renesass <mail@renesass.de>
2022-01-10 15:00:59 +0100

    Try to make it run

--- scripts/smac.py
+++ scripts/smac.py
@@ -5,10 +5,10 @@ import sys
 import os
 import inspect
 
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
-cmd_folder = os.path.realpath(os.path.join(cmd_folder, ".."))
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
+#cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+#cmd_folder = os.path.realpath(os.path.join(cmd_folder, ".."))
+# if cmd_folder not in sys.path:
+#    sys.path.insert(0, cmd_folder)
 
 from smac.smac_cli import SMACCLI
 
