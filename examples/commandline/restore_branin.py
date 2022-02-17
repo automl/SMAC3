1a85646ad3b7fe21b0ce1cf5cd0be683c77800c1
René Sass <mail@renesass.de>
2022-02-17 14:43:41 +0100

    Maybe now?

--- examples/commandline/restore_branin.py
+++ examples/commandline/restore_branin.py
@@ -7,8 +7,6 @@ budget. This will also work for SMAC runs that have crashed and are continued.
 """
 
 import logging
-logging.basicConfig(level=logging.INFO)
-
 import os
 
 from smac.facade.smac_ac_facade import SMAC4AC
@@ -16,6 +14,8 @@ from smac.runhistory.runhistory import RunHistory
 from smac.scenario.scenario import Scenario
 from smac.stats.stats import Stats
 from smac.utils.io.traj_logging import TrajLogger
+
+logging.basicConfig(level=logging.INFO)
 
 
 __copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
@@ -25,14 +25,15 @@ __license__ = "3-clause BSD"
 if "__main__" == __name__:
 
     # Initialize scenario, using runcount_limit as budget.
-    origiginal_scenario_dict = {
-        'algo': 'python branin.py',
-        'paramfile': 'branin/configspace.pcs',
-        'run_obj': 'quality',
-        'runcount_limit': 25,
-        'deterministic': True,
-        'output_dir': 'restore_me'}
-    original_scenario = Scenario(origiginal_scenario_dict)
+    original_scenario_dict = {
+        "algo": "python branin.py",
+        "paramfile": "branin/configspace.pcs",
+        "run_obj": "quality",
+        "runcount_limit": 25,
+        "deterministic": True,
+        "output_dir": "restore_me",
+    }
+    original_scenario = Scenario(original_scenario_dict)
 
     smac = SMAC4AC(scenario=original_scenario, run_id=1)
     smac.optimize()
@@ -41,7 +42,7 @@ if "__main__" == __name__:
 
     # Now the output is in the folder 'restore_me/run_1' (or whatever run_id has
     # been passed to the SMAC-object above)
-    old_output_dir = os.path.join(original_scenario.output_dir, 'run_1')
+    old_output_dir = os.path.join(original_scenario.output_dir, "run_1")
 
     # We could simply modify the scenario-object, stored in
     # 'smac.solver.scenario' and start optimization again:
@@ -52,9 +53,12 @@ if "__main__" == __name__:
     # Or, to show the whole process of recovering a SMAC-run from the output
     # directory, create a new scenario with an extended budget:
     new_scenario = Scenario(
-        origiginal_scenario_dict,
-        cmd_options={'runcount_limit': 50,  # overwrite these args
-                     'output_dir': 'restored'})
+        original_scenario_dict,
+        cmd_options={
+            "runcount_limit": 50,  # overwrite these args
+            "output_dir": "restored",
+        },
+    )
 
     # We load the runhistory
     rh_path = os.path.join(old_output_dir, "runhistory.json")
@@ -68,19 +72,19 @@ if "__main__" == __name__:
 
     # And the trajectory
     traj_path = os.path.join(old_output_dir, "traj_aclib2.json")
-    trajectory = TrajLogger.read_traj_aclib_format(
-        fn=traj_path,
-        cs=new_scenario.cs)
+    trajectory = TrajLogger.read_traj_aclib_format(fn=traj_path, cs=new_scenario.cs)
     incumbent = trajectory[-1]["incumbent"]
 
     # Now we can initialize SMAC with the recovered objects and restore the
     # state where we left off. By providing stats and a restore_incumbent, SMAC
     # automatically detects the intention of restoring a state.
-    smac = SMAC4AC(scenario=new_scenario,
-                   runhistory=runhistory,
-                   stats=stats,
-                   restore_incumbent=incumbent,
-                   run_id=1)
+    smac = SMAC4AC(
+        scenario=new_scenario,
+        runhistory=runhistory,
+        stats=stats,
+        restore_incumbent=incumbent,
+        run_id=1,
+    )
 
     # Because we changed the output_dir, we might want to copy the old
     # trajectory-file (runhistory and stats will be complete, but trajectory is
--- examples/commandline/spear_qcp_smac.sh
+++ examples/commandline/spear_qcp_smac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # An example showing how to use commandline to optimization with SMAC4AC facade
-python ../../scripts/smac.py --scenario spear_qcp/scenario.txt --verbose DEBUG --mode SMAC4AC
+python ./scripts/smac.py --scenario examples/python/spear_qcp/scenario.txt --verbose DEBUG --mode SMAC4AC
