d093f651a6cacf0b8ddedc13c9a73243c3a7f881
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-03-31 11:30:25 +0200

    Fix indentation (flake8)

--- smac/facade/roar_facade.py
+++ smac/facade/roar_facade.py
@@ -85,26 +85,27 @@ class ROAR(SMAC4AC):
 
     """
 
-    def __init__(self,
-         scenario: Scenario,
-         tae_runner: typing.Optional[
-             typing.Union[typing.Type[BaseRunner], typing.Callable]
-         ] = None,
-         tae_runner_kwargs: typing.Optional[typing.Dict] = None,
-         runhistory: RunHistory = None,
-         intensifier: typing.Optional[typing.Type[AbstractRacer]] = None,
-         intensifier_kwargs: typing.Optional[typing.Dict] = None,
-         acquisition_function_optimizer: typing.Optional[typing.Type[AcquisitionFunctionMaximizer]] = None,
-         acquisition_function_optimizer_kwargs: typing.Optional[dict] = None,
-         initial_design: typing.Optional[typing.Type[InitialDesign]] = None,
-         initial_design_kwargs: typing.Optional[dict] = None,
-         initial_configurations: typing.List[Configuration] = None,
-         stats: Stats = None,
-         rng: typing.Optional[typing.Union[int, np.random.RandomState]] = None,
-         run_id: typing.Optional[int] = None,
-         dask_client: typing.Optional[dask.distributed.Client] = None,
-         n_jobs: typing.Optional[int] = 1,
-     ):
+    def __init__(
+        self,
+        scenario: Scenario,
+        tae_runner: typing.Optional[
+            typing.Union[typing.Type[BaseRunner], typing.Callable]
+        ] = None,
+        tae_runner_kwargs: typing.Optional[typing.Dict] = None,
+        runhistory: RunHistory = None,
+        intensifier: typing.Optional[typing.Type[AbstractRacer]] = None,
+        intensifier_kwargs: typing.Optional[typing.Dict] = None,
+        acquisition_function_optimizer: typing.Optional[typing.Type[AcquisitionFunctionMaximizer]] = None,
+        acquisition_function_optimizer_kwargs: typing.Optional[dict] = None,
+        initial_design: typing.Optional[typing.Type[InitialDesign]] = None,
+        initial_design_kwargs: typing.Optional[dict] = None,
+        initial_configurations: typing.List[Configuration] = None,
+        stats: Stats = None,
+        rng: typing.Optional[typing.Union[int, np.random.RandomState]] = None,
+        run_id: typing.Optional[int] = None,
+        dask_client: typing.Optional[dask.distributed.Client] = None,
+        n_jobs: typing.Optional[int] = 1,
+    ):
         self.logger = logging.getLogger(self.__module__ + "." + self.__class__.__name__)
 
         scenario.acq_opt_challengers = 1  # type: ignore[attr-defined] # noqa F821
