8ca230e58d2195bc3cb08f5cf32d937a27ee3922
dengdifan <difandeng@gmail.com>
2022-05-05 10:34:41 +0200

    test and examples for EIPS acquisition function

--- smac/epm/uncorrelated_mo_rf_with_instances.py
+++ smac/epm/uncorrelated_mo_rf_with_instances.py
@@ -17,59 +17,13 @@ class UncorrelatedMultiObjectiveRandomForestWithInstances(UncorrelatedMultiObjec
     underlying forest model are mandatory. All other hyperparameters to
     the random forest can be passed via kwargs. Consult the documentation of
     the random forest for the hyperparameters and their meanings.
-
-
-    Parameters
-    ----------
-    target_names : list
-        List of str, each entry is the name of one target dimension. Length
-        of the list will be ``n_objectives``.
-    types : List[int]
-        Specifies the number of categorical values of an input dimension where
-        the i-th entry corresponds to the i-th input dimension. Let's say we
-        have 2 dimension where the first dimension consists of 3 different
-        categorical choices and the second dimension is continuous than we
-        have to pass [3, 0]. Note that we count starting from 0.
-    bounds : List[Tuple[float, float]]
-        bounds of input dimensions: (lower, uppper) for continuous dims; (n_cat, np.nan) for categorical dims
-    instance_features : np.ndarray (I, K)
-        Contains the K dimensional instance features of the I different instances
-    pca_components : float
-        Number of components to keep when using PCA to reduce dimensionality of instance features. Requires to
-        set n_feats (> pca_dims).
-
-
-    Attributes
-    ----------
-    target_names
-    num_targets
-    estimators
     """
 
-    def __init__(
-        self,
-        target_names: List[str],
-        configspace: ConfigurationSpace,
-        types: List[int],
-        bounds: List[Tuple[float, float]],
-        seed: int,
-        rf_kwargs: Optional[Dict[str, Any]] = None,
-        instance_features: Optional[np.ndarray] = None,
-        pca_components: Optional[int] = None,
-    ) -> None:
-        super().__init__(
-            target_names=target_names,
-            configspace=configspace,
-            bounds=bounds,
-            types=types,
-            seed=seed,
-            instance_features=instance_features,
-            pca_components=pca_components,
-        )
-        if rf_kwargs is None:
-            rf_kwargs = {}
-        rf_kwargs['seed'] = seed
-        rf_kwargs = rf_kwargs
-        self.estimators = [
-            RandomForestWithInstances(configspace, types, bounds, **rf_kwargs) for _ in range(self.num_targets)
+    def construct_estimators(self,
+                             configspace: ConfigurationSpace,
+                             types: List[int],
+                             bounds: List[Tuple[float, float]],
+                             model_kwargs: Dict[str, Any]) -> List[RandomForestWithInstances]:
+        return [
+            RandomForestWithInstances(configspace, types, bounds, **model_kwargs) for _ in range(self.num_targets)
         ]
