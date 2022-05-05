09e79d68078f211b3fafa0409788ab3aec067c11
dengdifan <difandeng@gmail.com>
2022-05-05 12:14:31 +0200

    add docstrings

--- smac/epm/uncorrelated_mo_rf_with_instances.py
+++ smac/epm/uncorrelated_mo_rf_with_instances.py
@@ -12,7 +12,7 @@ __license__ = "3-clause BSD"
 class UncorrelatedMultiObjectiveRandomForestWithInstances(UncorrelatedMultiObjectiveModel):
     """Wrapper for the random forest to predict multiple targets.
 
-    Only the a list with the target names and the types array for the
+    Only a list with the target names and the types array for the
     underlying forest model are mandatory. All other hyperparameters to
     the random forest can be passed via kwargs. Consult the documentation of
     the random forest for the hyperparameters and their meanings.
@@ -25,4 +25,25 @@ class UncorrelatedMultiObjectiveRandomForestWithInstances(UncorrelatedMultiObjec
         bounds: List[Tuple[float, float]],
         model_kwargs: Dict[str, Any],
     ) -> List[AbstractEPM]:
+        """
+        Construct a list of estimators. The number of the estimators equals 'self.num_targets'
+        Parameters
+        ----------
+        configspace : ConfigurationSpace
+            Configuration space to tune for.
+        types : List[int]
+            Specifies the number of categorical values of an input dimension where
+            the i-th entry corresponds to the i-th input dimension. Let's say we
+            have 2 dimension where the first dimension consists of 3 different
+            categorical choices and the second dimension is continuous than we
+            have to pass [3, 0]. Note that we count starting from 0.
+        bounds : List[Tuple[float, float]]
+            bounds of input dimensions: (lower, uppper) for continuous dims; (n_cat, np.nan) for categorical dims
+        model_kwargs : Dict[str, Any]
+            model kwargs for initializing models
+        Returns
+        -------
+        estimators: List[AbstractEPM]
+            A list of Random Forests
+        """
         return [RandomForestWithInstances(configspace, types, bounds, **model_kwargs) for _ in range(self.num_targets)]
