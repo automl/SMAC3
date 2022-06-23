4d8ffbe37ee6a26118a8ef8f47326cc88aa67fc8
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-06-23 11:27:43 +0200

    Add example for pSMAC

---
+++ examples/python/plot_pSMAC_synthetic_function.py
@@ -0,0 +1,93 @@
+"""
+Synthetic Function with few Hyperparameters
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+An example of applying SMAC to optimize a synthetic function (2d rosenbrock function).
+
+We use the PSMAC facade to demonstrate the parallelization of SMAC.
+Other than that, we use a :term:`Gaussian Process<GP>` to optimize our black-box
+function.
+"""
+import importlib
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+import numpy as np
+from ConfigSpace.hyperparameters import UniformFloatHyperparameter
+
+# Import ConfigSpace and different types of parameters
+from smac.configspace import ConfigurationSpace
+from smac.facade.experimental.psmac_facade import PSMAC
+from smac.facade.smac_bb_facade import SMAC4BB
+import smac
+importlib.reload(smac.facade.experimental.psmac_facade)
+from smac.facade.experimental.psmac_facade import PSMAC
+
+from smac.optimizer.acquisition import EI
+
+# Import SMAC-utilities
+from smac.scenario.scenario import Scenario
+
+__copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
+__license__ = "3-clause BSD"
+
+
+def rosenbrock_2d(x):
+    """The 2 dimensional Rosenbrock function as a toy model
+    The Rosenbrock function is well know in the optimization community and
+    often serves as a toy problem. It can be defined for arbitrary
+    dimensions. The minimium is always at x_i = 1 with a function value of
+    zero. All input parameters are continuous. The search domain for
+    all x's is the interval [-5, 10].
+    """
+
+    x1 = x["x0"]
+    x2 = x["x1"]
+
+    val = 100.0 * (x2 - x1**2.0) ** 2.0 + (1 - x1) ** 2.0
+    return val
+
+
+if __name__ == "__main__":
+    # Build Configuration Space which defines all parameters and their ranges
+    cs = ConfigurationSpace()
+    x0 = UniformFloatHyperparameter("x0", -5, 10, default_value=-3)
+    x1 = UniformFloatHyperparameter("x1", -5, 10, default_value=-4)
+    cs.add_hyperparameters([x0, x1])
+
+    # Scenario object
+    scenario = Scenario(
+        {
+            "run_obj": "quality",  # we optimize quality (alternatively runtime)
+            "runcount-limit": 20,  # max. number of function evaluations
+            "cs": cs,  # configuration space
+            "deterministic": True,
+        }
+    )
+
+    # Use 'gp' or 'gp_mcmc' here
+    model_type = "gp"
+
+    # Example call of the function
+    # It returns: Status, Cost, Runtime, Additional Infos
+    def_value = rosenbrock_2d(cs.get_default_configuration())
+    print("Default Value: %.2f" % def_value)
+
+    # Optimize, using a SMAC-object
+    print("Optimizing! Depending on your machine, this might take a few minutes.")
+    smac = PSMAC(
+        scenario=scenario,
+        facade_class=SMAC4BB,
+        model_type=model_type,
+        rng=np.random.RandomState(42),
+        acquisition_function=EI,  # or others like PI, LCB as acquisition functions
+        tae_runner=rosenbrock_2d,
+        n_optimizers=1,  # 3 parallel workers
+        n_incs=1,  # return one incumbent
+    )
+
+    incumbent = smac.optimize()
+
+    # trajectory = smac.get_trajectory()  # TODO fix this. SMAC needs to be fitted before accessing this
