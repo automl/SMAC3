c25016652d56a78ed9b230885acd2c0145c0947e
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-07-07 09:19:26 +0200

    Change directory of tests

--- tests/test_utils/io/test_result_merging.py
+++ tests/test_utils/io/test_result_merging.py
@@ -25,12 +25,18 @@ __license__ = "3-clause BSD"
 
 
 class ResultMergerTest(unittest.TestCase):
+    def setUp(self) -> None:
+        base_directory = os.path.split(__file__)[0]
+        base_directory = os.path.abspath(os.path.join(base_directory, "../../tests", ".."))
+        os.chdir(base_directory)
+
     def test_init_valueerror(self):
         with self.assertRaises(ValueError):
             rm = ResultMerger()
 
     def test_merge(self):
-        outdir = "../../test_files/example_run"
+        print(os.getcwd())
+        outdir = "test_files/example_run"
         rundirs = [outdir] * 3
         rm = ResultMerger(rundirs=rundirs)
         rh = rm.get_runhistory()
