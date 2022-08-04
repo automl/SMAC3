b875d7dca4ff066f00e9a0609016e39cfe88acc1
Difan Deng <33290713+dengdifan@users.noreply.github.com>
2022-08-04 09:56:11 +0200

    Simplify the maximizer's interface (#876)
    Co-authored-by: René Sass <mail@renesass.de>

--- smac/chooser/turbo_chooser.py
+++ smac/chooser/turbo_chooser.py
@@ -11,7 +11,7 @@ from smac.cli.scenario import Scenario
 from smac.configspace import Configuration
 from smac.model.random_forest.rf_with_instances import RandomForestWithInstances
 from smac.model.utils import get_types
-from smac.optimizer.subspaces.turbo_subspace import TuRBOSubSpace
+from smac.utils.subspaces.turbo_subspace import TuRBOSubSpace
 from smac.runhistory.runhistory import RunHistory
 from smac.runhistory.runhistory_transformer import AbstractRunhistoryTransformer
 from smac.utils.stats import Stats
