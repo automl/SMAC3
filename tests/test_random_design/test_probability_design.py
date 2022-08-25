b574ca895d55892fe9502573ea2c29a042b5faed
Katharina Eggensperger <eggenspk@cs.uni-freiburg.de>
2022-08-25 13:42:34 +0200

    ADD unittest for prob w/ cooldown

--- tests/test_random_design/test_probability_design.py
+++ tests/test_random_design/test_probability_design.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from smac.random_design.probability_design import ProbabilityRandomDesign
+from smac.random_design.probability_design import ProbabilityRandomDesign, \
+    ProbabilityCoolDownRandomDesign
 
 __copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
 __license__ = "3-clause BSD"
@@ -14,3 +15,15 @@ def test_chooser_prob():
             stats.append(c.check(j))
         assert np.isclose(np.mean(stats), 0.1 * i, atol=10**-2)
 
+
+def test_cooldownchooser_prob():
+    for i in range(10):
+        c = ProbabilityCoolDownRandomDesign(seed=1, probability=0.1 * i, factor=0.5)
+        expected = 0.1*i
+        for _ in range(3):
+            stats = []
+            for j in range(100000):
+                stats.append(c.check(j))
+            assert np.isclose(np.mean(stats), expected, atol=10**-2)
+            expected *= 0.5
+            c.next_iteration()
