383f1712514b28a166427ce1c47ebab3ec7d5ec9
René Sass <mail@renesass.de>
2022-11-28 13:58:55 +0100

    Replaced branin by exponential function

--- examples/1_basics/1_exponential_function.py
+++ examples/1_basics/1_exponential_function.py
@@ -12,19 +12,21 @@ be applied to problems with large evaluation budgets (up to 1000 evaluations).
 
 from ConfigSpace import Configuration, ConfigurationSpace, Float
 
-from smac import BlackBoxFacade, Scenario
+import numpy as np
+from matplotlib import pyplot as plt
+from smac import HyperparameterOptimizationFacade as HPOFacade
+from smac import Scenario, RunHistory
 
 __copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
 __license__ = "3-clause BSD"
 
 
-class Rosenbrock2D:
+class ExponentialFunction:
     @property
     def configspace(self) -> ConfigurationSpace:
         cs = ConfigurationSpace(seed=0)
-        x0 = Float("x0", (-5, 10), default=-3)
-        x1 = Float("x1", (-5, 10), default=-4)
-        cs.add_hyperparameters([x0, x1])
+        x = Float("x", (-5, 5), default=-5)
+        cs.add_hyperparameters([x])
 
         return cs
 
@@ -36,25 +38,42 @@ class Rosenbrock2D:
         zero. All input parameters are continuous. The search domain for
         all x's is the interval [-5, 10].
         """
-        x1 = config["x0"]
-        x2 = config["x1"]
+        x = config["x"]
+        return x * x
 
-        cost = 100.0 * (x2 - x1**2.0) ** 2.0 + (1 - x1) ** 2.0
-        return cost
+
+def plot(runhistory: RunHistory, incumbent: Configuration) -> None:
+    plt.figure()
+
+    # Plot ground truth
+    x = list(np.linspace(-5, 5, 100))
+    y = [xi * xi for xi in x]
+    plt.plot(x, y)
+
+    # Plot all trials
+    for k, v in runhistory.items():
+        config = runhistory.get_config(k.config_id)
+        x = config["x"]
+        y = v.cost  # type: ignore
+        plt.scatter(x, y, c="blue", alpha=0.1, zorder=9999, marker="o")
+
+    # Plot incumbent
+    plt.scatter(incumbent["x"], incumbent["x"] * incumbent["x"], c="red", zorder=10000, marker="x")
+
+    plt.show()
 
 
 if __name__ == "__main__":
-    model = Rosenbrock2D()
+    model = ExponentialFunction()
 
     # Scenario object specifying the optimization "environment"
-    scenario = Scenario(model.configspace, name="synthetic_function", n_trials=100)
+    scenario = Scenario(model.configspace, n_trials=100)
 
     # Now we use SMAC to find the best hyperparameters
-    smac = BlackBoxFacade(
+    smac = HPOFacade(
         scenario,
         model.train,  # We pass the target function here
-        overwrite=True,  # Overrides any previous results that are found that are inconsistent with the meta-data.
-        logging_level=0,
+        overwrite=True,  # Overrides any previous results that are found that are inconsistent with the meta-data
     )
 
     incumbent = smac.optimize()
@@ -65,4 +84,7 @@ if __name__ == "__main__":
 
     # Let's calculate the cost of the incumbent
     incumbent_cost = smac.validate(incumbent)
-    print(f"Default cost: {incumbent_cost}")
+    print(f"Incumbent cost: {incumbent_cost}")
+
+    # Let's plot it too
+    plot(smac.runhistory, incumbent)
