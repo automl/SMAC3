81d80c537187dfb5f930331a7b2428385507d502
Helena Graf <h.graf@ai.uni-hannover.de>
2023-02-16 13:31:37 +0100

    Add minimal example

--- examples/6_advanced_features/1_wandb_logging.py
+++ examples/6_advanced_features/1_wandb_logging.py
@@ -6,4 +6,28 @@ This example shows how to use Weights and Biases for logging.
 
 """
 
-pass
+from ConfigSpace import Configuration, ConfigurationSpace
+
+import numpy as np
+from smac import HyperparameterOptimizationFacade, Scenario
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+iris = datasets.load_iris()
+
+
+def train(config: Configuration, seed: int = 0) -> float:
+    classifier = SVC(C=config["C"], random_state=seed)
+    scores = cross_val_score(classifier, iris.data, iris.target, cv=5)
+    return 1 - np.mean(scores)
+
+
+configspace = ConfigurationSpace({"C": (0.100, 1000.0)})
+
+# Scenario object specifying the optimization environment
+scenario = Scenario(configspace, deterministic=True, n_trials=200)
+
+# Use SMAC to find the best configuration/hyperparameters
+smac = HyperparameterOptimizationFacade(scenario, train)
+incumbent = smac.optimize()
