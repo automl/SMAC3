c8a4be5fbba619fdda3c5bbf9f555c45ba7c948e
Helena Graf <h.graf@ai.uni-hannover.de>
2023-05-11 08:28:55 +0200

    Update search space shrinking

--- smac/intensifier/search_space_modifier.py
+++ smac/intensifier/search_space_modifier.py
@@ -1,11 +1,10 @@
 from typing import Callable
 
-from functools import reduce
-
 from ConfigSpace import ConfigurationSpace
 from ConfigSpace.hyperparameters import IntegerHyperparameter
 
 from smac import RunHistory
+from smac.constants import VERY_SMALL_NUMBER
 from smac.utils.logging import get_logger
 
 logger = get_logger(__name__)
@@ -39,8 +38,6 @@ class MultiFidelitySearchSpaceShrinker(AbstractSearchSpaceModifier):
         hparam_values: dict[str, list] = {hparam: [config[hparam] for config in selected_configs] for hparam in hparams}
 
         # compute the new boundaries
-        hparam_shrinkage = []
-        hparam_boundaries = {}
         for hyperparameter in hparams:
             old_min_boundary = search_space[hyperparameter].lower
             old_max_boundary = search_space[hyperparameter].upper
@@ -59,67 +56,53 @@ class MultiFidelitySearchSpaceShrinker(AbstractSearchSpaceModifier):
             # compute shrinkage for both directions
             shrinkage_lower = new_min_boundary - old_min_boundary
             shrinkage_upper = old_max_boundary - new_max_boundary
-            relative_shrinkage = (shrinkage_lower + shrinkage_upper) / (old_max_boundary - old_min_boundary)
+            old_size = old_max_boundary - old_min_boundary
+            new_size = new_max_boundary - new_min_boundary
 
-            if relative_shrinkage != 0:
-                hparam_shrinkage.append(relative_shrinkage)
-            hparam_boundaries[hyperparameter] = (shrinkage_lower, shrinkage_upper, old_min_boundary, old_max_boundary)
-            logger.debug(
-                f"Shrink {hyperparameter}: lower {shrinkage_lower}, upper {shrinkage_upper}, "
-                f"old bounds {old_min_boundary} - {old_max_boundary}, "
-                f"new bounds {new_min_boundary} - {new_max_boundary}"
-            )
+            if new_size == 0:
+                shrinkage_lower -= VERY_SMALL_NUMBER if shrinkage_lower != 0 else 0
+                shrinkage_upper -= VERY_SMALL_NUMBER if shrinkage_upper != 0 else 0
+                new_size = old_size - (shrinkage_upper + shrinkage_lower)
 
-        if len(hparam_shrinkage) == 0:
-            logger.debug("Did not shrink search space.")
-            return
+            if new_size / old_size < self.max_shrinkage:
 
-        # compute the total shrinkage
-        total_shrinkage = reduce(lambda x, y: x * y, hparam_shrinkage)
+                new_new_size = old_size * self.max_shrinkage
+                shrinkage_lower_percentage = shrinkage_lower / (shrinkage_upper + shrinkage_lower)
+                shrinkage_upper_percentage = shrinkage_upper / (shrinkage_upper + shrinkage_lower)
 
-        clipped_total_shrinkage = min(total_shrinkage, self.max_shrinkage)
-        # equally divide the reduction of shrinkage among all hyperparameters with this factor
-        factor = (clipped_total_shrinkage / total_shrinkage) ** (1 / len(hparam_shrinkage))
+                shrinkage_lower = shrinkage_lower_percentage * (old_size - new_new_size)
+                shrinkage_upper = shrinkage_upper_percentage * (old_size - new_new_size)
 
-        if clipped_total_shrinkage != total_shrinkage:
-            logger.debug(
-                f"Clipping total shrinkage from {total_shrinkage} to {clipped_total_shrinkage}" f" (factor: {factor})"
-            )
+                new_min_boundary = old_min_boundary + shrinkage_lower
+                new_max_boundary = old_max_boundary - shrinkage_upper
 
-        for hparam in hparams:
-            boundaries = hparam_boundaries[hparam]
-            new_min_boundary = boundaries[2] + (boundaries[0] * factor)
-            new_max_boundary = boundaries[3] - (boundaries[1] * factor)
+                new_size = new_new_size
 
-            # set the new boundaries
-            logger.debug(f"Setting new boundaries for {hparam} to ({new_min_boundary}, {new_max_boundary})")
+            if new_size != old_size:
+                # min and max cannot be the same
+                if isinstance(search_space[hyperparameter], IntegerHyperparameter):
+                    new_min_boundary = round(new_min_boundary)
+                    new_max_boundary = round(new_max_boundary)
 
-            # min and max cannot be the same
-            if isinstance(search_space[hparam], IntegerHyperparameter):
-                new_min_boundary = round(new_min_boundary)
-                new_max_boundary = round(new_max_boundary)
+                    # don't completely remove the hyperparameter
+                    if new_min_boundary == new_max_boundary:
+                        # if lower shrinkage greater than upper shrinkage, increase upper bound
+                        if shrinkage_lower > shrinkage_upper and new_max_boundary + 1 <= old_max_boundary:
+                            new_max_boundary = new_max_boundary + 1
+                        elif shrinkage_upper < shrinkage_lower:
+                            new_min_boundary = new_max_boundary - 1
+                        else:
+                            # if both shrinkages are equal, move both bounds
+                            new_max_boundary = new_max_boundary + 1
+                            new_min_boundary = new_min_boundary - 1
 
-                # don't completely remove the hyperparameter
-                if new_min_boundary == new_max_boundary:
-                    # if lower shrinkage greater than upper shrinkage, increase upper bound
-                    if boundaries[0] > boundaries[1] and new_max_boundary + 1 <= boundaries[3]:
-                        new_max_boundary = new_max_boundary + 1
-                    elif boundaries[0] < boundaries[1]:
-                        new_min_boundary = new_max_boundary - 1
-                    else:
-                        # if both shrinkages are equal, move both bounds
-                        new_max_boundary = new_max_boundary + 1
-                        new_min_boundary = new_min_boundary - 1
+                logger.debug(
+                    f"Shrinking {hyperparameter}, old min {old_min_boundary}, old max {old_max_boundary}, "
+                    f"shrinkage lower {shrinkage_lower}, shrinkage upper {shrinkage_upper}, "
+                    f"new lower {new_min_boundary}, new upper {new_max_boundary}"
+                )
 
-            logger.debug(f"Parameter {hparam}")
-            logger.debug(
-                f"Old min: {boundaries[2]}, new min: {new_min_boundary}, "
-                f"adjustment: {boundaries[0] * factor}, adjustment without factor: {boundaries[0]}"
-            )
-            logger.debug(
-                f"Old max: {boundaries[3]}, new max: {new_max_boundary}, "
-                f"adjustment: {boundaries[1] * factor}, adjustment without factor: {boundaries[1]}"
-            )
-
-            new_hyperparameter = self.get_hyperparameter_for_bounds[hparam](new_min_boundary, new_max_boundary)
-            search_space._hyperparameters[hparam] = new_hyperparameter
+                new_hyperparameter = self.get_hyperparameter_for_bounds[hyperparameter](
+                    new_min_boundary, new_max_boundary
+                )
+                search_space._hyperparameters[hyperparameter] = new_hyperparameter
