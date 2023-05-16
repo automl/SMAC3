105b9e6d459b9da88d1ad12cc18b1ecdd0f389a5
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2023-05-16 14:43:06 +0200

    Change info to warning

--- smac/facade/abstract_facade.py
+++ smac/facade/abstract_facade.py
@@ -189,7 +189,7 @@ class AbstractFacade:
         # In case of multiple jobs, we need to wrap the runner again using DaskParallelRunner
         if (n_workers := scenario.n_workers) > 1 or dask_client is not None:
             if dask_client is not None:
-                logger.info(
+                logger.warning(
                     "Provided `dask_client`. Ignore `scenario.n_workers`, directly set `n_workers` in `dask_client`."
                 )
             else:
