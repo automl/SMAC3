5cb810221ca612c7443092cbc08b3602f2f6c055
benjamc <c.benjamins@ai.uni-hannover.de>
2024-07-17 14:59:43 +0200

    fix(jsondump): add numpy encoder
    ConfigSpace integer hyperparameters do not return python int type but numpy int64, which is not json serializable

---
+++ smac/utils/numpyencoder.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+import numpy as np
+import json
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
+            return int(obj)
+
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+
+        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+
+        elif isinstance(obj, (np.bool_)):
+            return bool(obj)
+
+        elif isinstance(obj, (np.void)): 
+            return None
+
+        return json.JSONEncoder.default(self, obj)
