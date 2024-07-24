5409bc0131882d224002ed69e7d668483fb48418
benjamc <c.benjamins@ai.uni-hannover.de>
2024-07-24 11:54:22 +0200

    Fix calculation

--- smac/intensifier/hyperband_utils.py
+++ smac/intensifier/hyperband_utils.py
@@ -52,7 +52,7 @@ def determine_HB(min_budget: float, max_budget: float, eta: int = 3) -> dict:
         "budgets_in_stage": _budgets_in_stage,
         "trials_used": total_trials,
         "budget_used": total_budget,
-        "number_of_brackets": _s_max,
+        "number_of_brackets": len(_max_iterations),
     }
 
 
@@ -75,13 +75,19 @@ def determine_hyperband_for_multifidelity(
     Returns
     -------
     dict
-        Info about the Hyperband round
+        Info about one Hyperband round
             "max_iterations"
             "n_configs_in_stage"
             "budgets_in_stage"
             "trials_used"
             "budget_used"
             "number_of_brackets"
+        Info about whole optimization
+            "n_trials"
+            "total_budget"
+            "eta"
+            "min_budget"
+            "max_budget"
 
     """
     # Determine the HB
@@ -108,9 +114,6 @@ def determine_hyperband_for_multifidelity(
             # If we are in the case of b*c > remaining_budget, we will not have any
             # budget left. We can not add full c but the number of trials that still fit
             remaining_budget = max(0, remaining_budget - b * c)
-
-            # print(stage, b, c)
-            # print("-"*20, remaining_trials, remaining_budget)
 
     n_trials = int(number_of_full_hb_rounds * trials_used_per_hb_round + remaining_trials)
 
