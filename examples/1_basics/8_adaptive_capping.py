4d8ee92e23f037f579dee9283c17eaaf5e5ab272
timruhkopf <timruhkopf@gmail.com>
2024-09-25 14:34:02 +0200

    [feature] adaptive capping seems to work now

--- examples/1_basics/8_adaptive_capping.py
+++ examples/1_basics/8_adaptive_capping.py
@@ -6,13 +6,25 @@ Adaptive capping is often used in optimization algorithms, particularly in
 scenarios where the time taken to evaluate solutions can vary significantly.
 
 """
-
+import math
 import time
+import warnings
+from typing import List
+
 from ConfigSpace import ConfigurationSpace, Configuration, Float
 import signal
 from contextlib import contextmanager
 
 from smac.runhistory import InstanceSeedBudgetKey, TrialInfo
+import itertools
+import warnings
+
+import numpy as np
+from ConfigSpace import Categorical, Configuration, ConfigurationSpace, Float
+from sklearn import datasets
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+
 
 
 class TimeoutException(Exception):
@@ -44,23 +56,23 @@ class CappedProblem:
         cs.add_hyperparameters([x0, x1])
         return cs
 
-    def train(self, config: Configuration, budget, seed: int = 0) -> float:
+    def train(self, config: Configuration, instance:str, budget, seed: int = 0) -> float:
         x0 = config["x0"]
         x1 = config["x1"]
 
         try:
-            with timeout(budget):
-                runtime = 1.5 * x1 + x0
+            with timeout(int(math.ceil(budget))):
+                runtime = 0.5 * x1 + 0.5 * x0 * int(instance)
                 time.sleep(runtime)
                 return runtime
         except TimeoutException as e:
-            print(f"Timeout for configuration {config} with budget {budget}")
+            print(f"Timeout for configuration {config} with runtime budget {budget}")
             return budget  # FIXME: what should be returned here?
 
 
 if __name__ == '__main__':
 
-    # FIXME AC facade instad of HPO
+    # FIXME AC facade instead of HPO
     from smac import HyperparameterOptimizationFacade, RunHistory
     from smac import Scenario
 
@@ -72,6 +84,8 @@ if __name__ == '__main__':
         capped_problem.configspace,
         walltime_limit=200,  # After 200 seconds, we stop the hyperparameter optimization
         n_trials=500,  # Evaluate max 500 different trials
+        instances=['1', '2', '3'],
+        instance_features={'1': [1], '2': [2], '3': [3]}
     )
 
     # We want to run five random configurations before starting the optimization.
@@ -89,7 +103,7 @@ if __name__ == '__main__':
             ----------
             scenario : Scenario
                 Scenario object
-            runtime_cutoff : int, defaults to None: Initial runtime budget in seconds for
+            runtime_cutoff : int, defaults to None: Initial runtime budget in seconds for.
             adaptive capping.
                 A non-None value will trigger adaptive capping and require the target algorithm to accept
                 a budget argument that is the number of seconds to maximally run the target algorithm.
@@ -97,6 +111,25 @@ if __name__ == '__main__':
             super().__init__(scenario, *args, **kwargs)
             self.runtime_cutoff = runtime_cutoff
             self.adaptive_capping_slackfactor = adaptive_capping_slackfactor
+
+        def get_trials_of_interest(
+                self,
+                config: Configuration,
+                *,
+                validate: bool = False,
+                seed: int | None = None,
+        ) -> list[TrialInfo]:
+            """Returns the trials of interest for a given configuration.
+            Expands the keys from ``get_instance_seed_keys_of_interest`` with the config.
+            """
+            is_keys = self.get_instance_seed_keys_of_interest(validate=validate, seed=seed)
+
+            trials = []
+            for key in is_keys:
+                # FIXME: trialinfo needs budget key
+                trials.append(TrialInfo(config=config, instance=key.instance, seed=key.seed))
+
+            return trials
 
         def _get_next_trials(
                 self,
@@ -106,13 +139,19 @@ if __name__ == '__main__':
                 from_keys: list[InstanceSeedBudgetKey] | None = None,
                 shuffle: bool = True,
         ) -> list[TrialInfo]:
-            trials = super()._get_next_trials(config, N=N, from_keys=from_keys, shuffle=shuffle)
-            if self.runtime_cutoff is not None:
+            trials = super()._get_next_trials(
+                config,
+                N=N,
+                from_keys=from_keys,
+                shuffle=shuffle
+            )
+            if self.runtime_cutoff is not None and bool(trials):
                 # We need to adapt the budget to the runtime cutoff
-                budget = self._get_adaptivecapping_budget(trials)
+                budgets = [self._get_adaptivecapping_budget(t.config) for t in trials]
+
                 trials = [
-                    TrialInfo(config=t.config, seed=t.seed, budget=budget)
-                    for t in trials
+                    TrialInfo(config=t.config, instance=t.instance, seed=t.seed, budget=b)
+                    for b, t in zip(budgets, trials)
                 ]
 
             return trials
@@ -127,6 +166,11 @@ if __name__ == '__main__':
         ) -> float:
             """Adaptive capping: Compute cutoff based on time so far used for incumbent and reduce
             cutoff for next run of challenger accordingly.
+
+            Warning:
+            For concurrent runs, the budget will be determined for a challenger x instance
+            combination at the moment the challenger is considered for the instance, ignorant of
+            the runtime cost of the currently running instances of the same configuration.
 
             !Only applicable if self.run_obj_time
 
@@ -154,11 +198,18 @@ if __name__ == '__main__':
             if len(incumbents) == 0:
                 return  self.runtime_cutoff
 
+            if len(incumbents) > 1:
+                warnings.warn("Adaptive capping is only supported for single incumbent scenarios")
+
             inc_sum_cost = self.runhistory.sum_cost(
-                config=incumbent,
+                config=incumbents[0],
                 instance_seed_budget_keys=None,
                 normalize=True
             )
+
+            # if len(challenger) == 0:
+            #     # we only got the
+            #     return
 
             # original logic for get_runs_for_config:
             # https://github.com/automl/SMAC3/blob/f1d2aa2ea3b6ad4075550af69e3300f19411a5ea/smac/runhistory/runhistory.py#L772
@@ -167,6 +218,8 @@ if __name__ == '__main__':
                 challenger,
                 highest_observed_budget_only=True
             )
+            # fixme: for each challenger, we need to compute its total cost!
+            #  and then we need to return a per config based budget!
             chal_sum_cost = self.runhistory.sum_cost(
                 config=challenger, instance_seed_budget_keys=chall_inst_seeds, normalize=True
             )
@@ -177,12 +230,11 @@ if __name__ == '__main__':
                 inc_sum_cost * self.adaptive_capping_slackfactor - chal_sum_cost
             )
 
-            self.runtime_cutoff = cutoff
             return cutoff
 
 
     # Create our intensifier
-    intensifier = CappedIntensifier(scenario, runtime_cutoff=15)
+    intensifier = CappedIntensifier(scenario, runtime_cutoff=10)
 
     # Create our SMAC object and pass the scenario and the train method
     smac = HyperparameterOptimizationFacade(
