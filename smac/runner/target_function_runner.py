4f224509e98b5e41b2991d0c5a27912a06cf8a41
benjamc <c.benjamins@instadeep.com>
2024-12-04 16:09:10 +0100

    fix(target_function_runner): correctly query attr (underscore missing)

--- smac/runner/target_function_runner.py
+++ smac/runner/target_function_runner.py
@@ -222,7 +222,7 @@ class TargetFunctionRunner(AbstractSerialRunner):
             ordered_cost: list[float] = []
             for name in self._objectives:
                 if name not in result:
-                    raise RuntimeError(f"Objective {name} was not found in the returned costs.")
+                    raise RuntimeError(f"Objective {name} was not found in the returned costs.")  # noqa: E713
 
                 ordered_cost.append(result[name])
 
@@ -240,7 +240,7 @@ class TargetFunctionRunner(AbstractSerialRunner):
 
         if cost is None:
             status = StatusType.CRASHED
-            cost = self.crash_cost
+            cost = self._crash_cost
 
         # We want to get either a float or a list of floats.
         cost = np.asarray(cost).squeeze().tolist()
