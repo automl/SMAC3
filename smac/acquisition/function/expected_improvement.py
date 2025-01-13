c80f64946109b2163a676a10c13a6749e52ef3d8
Daphne12345 <daphne.theo@yahoo.de>
2025-01-13 14:06:06 +0100

    Added equations and refernces for EI

--- smac/acquisition/function/expected_improvement.py
+++ smac/acquisition/function/expected_improvement.py
@@ -18,21 +18,20 @@ logger = get_logger(__name__)
 
 class EI(AbstractAcquisitionFunction):
     r"""The Expected Improvement (EI) criterion is used to decide where to evaluate a function f(x) next. The goal is to
-    balance exploration and exploitation. Expected Improvement in a genereal form (with or without function values in
-    log space) acquisition function
+    balance exploration and exploitation. Expected Improvement (with or without function values in log space)
+    acquisition function
 
     :math:`EI(X) := \mathbb{E}\left[ \max\{0, f(\mathbf{X^+}) - f_{t+1}(\mathbf{X}) - \xi \} \right]`,
     with :math:`f(X^+)` as the best location.
 
-    For logEI, this equation is implemented:
-    :math: `logEI(X) :=  y^\star \Phi(z) - \exp\left(\mu + \frac{\sigma^2}{2}\right) \Phi(z - \sigma)`
+    Reference for EI: Jones, D.R. and Schonlau, M. and Welch, W.J. (1998). Efficient Global Optimization of Expensive
+    Black-Box Functions. Journal of Global Optimization 13, 455–492
 
-    The equation is derived from the orginal equation for logEI:
-    :math: `logEI = \int_{-\infty}^{\log y^*} \left(y^* - \exp l \right) p(l \mid \mathbf{x}, \mathcal{D}) \, dl`
-    Reference: Hutter, F. and Hoos, H. and Leyton-Brown, K. and Murphy, K. (2009). An experimental investigation of
-    model-based parameter optimisation: SPO and beyond. In: Conference on Genetic and Evolutionary Computation
+    Reference for logEI: Hutter, F. and Hoos, H. and Leyton-Brown, K. and Murphy, K. (2009). An experimental
+    investigation of model-based parameter optimisation: SPO and beyond. In: Conference on Genetic and
+    Evolutionary Computation
 
-    The derivation of the equation can be found here:
+    The logEI implemententation is based on the derivation of the orginal equation by:
     Watanabe, S. (2024). Derivation of Closed Form of Expected Improvement for Gaussian Process Trained on
     Log-Transformed Objective. https://arxiv.org/abs/2411.18095
 
