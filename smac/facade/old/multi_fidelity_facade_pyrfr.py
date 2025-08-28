5d9f79ff9a45f9ab307b76c37c17df69e805285d
dengdifan <difandeng@gmail.com>
2025-08-28 14:53:07 +0200

    fix pre-commit

--- smac/facade/old/multi_fidelity_facade_pyrfr.py
+++ smac/facade/old/multi_fidelity_facade_pyrfr.py
@@ -4,6 +4,7 @@ from smac.facade.multi_fidelity_facade import MultiFidelityFacade
 from smac.facade.old.hyperparameter_optimization_facade_pyrfr import (
     HyperparameterOptimizationRFRFacade,
 )
+
 __copyright__ = "Copyright 2022, automl.org"
 __license__ = "3-clause BSD"
 
