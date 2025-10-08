e0e59a98b0ac269dfffd36303364209443353e5b
benjamc <c.benjamins@ai.uni-hannover.de>
2025-10-08 09:34:51 +0200

    refactor(expected_hypervolume): delete ehvi method which was a comment anyway

--- smac/acquisition/function/expected_hypervolume.py
+++ smac/acquisition/function/expected_hypervolume.py
@@ -14,17 +14,11 @@ from smac.runhistory.encoder import AbstractRunHistoryEncoder
 from smac.utils.logging import get_logger
 from smac.utils.multi_objective import normalize_costs
 
-# import torch
-# from botorch.acquisition.multi_objective import ExpectedHypervolumeImprovement
-# from botorch.models.model import Model
-# from botorch.utils.multi_objective.box_decompositions.non_dominated import (
-#     NondominatedPartitioning,
-# )
-
 __copyright__ = "Copyright 2022, automl.org"
 __license__ = "3-clause BSD"
 
 logger = get_logger(__name__)
+
 
 class AbstractHVI(AbstractAcquisitionFunction):
     def __init__(self):
@@ -127,87 +121,7 @@ class AbstractHVI(AbstractAcquisitionFunction):
             hv = self.get_hypervolume(points)
             phvi[i] = hv - self.population_hv
 
-        # if len(X) == 10000:
-        #     for op in ["max", "min", "mean", "median"]:
-        #         val = getattr(np, op)(phvi)
-        #         print(f"{op:6} - {val}")
-        #     time.sleep(1.5)
-
         return phvi.reshape(-1, 1)
-
-
-# class EHVI(AbstractHVI):
-#     def __init__(self):
-#         super(EHVI, self).__init__()
-#         self._ehvi: ExpectedHypervolumeImprovement | None = None
-#
-#     @property
-#     def name(self) -> str:
-#         return "Expected Hypervolume Improvement"
-#
-#     def _update(self, **kwargs: Any) -> None:
-#         super(EHVI, self)._update(**kwargs)
-#         incumbents: list[Configuration] = kwargs.get("incumbents", None)
-#
-#         # Update EHVI
-#         # Prediction all
-#         population_configs = incumbents
-#         population_X = np.array([config.get_array() for config in population_configs])
-#         population_costs, _ = self.model.predict_marginalized(population_X)
-#         # Normalize the objectives here to give equal attention to the objectives when computing the HV
-#         population_costs = [normalize_costs(p, self._objective_bounds) for p in population_costs]
-#
-#         # BOtorch EHVI implementation
-#         bomodel = _ModelProxy(self.model, self._objective_bounds)
-#         # ref_point = pygmo.hypervolume(population_costs).refpoint(
-#         #     offset=1
-#         # )  # TODO get proper reference points from user/cutoffs
-#         ref_point = [1.1] * len(self._objective_bounds)
-#         # ref_point = torch.asarray(ref_point)
-#         # TODO partition from all runs instead of only population?
-#         # TODO NondominatedPartitioning and ExpectedHypervolumeImprovement seem no too difficult to implement natively
-#         # TODO pass along RNG
-#         # Transfrom the objective space to cells based on the population
-#         partitioning = NondominatedPartitioning(torch.asarray(ref_point), torch.asarray(population_costs))
-#         self._ehvi = ExpectedHypervolumeImprovement(bomodel, ref_point, partitioning)
-#
-#     def _compute(self, X: np.ndarray) -> np.ndarray:
-#         """Computes the EHVI values and its derivatives.
-#
-#         Parameters
-#         ----------
-#         X: np.ndarray(N, D), The input points where the acquisition function
-#             should be evaluated. The dimensionality of X is (N, D), with N as
-#             the number of points to evaluate at and D is the number of
-#             dimensions of one X.
-#
-#         Returns
-#         -------
-#         np.ndarray(N,1)
-#             Expected HV Improvement of X
-#         """
-#         if self._ehvi is None:
-#             raise ValueError(f"The expected hypervolume improvement is not defined yet. Call self.update.")
-#
-#         if len(X.shape) == 1:
-#             X = X[:, np.newaxis]
-#
-#         # m, var_ = self.model.predict_marginalized_over_instances(X)
-#         # Find a way to propagate the variance into the HV
-#         boX = torch.asarray(X).reshape(X.shape[0], 1, -1)  # 2D -> #3D
-#         improvements = self._ehvi(boX).numpy().reshape(-1, 1)  # TODO here are the expected hv improvements computed.
-#         return improvements
-#
-#         # TODO non-dominated sorting of costs. Compute EHVI only until the EHVI is not expected to improve anymore.
-#         # Option 1: Supplement missing instances of population with acq. function to get predicted performance over
-#         # all instances. Idea is this prevents optimizing for the initial instances which get it stuck in local optima
-#         # Option 2: Only on instances of population
-#         # Option 3: EVHI per instance and aggregate afterwards
-#         # ehvi = np.zeros(len(X))
-#         # for i, indiv in enumerate(m):
-#         #     ehvi[i] = self.get_hypervolume(population_costs + [indiv]) - population_hv
-#         #
-#         # return ehvi.reshape(-1, 1)
 
 
 class PHVI(AbstractHVI):
