23ee19cea57c33ff524699a3c7a3c6c798e6c7cb
lhennig0103 <144096938+lhennig0103@users.noreply.github.com>
2025-11-13 17:06:54 +0100

    Add RunHistoryPowerTransformEncoder class
    Implement RunHistoryPowerTransformEncoder to apply PowerTransformer on response values.

---
+++ smac/runhistory/encoder/power_transform_encoder.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import numpy as np
+from sklearn.preprocessing import PowerTransformer
+
+from smac.runhistory.encoder.encoder import RunHistoryEncoder
+from smac.utils.logging import get_logger
+
+__copyright__ = "Copyright 2025, Leibniz University Hanover, Institute of AI"
+__license__ = "3-clause BSD"
+
+logger = get_logger(__name__)
+
+
+class RunHistoryPowerTransformEncoder(RunHistoryEncoder):
+    def transform_response_values(self, values: np.ndarray) -> np.ndarray:
+        """Apply PowerTransformer (Yeo-Johnson) to response values."""
+
+        if values.size == 0:
+            logger.debug("Received empty array for transformation.")
+            return values.reshape(-1, 1)
+
+        values = values.reshape(-1, 1)
+        transformer = PowerTransformer(method="yeo-johnson", standardize=True)
+        return transformer.fit_transform(values)
