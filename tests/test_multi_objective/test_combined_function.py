ab77880dffccc16a905e10d887db36cf0002cc10
Daphne12345 <daphne.theo@yahoo.de>
2026-04-28 12:00:54 +0200

    use walltime_limit=3600 in MO tests so GP-based facades always complete all n_trials

--- tests/test_multi_objective/test_combined_function.py
+++ tests/test_multi_objective/test_combined_function.py
@@ -73,7 +73,9 @@ def test_mean_aggregation(facade, make_scenario, configspace):
     N_TRIALS = 64
     RETRAIN_AFTER = 8
 
-    scenario: Scenario = make_scenario(configspace, use_multi_objective=True, n_trials=N_TRIALS)
+    # walltime_limit must be large enough that n_trials (not walltime) is always the binding constraint.
+    # BlackBoxFacade trains a GP which is slower than other facades.
+    scenario: Scenario = make_scenario(configspace, use_multi_objective=True, n_trials=N_TRIALS, walltime_limit=3600)
         # TODO: Check whether different weighting affects the sampled configurations.
     weights = [[0.1,0.9], [0.5,0.5], [0.8,0.2], [1.0,0.0], [0.0,1.0], None]
     for weight_pair in weights:
@@ -116,7 +118,7 @@ def test_parego(facade, make_scenario, configspace):
     N_TRIALS = 64
     RETRAIN_AFTER = 8
 
-    scenario: Scenario = make_scenario(configspace, use_multi_objective=True, n_trials=N_TRIALS)
+    scenario: Scenario = make_scenario(configspace, use_multi_objective=True, n_trials=N_TRIALS, walltime_limit=3600)
     multi_objective_algorithm = WrapStrategy(ParEGO, scenario=scenario)
     intensifier = Intensifier(scenario, max_config_calls=1, max_incumbents=10)
     config_selector = ConfigSelector(scenario, retrain_after=RETRAIN_AFTER)
